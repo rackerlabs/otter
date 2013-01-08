5cb9246f79e4019b6b401760679070fb553bc4d2
Ken Wronkiewicz <ken.wronkiewicz@cloudkick.com>
2013-01-08 11:12:53 -0800

    adding hashkey generation code.

---
+++ otter/util/hashkey.py
@@ -0,0 +1,10 @@
+""" Hash key related library code """
+import random
+
+""" Cribbed off of the way ELE works """
+def generate_random_str(len):
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    out_str = ""
+    for i in xrange(len):
+        out_str += random.choice(chars)
+    return out_str
