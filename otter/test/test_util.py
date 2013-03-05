18911f40ae3de2cf0234e55aad73fe06822b85cf
David Reid <dreid@dreid.org>
2013-03-05 10:19:06 -0800

    Use large-random tokens for capabilities.
    generate_capability now returns a 2-tuple of the version (always 1)
    and a hex encoded 256-bit random integer.
    
    This branch fixes all existing test coverage and adds specific tests for
    generate_capability.

--- otter/test/test_util.py
+++ otter/test/test_util.py
@@ -1,10 +1,12 @@
 """
 Tests for ``otter.util``
 """
+import mock
 
 from twisted.trial.unittest import TestCase
 
 from otter.util.http import append_segments
+from otter.util.hashkey import generate_capability
 
 
 class HTTPUtilityTests(TestCase):
@@ -44,3 +46,33 @@ class HTTPUtilityTests(TestCase):
             append_segments('http://example.com', 'foo bar'),
             'http://example.com/foo%20bar'
         )
+
+
+class CapabilityTests(TestCase):
+    """
+    Test capability generation.
+    """
+    @mock.patch('otter.util.hashkey.os.urandom')
+    def test_urandom_32_bytes(self, urandom):
+        """
+        generate_capability will use os.urandom to get 32 bytes of random
+        data.
+        """
+        (_v, _cap) = generate_capability()
+        urandom.assert_called_once_with(32)
+
+    @mock.patch('otter.util.hashkey.os.urandom')
+    def test_hex_encoded_cap(self, urandom):
+        """
+        generate_capability will hex encode the random bytes from os.urandom.
+        """
+        urandom.return_value = '\xde\xad\xbe\xef'
+        (_v, cap) = generate_capability()
+        self.assertEqual(cap, 'deadbeef')
+
+    def test_version_1(self):
+        """
+        generate_capability returns a version 1 capability.
+        """
+        (v, _cap) = generate_capability()
+        self.assertEqual(v, "1")
