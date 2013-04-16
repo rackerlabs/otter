30b14197a4d5177dabf0b4566d641b1e60621224
David Reid <dreid@dreid.org>
2013-04-16 15:28:36 -0700

    Docstrings, some refactorings.

--- otter/auth.py
+++ otter/auth.py
@@ -1,12 +1,12 @@
 """
-Functions for resolving authentication information.
+Functions for interacting with the authentication API.
 """
 
 import json
 import treq
 
 from otter.util.config import config_value
-from otter.util.http import headers, check_success
+from otter.util.http import headers, check_success, append_segments
 
 
 def authenticate_tenant(tenant_id):
@@ -16,16 +16,26 @@ def authenticate_tenant(tenant_id):
     :params tenant_id: id of the tenant to authenticate as
     :returns: Deferred that fires with a 2-tuple of auth token and service catalog.
     """
+    ia = _ImpersonatingAuthenticator(config_value('identity.username'),
+                                     config_value('identity.password'),
+                                     config_value('identity.url'),
+                                     config_value('identity.admin_url'))
+    return ia.token_for_tenant(tenant_id)
 
-    raise NotImplementedError()
 
-
-class ImpersonationAuthenticator(object):
-    def __init__(self, service_user, service_api_key, url, admin_url):
+class _ImpersonatingAuthenticator(object):
+    """
+    An authentication handler that first uses a service account to authenticate
+    and then impersonates the desired tenant_id.
+    """
+    def __init__(self, service_user, service_password, url, admin_url):
         self._service_user = service_user
-        self._service_api_key = service_api_key
-        self._url = url
-        self._admin_url = admin_url
+        self._service_password = service_password
+
+        # XXX: TODO: This is not the correct way to deal with unicode URLs.
+        # Maybe append_segments should support it better.
+        self._url = url.encode('ascii')
+        self._admin_url = admin_url.encode('ascii')
 
     def token_for_tenant(self, tenant_id):
         """
@@ -35,11 +45,11 @@ class ImpersonationAuthenticator(object):
         """
         d = authenticate_user(self._url,
                               self._service_user,
-                              self._service_api_key)
+                              self._service_password)
         d.addCallback(extract_token)
 
         def find_user(impersonator_token):
-            return lookup_user(
+            return users_for_tenant(
                 self._admin_url,
                 impersonator_token,
                 tenant_id).addCallback(
@@ -58,7 +68,7 @@ class ImpersonationAuthenticator(object):
         d.addCallback(impersonate)
 
         def endpoints((impersonator_token, token)):
-            scd = service_catalog(self._admin_url, impersonator_token, token)
+            scd = endpoints_for_token(self._admin_url, impersonator_token, token)
             scd.addCallback(lambda catalog: (token, catalog.get('endpoints', [])))
             return scd
 
@@ -67,30 +77,67 @@ class ImpersonationAuthenticator(object):
         return d
 
 
-def extract_token(resp):
-    return resp['access']['token']['id'].encode('ascii')
+def extract_token(auth_response):
+    """
+    Extract an auth token from an authentication response.
+
+    :param dict auth_response: A dictionary containing the decoded response
+        from the authentication API.
+    :rtype: str
+    """
+    return auth_response['access']['token']['id'].encode('ascii')
 
 
-def service_catalog(auth_endpoint, impersonator_token, auth_token):
-    d = treq.get(auth_endpoint + '/tokens/' + auth_token + '/endpoints',
-                 headers=headers(impersonator_token))
+def endpoints_for_token(auth_endpoint, identity_admin_token, user_token):
+    """
+    Get the list of endpoints from the service_catalog for the specified token.
+
+    :param str auth_endpoint: Identity API endpoint URL.
+    :param str identity_admin_token: An Auth token for an identity admin user
+        who can get the endpoints for a specified user token.
+    :param str user_token: The user token to request endpoints for.
+
+    :return: decoded JSON response as dict.
+    """
+
+    d = treq.get(append_segments(auth_endpoint, 'tokens', user_token, 'endpoints'),
+                 headers=headers(identity_admin_token))
     d.addCallback(check_success, [200, 203])
     d.addCallback(treq.json_content)
     return d
 
 
-def lookup_user(auth_endpoint, auth_token, tenant_id):
+def users_for_tenant(auth_endpoint, identity_admin_token, tenant_id):
+    """
+    Retrieve all users for the specified tenant.
+
+    :param str auth_endpoint: Identity API endpoint URL.
+    :param str identity_admin_token: An Auth token for an identity admin user
+        who can get the endpoints for a specified user token.
+    :param tenant_id: The tenant ID we wish to find a user for.
+
+    :return: Decoded JSON response as dict.
+    """
     d = treq.get(
-        auth_endpoint + '/tenants/' + str(tenant_id) + '/users',
-        headers=headers(auth_token))
+        append_segments(auth_endpoint, 'tenants', str(tenant_id), 'users'),
+        headers=headers(identity_admin_token))
     d.addCallback(check_success, [200, 203])
     d.addCallback(treq.json_content)
     return d
 
 
 def authenticate_user(auth_endpoint, username, password):
+    """
+    Authenticate to a Identity auth endpoint with a username and password.
+
+    :param str auth_endpoint: Identity API endpoint URL.
+    :param str username: Username to authenticate as.
+    :param str password: Password for the specified user.
+
+    :return: Decoded JSON response as dict.
+    """
     d = treq.post(
-        auth_endpoint + '/tokens',
+        append_segments(auth_endpoint, 'tokens'),
         json.dumps(
             {
                 "auth": {
@@ -108,8 +155,20 @@ def authenticate_user(auth_endpoint, username, password):
 
 
 def impersonate_user(auth_endpoint, impersonater_token, username, expires_in=10800):
+    """
+    Acquire an auth-token for a user via impersonation.
+
+    :param str auth_endpoint: Identity API endpoint URL.
+    :param str impersonater_token: Auth token that has the appropriate
+        permissions to impersonate other users.
+    :param str username: Username to impersonate.
+    :param str expires_in: Number of seconds for which the token will be valid.
+
+    :return: Decoded JSON as dict.
+    """
+
     d = treq.post(
-        auth_endpoint + '/RAX-AUTH/impersonation-tokens',
+        append_segments(auth_endpoint, 'RAX-AUTH', 'impersonation-tokens'),
         json.dumps({
             "RAX-AUTH:impersonation": {
                 "user": {"username": username},
@@ -129,14 +188,16 @@ if __name__ == '__main__':
 
     from twisted.internet.task import react
     from twisted.internet.defer import inlineCallbacks
+
     from otter.util.config import set_config_data
+
     set_config_data(jsonfig.from_path('config.json'))
 
     @inlineCallbacks
-    def main(reactor, username, api_key):
-        ia = ImpersonationAuthenticator(username, api_key,
-                                        url=config_value('identity.url').encode('ascii'),
-                                        admin_url=config_value('identity.admin_url').encode('ascii'))
+    def main(reactor, username, password):
+        ia = _ImpersonatingAuthenticator(username, password,
+                                         config_value('identity.url'),
+                                         config_value('identity.admin_url'))
 
         r = yield ia.token_for_tenant('5821004')
         pprint.pprint(r)
