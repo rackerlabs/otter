4f0c8174bd421100a574b3a1c578693686ced5f1
David Reid <dreid@dreid.org>
2013-04-23 10:27:56 -0700

    When in doubt, MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,51 +1,14 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 NAME = 'otter'
-SCHEMA_DIR = 'schema'
 
-
-def getPackages(base):
-    """
-    Recursively find python packages.
-    """
-    packages = []
-
-    def visit(arg, directory, files):
-        if '__init__.py' in files:
-            packages.append(directory.replace('/', '.'))
-
-    os.path.walk(base, visit, None)
-
-    return packages
-
-packages = getPackages(NAME)
-
-
-def getSchema(base):
-    """
-    Recursively find cql files
-    """
-    schemas = []
-
-    def visit(arg, directory, files):
-        schemas.append(
-            (directory.rstrip('/'),
-             [os.path.join(directory, filename)
-              for filename in files if filename.endswith('.cql')]))
-
-    os.path.walk(base, visit, None)
-    return schemas
-
-data_files = getSchema(SCHEMA_DIR)
-data_files.append(('otter/rest', ['otter/rest/otter_ascii.txt']))
-
+packages = find_packages()
 
 # If a twisted/plugins directory exists make sure we install the
 # twisted.plugins packages.
 if os.path.exists('twisted/plugins'):
     packages.append('twisted.plugins')
-
 
 # This is an incomplete setup definition useful only for
 # installation into a virtualenv with terrarium for deployment.
@@ -53,7 +16,6 @@ setup(
     name=NAME,
     version='0.0.0',
     packages=packages,
-    data_files=data_files,
     scripts=['scripts/load_cql.py']
 )
 
