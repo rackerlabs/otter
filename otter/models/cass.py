9c2b18a0abf42111f4796c0d4c43a4585431a807
Shawn Ashlee <shawnashlee@yahoo.com>
2013-06-27 11:51:53 -0700

    Adding get_counts method to ScalingGroupCollection

--- otter/models/cass.py
+++ otter/models/cass.py
@@ -105,6 +105,9 @@ _cql_list_webhook = ('SELECT "webhookId", data, capability, deleted FROM {cf} '
 _cql_find_webhook_token = ('SELECT "tenantId", "groupId", "policyId", deleted FROM {cf} WHERE '
                            '"webhookKey" = :webhookKey;')
 
+_cql_count_for_tenant = ('SELECT COUNT(*) FROM {cf} WHERE "tenantId" = :tenantId '
+                         'AND WHERE deleted=false;')
+
 
 def filter_deleted(cass_result):
     """
@@ -934,4 +937,21 @@ class CassScalingGroupCollection:
                                     {"webhookKey": capability_hash},
                                     get_consistency_level('list', 'group'))
         d.addCallback(_do_webhook_lookup)
+        return d
+
+    def get_counts(self, log, tenant_id):
+        """
+        see :meth:`otter.models.interface.IScalingGroupCollection.get_counts`
+        """
+
+        fields = ['scaling_config', 'scaling_policies', 'policy_webhooks']
+        deferred = [self.connection.execute(_cql_count_for_tenant.format(cf=field),
+                                            {'tenantId': tenant_id},
+                                            get_consistency_level('count', 'group'))
+                    for field in fields]
+
+        d = defer.gatherResults(deferred)
+        d.addCallback(lambda results: [i.values()[0] for i in results])
+        d.addCallback(lambda results: dict(zip(
+            ('groups', 'policies', 'webhooks'), results)))
         return d
