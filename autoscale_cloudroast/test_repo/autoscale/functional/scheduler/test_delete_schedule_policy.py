a407c15930afcd990aa466511116486733457c8e
Lekha Jeevan <lekha.jeevan@rackspace.com>
2013-07-08 10:34:27 -0700

    chnages to delete scheduler test

--- autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_delete_schedule_policy.py
+++ autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_delete_schedule_policy.py
@@ -30,13 +30,6 @@ class DeleteSchedulerPolicy(ScalingGroupFixture):
         self.assertEquals(self.at_style_policy['status_code'], 201,
                           msg='Create schedule policy (at style) failed with {0} for group {1}'
                           .format(self.at_style_policy['status_code'], self.group.id))
-        self.delete_at_style_policy = self.autoscale_client.delete_scaling_policy(
-            group_id=self.group.id,
-            policy_id=self.at_style_policy['id'])
-        self.assertEquals(self.delete_at_style_policy.status_code, 204,
-                          msg='Delete scheduler policy (at style) failed with {0}'
-                          'for group {1}'
-                          .format(self.delete_at_style_policy.status_code, self.group.id))
         self.cron_style_policy = self.autoscale_behaviors.create_schedule_policy_given(
             group_id=self.group.id,
             sp_change=self.sp_change,
@@ -44,13 +37,6 @@ class DeleteSchedulerPolicy(ScalingGroupFixture):
         self.assertEquals(self.cron_style_policy['status_code'], 201,
                           msg='Create schedule policy (cron style) failed with {0} for group {1}'
                           .format(self.cron_style_policy['status_code'], self.group.id))
-        self.delete_cron_style_policy = self.autoscale_client.delete_scaling_policy(
-            group_id=self.group.id,
-            policy_id=self.cron_style_policy['id'])
-        self.assertEquals(self.delete_cron_style_policy.status_code, 204,
-                          msg='Delete scheduler policy (at style) failed with {0}'
-                          'for group {1}'
-                          .format(self.delete_cron_style_policy.status_code, self.group.id))
 
     def tearDown(self):
         """
@@ -58,14 +44,45 @@ class DeleteSchedulerPolicy(ScalingGroupFixture):
         """
         pass
 
-    def test_delete_policy_schedule_at_style_and_cron_style(self):
+    def test_delete_policy_schedule_at_style(self):
         """
-        Verify the delete scheduler policy via at style and cron style,
+        Verify the delete scheduler policy via at style,
         for response code 204, headers.
         """
-        self.assertTrue(self.delete_at_style_policy.headers is not None,
+        delete_at_style_policy = self.autoscale_client.delete_scaling_policy(
+            group_id=self.group.id,
+            policy_id=self.at_style_policy['id'])
+        self.assertEquals(delete_at_style_policy.status_code, 204,
+                          msg='Delete scheduler policy (at style) failed with {0}'
+                          'for group {1}'
+                          .format(delete_at_style_policy.status_code, self.group.id))
+        self.assertTrue(delete_at_style_policy.headers is not None,
                         msg='The headers are not as expected')
-        self.validate_headers(self.delete_at_style_policy.headers)
-        self.assertTrue(self.delete_cron_style_policy.headers is not None,
+        self.validate_headers(delete_at_style_policy.headers)
+        self.assertTrue(self.at_style_policy['id'] not in self._policy_list_for_group(self.group.id))
+        self.assertTrue(self.cron_style_policy['id'] in self._policy_list_for_group(self.group.id))
+
+    def test_delete_policy_schedule_cron_style(self):
+        """
+        Verify the delete scheduler policy via cron style,
+        for response code 204, headers.
+        """
+        delete_at_style_policy = self.autoscale_client.delete_scaling_policy(
+            group_id=self.group.id,
+            policy_id=self.cron_style_policy['id'])
+        self.assertEquals(delete_at_style_policy.status_code, 204,
+                          msg='Delete scheduler policy (at style) failed with {0}'
+                          'for group {1}'
+                          .format(delete_at_style_policy.status_code, self.group.id))
+        self.assertTrue(delete_at_style_policy.headers is not None,
                         msg='The headers are not as expected')
-        self.validate_headers(self.delete_cron_style_policy.headers)
+        self.validate_headers(delete_at_style_policy.headers)
+        self.assertTrue(self.at_style_policy['id'] in self._policy_list_for_group(self.group.id))
+        self.assertTrue(self.cron_style_policy['id'] not in self._policy_list_for_group(self.group.id))
+
+    def _policy_list_for_group(self, group_id):
+        """
+        Lists the policies in a group and returns the policy id list
+        """
+        list_policies_resp = self.autoscale_client.list_policies(group_id)
+        return [each_policy.id for each_policy in list_policies_resp.entity]
