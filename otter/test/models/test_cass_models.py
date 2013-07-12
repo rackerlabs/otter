7328d2e37f09fda0777375974cbf3f79ca8832df
Ken Wronkiewicz <ken.wronkiewicz@cloudkick.com>
2013-07-11 17:24:56 -0700

    changing the model so that if you try to change the type, it will fail validation.

--- otter/test/models/test_cass_models.py
+++ otter/test/models/test_cass_models.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import iso8601
 
 from twisted.trial.unittest import TestCase
+from jsonschema import ValidationError
 
 from otter.json_schema import group_examples
 
@@ -744,18 +745,35 @@ class CassScalingGroupTestCase(IScalingGroupProviderMixin, LockMixin, TestCase):
         Test that you can update a scaling policy, and if successful it returns
         None
         """
-        cass_response = [{'data': '{}'}]
+        cass_response = [{'data': '{"type": "helvetica"}'}]
         self.returns = [cass_response, None]
-        d = self.group.update_policy('12345678', {"b": "lah"})
+        d = self.group.update_policy('12345678', {"b": "lah", "type": "helvetica"})
         self.assertIsNone(self.successResultOf(d))  # update returns None
         expectedCql = (
             'BEGIN BATCH INSERT INTO scaling_policies("tenantId", "groupId", "policyId", data) '
             'VALUES (:tenantId, :groupId, :policyId, :policy) APPLY BATCH;')
-        expectedData = {"policy": '{"_ver": 1, "b": "lah"}',
+        expectedData = {"policy": '{"_ver": 1, "b": "lah", "type": "helvetica"}',
                         "groupId": '12345678g',
                         "policyId": '12345678',
                         "tenantId": '11111'}
         self.connection.execute.assert_called_with(
+            expectedCql, expectedData, ConsistencyLevel.TWO)
+
+    def test_update_scaling_policy_type_change(self):
+        """
+        Test that you can update a scaling policy, and if successful it returns
+        None
+        """
+        cass_response = [{'data': '{"type": "blah"}'}]
+        self.returns = [cass_response, None]
+        d = self.group.update_policy('12345678', {"b": "lah", "type": "yarg"})
+        self.assert_deferred_failed(d, ValidationError)
+        expectedCql = ('SELECT data FROM scaling_policies WHERE "tenantId" = :tenantId '
+                       'AND "groupId" = :groupId AND "policyId" = :policyId;')
+        expectedData = {"groupId": '12345678g',
+                        "policyId": '12345678',
+                        "tenantId": '11111'}
+        self.connection.execute.assert_called_once_with(
             expectedCql, expectedData, ConsistencyLevel.TWO)
 
     def test_update_scaling_policy_bad(self):
