5b121b2450ecb0ffbe630a7cdc406087bde39088
Manish Tomar <manish.tomar@rackspace.com>
2013-07-18 15:03:38 -0700

    lock waiting time is random
    If and when multiple server execution try to update the state at same time, the lock is not acquired
    since all wait for same amount of time. So, randomizing the wait time

--- otter/models/cass.py
+++ otter/models/cass.py
@@ -18,6 +18,7 @@ from silverberg.lock import BasicLock, with_lock
 import json
 import iso8601
 from datetime import datetime
+import random
 
 
 LOCK_TABLE_NAME = 'locks'
@@ -402,7 +403,7 @@ class CassScalingGroup(object):
             d.addCallback(lambda state: modifier_callable(self, state, *args, **kwargs))
             return d.addCallback(_write_state)
         lock = BasicLock(self.connection, LOCK_TABLE_NAME, self.uuid,
-                         max_retry=4, retry_wait=5)
+                         max_retry=5, retry_wait=random.uniform(3,5))
         return with_lock(lock, _modify_state)
 
     def update_config(self, data):
