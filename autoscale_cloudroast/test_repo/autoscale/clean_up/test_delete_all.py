916da6aa1d996f4f929f2bf0beb7908097dc5ef3
Lekha Jeevan <lekha.jeevan@rackspace.com>
2013-07-31 14:05:42 -0700

    moved clean-up scripts

--- autoscale_cloudroast/test_repo/autoscale/clean_up/test_delete_all.py
+++ autoscale_cloudroast/test_repo/autoscale/clean_up/test_delete_all.py
@@ -2,10 +2,8 @@
 Deletes.
 """
 from test_repo.autoscale.fixtures import AutoscaleFixture
-import unittest
 
 
-@unittest.skip('for now')
 class DeleteAll(AutoscaleFixture):
 
     """
@@ -23,18 +21,18 @@ class DeleteAll(AutoscaleFixture):
             self.autoscale_client.delete_scaling_group(each_group.id)
         list_groups_response = self.autoscale_client.list_scaling_groups()
         list_groups = list_groups_response.entity
-        self.assertTrue(len(list_groups) is '0',
+        self.assertTrue(len(list_groups) is 0,
                         msg="Groups still exist on the account")
 
     def test_delete_all_servers_on_account(self):
         """
-        Deletes all the servers on the account id
+        Deletes all the servers on the account
         """
         all_servers_response = self.server_client.list_servers()
         all_servers = all_servers_response.entity
         server_id_list = []
-        for each in all_servers:
-            server_id_list.append(each.id)
-        for each in server_id_list:
-            delete_response = self.server_client.delete_server(each)
+        for each_server in all_servers:
+            server_id_list.append(each_server.id)
+        for each_server_id in server_id_list:
+            delete_response = self.server_client.delete_server(each_server_id)
             self.assertEquals(delete_response.status_code, 204)
