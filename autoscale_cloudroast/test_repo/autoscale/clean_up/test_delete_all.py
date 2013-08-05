8626a596c1fd574b8cd0cef313dbfbcb1d25709f
Lekha Jeevan <lekha.jeevan@rackspace.com>
2013-08-04 19:58:24 -0700

    better reporting in cleanup script

--- autoscale_cloudroast/test_repo/autoscale/clean_up/test_delete_all.py
+++ autoscale_cloudroast/test_repo/autoscale/clean_up/test_delete_all.py
@@ -1,6 +1,7 @@
 """
 Deletes.
 """
+from cafe.drivers.unittest.decorators import tags
 from test_repo.autoscale.fixtures import AutoscaleFixture
 
 
@@ -10,6 +11,7 @@ class DeleteAll(AutoscaleFixture):
     Get list of groups/servers on account and delete them
     """
 
+    @tags(type='cleanup')
     def test_delete_all_groups_on_account(self):
         """
         Delete all groups on the account
@@ -19,21 +21,21 @@ class DeleteAll(AutoscaleFixture):
         for each_group in list_groups:
             self.empty_scaling_group(each_group)
             self.autoscale_client.delete_scaling_group(each_group.id)
-        list_groups = (self.autoscale_client.list_scaling_groups()).entity
-        print '{0} groups still exist on the account'.format(len(list_groups))\
-            if len(list_groups) is not 0 else "Deleted {0} groups".format(len(list_groups))
+        list_groups_again = (self.autoscale_client.list_scaling_groups()).entity
+        print 'Deleting {0} groups, {1} still exist'.format(len(list_groups), len(list_groups_again))\
+            if len(list_groups_again) is not 0 else "Deleted {0} groups".format(len(list_groups))
 
+    @tags(type='cleanup', anytag='yes')
     def test_delete_all_servers_on_account(self):
         """
         Deletes all servers on the account
         """
-        all_servers_response = self.server_client.list_servers()
-        all_servers = all_servers_response.entity
+        all_servers = (self.server_client.list_servers()).entity
         server_id_list = []
         for each_server in all_servers:
             server_id_list.append(each_server.id)
         for each_server_id in server_id_list:
-            delete_response = self.server_client.delete_server(each_server_id)
-            print 'Delete server failed with {0}'.format(delete_response.status_code) \
-                if delete_response.status_code is not 204 else "Deleted {0} servers".format(
-                    len(server_id_list))
+            self.server_client.delete_server(each_server_id)
+        list_servers = (self.server_client.list_servers()).entity
+        print 'Deleting {0} servers, {1} still exist'.format(len(all_servers), len(list_servers))\
+            if len(list_servers) is not 0 else "Deleted {0} servers".format(len(all_servers))
