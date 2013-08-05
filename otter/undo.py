2bcabe52aed5af21668fd6f22ea20e9ea7dcd95d
David Reid <dreid@dreid.org>
2013-08-05 11:36:34 -0700

    Initial implementaiton of an in memory undo stack.

---
+++ otter/undo.py
@@ -0,0 +1,50 @@
+from zope.interface import Interface, implementer
+from twisted.internet.defer import maybeDeferred
+
+class IUndoStack(Interface):
+    """
+    A stack for which to record the information needed to undo some operations.
+    """
+
+    def push(f, *args, **kwargs):
+        """
+        Push a function ``f`` onto the undo stack.
+
+        :param f: A function that will be called with the specified ``args`` and
+            ``kwargs`` when the stack is rewound.
+        """
+
+    def rewind():
+        """
+        Rewind the undo stack.
+
+        :rtype: ``Deferred``
+        """
+
+
+@implementer(IUndoStack)
+class InMemoryUndoStack(object):
+    """
+    An Undo Stack that stores the operations in a local queue.
+    """
+    def __init__(self, coiterate):
+        self._ops = []
+        self._coiterate = coiterate
+
+    def push(self, f, *args, **kwargs):
+        """
+        See :py:function:`IUndoStack.push`
+        """
+        self._ops.append((f, args, kwargs))
+
+    def rewind(self):
+        """
+        See :py:function:`IUndoStack.rewind`
+        """
+        ops, self._ops = reversed(self._ops), []
+
+        def run_ops():
+            for (f, args, kwargs) in ops:
+                yield maybeDeferred(f, *args, **kwargs)
+
+        return self._coiterate(run_ops())
