1389f23ef91f77d3cebb8ac84e9057cd23dc36d5
Lekha Jeevan <lekha.jeevan@rackspace.com>
2013-08-12 12:59:44 -0700

    corrected terminology

--- autoscale_cloudroast/test_repo/autoscale/system/integration/test_system_integration_repose_unauthenticated.py
+++ autoscale_cloudroast/test_repo/autoscale/system/integration/test_system_integration_repose_unauthenticated.py
@@ -1,11 +1,7 @@
 """
-System Integration tests autoscaling with repose
+System Integration tests autoscaling with repose when unauthenticated
 """
 from test_repo.autoscale.fixtures import AutoscaleFixture
-from cloudcafe.identity.v2_0.tokens_api.behaviors import \
-    TokenAPI_Behaviors as OSTokenAPI_Behaviors
-from cloudcafe.identity.v2_0.tokens_api.client import \
-    TokenAPI_Client as OSTokenAPI_Client
 from autoscale.client import AutoscalingAPIClient
 from cafe.drivers.unittest.decorators import tags
 
@@ -13,87 +9,79 @@ from cafe.drivers.unittest.decorators import tags
 class AutoscaleReposeUnauthTests(AutoscaleFixture):
 
     """
-    System tests to verify repose integration with autoscale
+    System tests to verify repose integration with autoscale when unauthenticated
     """
 
     @classmethod
     def setUpClass(cls):
         """
-        Create a client for a test account that does not have the autoscale endpoint
-        in its service catalog
+        Create an autoscale api client for requests, unauthenticated
         """
         super(AutoscaleReposeUnauthTests, cls).setUpClass()
-        cls.url = cls.url.replace(cls.tenant_id, cls.non_autoscale_tenant)
-        endpoint = cls.endpoint_config.auth_endpoint
-        token_client = OSTokenAPI_Client(
-            endpoint, 'json', 'json')
-        token_behaviors = OSTokenAPI_Behaviors(token_client)
-        access_data = token_behaviors.get_access_data(cls.non_autoscale_username,
-                                                      cls.non_autoscale_password,
-                                                      cls.non_autoscale_tenant)
         cls.autoscale_temp_client = AutoscalingAPIClient(
-            url=cls.url, auth_token=access_data.token.id_, serialize_format='json',
+            url=cls.url, auth_token=None, serialize_format='json',
             deserialize_format='json')
 
     @tags(type='repose')
     def test_system_repose_unauthenticated_rate_limits_without_trailing_slash(self):
         """
         Verify the relative rate limit api without a trailing slash, when unauthenticated,
-        returns reponse code 403
+        returns reponse code 401
         """
         limits_response = self.autoscale_temp_client.view_limits()
-        self.assertEquals(limits_response.status_code, 403,
-                          msg='Get Limits returned response code {0}'.format(
-                          limits_response.status_code))
+        self.assertEquals(limits_response.status_code, 401,
+                          msg='Get Limits returned response code '
+                          '{0}'.format(limits_response.status_code))
 
     @tags(type='repose')
     def test_system_repose_unauthenticated_rate_limits_with_trailing_slash(self):
         """
         Verify the relative rate limit api with a trailing slash, when unauthenticated,
-        returns reponse code 403
+        returns reponse code 401
         """
         limits_response = self.autoscale_temp_client.view_limits(self.url + '/limits/')
-        self.assertEquals(limits_response.status_code, 403,
-                          msg='Limits returned response code {0}'.format(
-                          limits_response.status_code))
+        self.assertEquals(limits_response.status_code, 401,
+                          msg='Limits returned response code '
+                          '{0}'.format(limits_response.status_code))
 
     @tags(type='repose')
     def test_system_repose_unauthenticated_list_groups_on_account_without_trailing_slash(self):
         """
         Verify list scaling groups for a tenant through repose without a trailing slash,
-        when unauthenticated, returns response code 403
+        when unauthenticated, returns response code 401
         """
         list_response = self.autoscale_temp_client.list_scaling_groups(self.url + '/groups')
-        self.assertEquals(list_response.status_code, 403,
-                          msg='List scaling group returned response code {0}'.format(
-                          list_response.status_code))
+        self.assertEquals(list_response.status_code, 401,
+                          msg='List scaling group returned response code '
+                          '{0}'.format(list_response.status_code))
 
     @tags(type='repose')
     def test_system_repose_unauthenticated_list_groups_on_account_with_trailing_slash(self):
         """
         Verify list scaling groups for a tenant through repose with a trailing slash,
-        when unauthenticated, returns response code 403
+        when unauthenticated, returns response code 401
         """
         list_response = self.autoscale_temp_client.list_scaling_groups(self.url + '/groups/')
-        self.assertEquals(list_response.status_code, 403,
-                          msg='List scaling group returned response code {0}'.format(
-                          list_response.status_code))
+        self.assertEquals(list_response.status_code, 401,
+                          msg='List scaling group returned response code '
+                          '{0}'.format(list_response.status_code))
 
     @tags(type='repose')
     def test_system_repose_unauthenticated_list_groups_on_account_with_non_existant_group(self):
         """
         Verify GET non existing scaling groups through repose without a trailing slash,
-        when unauthenticated, returns response code 403
+        when unauthenticated, returns response code 401
         """
         list_response = self.autoscale_temp_client.list_scaling_groups(self.url + '/groups/76765')
-        self.assertEquals(list_response.status_code, 403,
-                          msg='List scaling group returned response code {0}'.format(
-                          list_response.status_code))
+        self.assertEquals(list_response.status_code, 401,
+                          msg='List scaling group returned response code '
+                          '{0}'.format(list_response.status_code))
 
     @tags(type='repose')
     def test_system_repose_unauthenticated_execute_webhook(self):
         """
-        Verify execute webhook of a tenant by another tenant returns response code 202
+        Verify execute webhook through repose returns response code 202,
+        even when unauthenticated
         """
         group = self.autoscale_behaviors.create_scaling_group_min().entity
         policy = self.autoscale_behaviors.create_policy_webhook(group.id, {'change': 1})
