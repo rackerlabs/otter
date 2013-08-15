a171ad409dc625847c933ff6d7c87f4a0494ecb8
cyli <cyli@twistedmatrix.com>
2013-08-14 19:53:07 -0700

    timeout_deferred in deferredutils, add a retry function and tests
    Conflicts:
    	otter/util/deferredutils.py

---
+++ otter/test/test_deferredutils.py
@@ -0,0 +1,194 @@
+"""
+Tests for `otter.utils.deferredutils`
+"""
+import mock
+
+from twisted.internet.task import Clock
+from twisted.internet.defer import CancelledError, Deferred
+from twisted.trial.unittest import TestCase
+
+from otter.util.deferredutils import timeout_deferred, retry, TransientRetryError
+from otter.test.utils import DummyException
+
+
+class TimeoutDeferredTests(TestCase):
+    """
+    Tests for the method method ``timeout_deferred``
+    """
+    def test_propagates_result_if_success_before_timeout(self):
+        """
+        The deferred callbacks with the result if it succeeds before the
+        timeout (e.g. timing out the deferred does not obscure the callback
+        value).
+        """
+        clock = Clock()
+        d = Deferred()
+        timeout_deferred(d, 10, clock)
+        d.callback("Result")
+        self.assertEqual(self.successResultOf(d), "Result")
+
+        # the timeout never happens - no errback occurs
+        clock.advance(15)
+        self.assertIsNone(self.successResultOf(d))
+
+    def test_propagates_failure_if_failed_before_timeout(self):
+        """
+        The deferred errbacks with the failure if it fails before the
+        timeout (e.g. timing out the deferred does not obscure the errback
+        failure).
+        """
+        clock = Clock()
+        d = Deferred()
+        timeout_deferred(d, 10, clock)
+        d.errback(DummyException("fail"))
+        self.failureResultOf(d, DummyException)
+
+        # the timeout never happens - no further errback occurs
+        clock.advance(15)
+        self.assertIsNone(self.successResultOf(d))
+
+    def test_cancels_if_past_timeout(self):
+        """
+        The deferred errbacks with an CancelledError if the timeout occurs
+        before it either callbacks or errbacks.
+        """
+        clock = Clock()
+        d = Deferred()
+        timeout_deferred(d, 10, clock)
+        self.assertNoResult(d)
+
+        clock.advance(15)
+
+        self.failureResultOf(d, CancelledError)
+
+
+class RetryTests(TestCase):
+    """
+    Tests for the helper method method ``retry``
+    """
+    def setUp(self):
+        """
+        Set up a function to be used for retrying
+        """
+        self.retries = []
+
+        def retry_function():
+            d = Deferred()
+            wrapped = mock.MagicMock(spec=d, wraps=d)
+            self.retries.append(wrapped)
+            return wrapped
+
+        self.clock = Clock()
+        self.interval = 5
+        self.retry_function = retry_function
+
+    def test_propagates_result_and_stops_loop_on_callback(self):
+        """
+        The deferred callbacks with the result as soon as the ``retry_function``
+        succeeds.  Looping also stops.
+        """
+        d = retry(self.retry_function, self.interval, self.clock)
+
+        # no result until the retry_function's deferred fires
+        self.assertNoResult(d)
+        self.assertEqual(len(self.retries), 1)
+
+        self.retries[-1].callback('result!')
+        self.assertEqual(self.successResultOf(d), 'result!')
+
+        # loop is stopped - retry_function not called again.
+        self.clock.advance(self.interval)
+        self.assertEqual(len(self.retries), 1)
+
+    def test_ignores_transient_failures_and_retries(self):
+        """
+        Retries after interval if ``retry_function`` errbacks with a
+        TransientRetryError, and said TransientRetryError is eaten.
+        """
+        d = retry(self.retry_function, self.interval, self.clock)
+
+        self.assertNoResult(d)
+        self.assertEqual(len(self.retries), 1)
+
+        # no result on errback
+        self.retries[-1].errback(TransientRetryError('hey'))
+        self.assertIsNone(self.successResultOf(self.retries[-1]))
+        self.assertNoResult(d)
+
+        self.clock.advance(self.interval)
+
+        # since it was an errback, loop retries the function again
+        self.assertNoResult(d)
+        self.assertEqual(len(self.retries), 2)
+
+        # stop loop
+        self.retries[-1].callback('result!')
+
+    def test_stops_on_non_transient_error(self):
+        """
+        The deferred errbacks with whatever error the ``retry_function``
+        errbacks with, if it is not a :class:`TransientRetryError`.  Looping
+        also stops.
+        """
+        d = retry(self.retry_function, self.interval, self.clock)
+
+        self.assertNoResult(d)
+        self.assertEqual(len(self.retries), 1)
+
+        self.retries[-1].errback(DummyException('fail!'))
+        self.failureResultOf(d, DummyException)
+
+        # loop is stopped - retry_function not called again.
+        self.clock.advance(self.interval)
+        self.assertEqual(len(self.retries), 1)
+
+    def test_cancelling_deferred_cancels_work_in_progress(self):
+        """
+        Cancelling the deferred cancels the deferred returned by
+        ``retry_function`` if it is still in progress, but eats the
+        :class:`CancelledError` (but the overall retry deferred still
+        errbacks with a :class:`CancelledError`)
+        """
+        d = retry(self.retry_function, self.interval, self.clock)
+        self.assertEqual(len(self.retries), 1)
+        self.assertNoResult(self.retries[-1])
+
+        # cancel main deferred
+        d.cancel()
+        self.failureResultOf(d, CancelledError)
+
+        # retry_function's deferred is cancelled, and error eaten
+        self.retries[-1].cancel.assert_called_once_with()
+        self.assertIsNone(self.successResultOf(self.retries[-1]))
+
+    def test_cancelling_deferred_does_not_cancel_completed_work(self):
+        """
+        Cancelling the deferred does not attempt to cancel previously
+        callbacked results from ``retry_function``
+        """
+        d = retry(self.retry_function, self.interval, self.clock)
+
+        self.assertEqual(len(self.retries), 1)
+        self.retries[-1].errback(TransientRetryError('temp'))
+
+        # cancel main deferred
+        d.cancel()
+        self.failureResultOf(d, CancelledError)
+
+        # retry_function's deferred is not cancelled
+        self.assertEqual(self.retries[-1].cancel.call_count, 0)
+        self.assertIsNone(self.successResultOf(self.retries[-1]))
+
+    def test_cancelling_deferred_stops_loop(self):
+        """
+        Cancelling the deferred prevents the loop from retrying the function
+        again.
+        """
+        d = retry(self.retry_function, self.interval, self.clock)
+        self.assertEqual(len(self.retries), 1)
+
+        d.cancel()
+        self.failureResultOf(d, CancelledError)
+
+        self.clock.advance(self.interval)
+        self.assertEqual(len(self.retries), 1)
--- otter/util/deferredutils.py
+++ otter/util/deferredutils.py
@@ -3,6 +3,7 @@ Deferred utilities
 """
 
 from twisted.internet import defer
+from twisted.internet.task import LoopingCall
 
 
 def unwrap_first_error(possible_first_error):
@@ -38,3 +39,87 @@ def ignore_and_log(failure, exception_type, log, msg):
     """
     failure.trap(exception_type)
     log.msg(msg, reason=failure)
+
+
+def timeout_deferred(deferred, timeout, clock):
+    """
+    Time out a deferred - schedule for it to be canceling it after ``timeout``
+    seconds from now, as per the clock.  If it gets cancelled, it errbacks with
+    a :class:`twisted.internet.defer.CancelledError`, unless a cancelable
+    function is passed to the Deferred's initialization and it callbacks or
+    errbacks when cancelled.  (see the documentation for
+    :class:`twisted.internet.defer.Deferred`) for more details.
+
+    :param Deferred deferred: Which deferred to time out (cancel)
+    :param int timeout: How long before timing out the deferred (in seconds)
+
+    from:  https://twistedmatrix.com/trac/ticket/990
+    """
+    delayed_call = clock.callLater(timeout, deferred.cancel)
+
+    def cancelTimeout(result):
+        if delayed_call.active():
+            delayed_call.cancel()
+        return result
+
+    deferred.addBoth(cancelTimeout)
+
+
+class TransientRetryError(Exception):
+    """
+    Transient error that means that retry should continue retrying
+    """
+    def __init__(self, wrapped):
+        wrapped = wrapped
+
+    def __repr__(self):
+        """
+        The ``repr`` of :class:`TransientRetryError` includes the ``repr`` of
+        the wrapped failure
+        """
+        return "Transient error [{0!s}]".format(self.wrapped)
+
+
+def retry(retry_function, interval, clock=None):
+    """
+    Retries a function every ``interval`` until it succeeds or errbacks with
+    something other than :class:`TransientRetryError`
+
+    The ``retry_function`` needs to wrap all transient failures (for which the
+    desired effect is to retry) with a :class:`TransientRetryError`.  On
+    successful callback from ``retry_function ``, the loop will stop.
+    """
+    deferred = defer.Deferred()
+
+    # this is needed to cancel an existing operation if one is in progress
+    operation_d = []
+
+    def pop_operation(anything):
+        operation_d.pop()
+        return anything
+
+    def real_retry_function():
+        retry = retry_function()
+        retry.addBoth(pop_operation)
+        retry.addCallback(deferred.callback)
+        retry.addErrback(lambda f: f.trap(TransientRetryError))
+        retry.addErrback(deferred.errback)
+        operation_d.append(retry)
+        return retry
+
+    lc = LoopingCall(real_retry_function)
+
+    if clock is not None:  # pragma: no cover
+        lc.clock = clock
+
+    def stop_loop(anything):
+        if len(operation_d) > 0:
+            operation_d[0].addErrback(lambda f: f.trap(defer.CancelledError))
+            operation_d[0].cancel()
+        lc.stop()
+        return anything
+
+    deferred.addBoth(stop_loop)
+    lc.start(interval)
+
+    return deferred
