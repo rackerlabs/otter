54149024b33f5e68febca58f3a089b736adfb13f
Phil Kates <me@philkates.com>
2013-08-23 13:36:58 -0700

    Simple runner script to setup a full otter env
    Should probably be moved to the Makefile eventually

---
+++ docker/runner.sh
@@ -0,0 +1,34 @@
+
+# Build cassandra
+docker build -t cassandra docker/cassandra
+
+# Build otter/base
+docker build -t otter/base docker/base
+
+# Build otter
+docker build -t otter .
+
+# Build cloudcafe
+# This sucks because cloudcafe sucks
+rm -rf docker/cloudcafe/autoscale_cloudcafe docker/cloudcafe/autoscale_cloudroast
+cp -r autoscale_cloudcafe docker/cloudcafe
+cp -r autoscale_cloudroast docker/cloudcafe
+docker build -t otter/cloudcafe docker/cloudcafe
+
+# Setup CloudCafe environment variables
+CC_ENVS="-e OTTER_IP=$OTTER_IP"
+CC_ENVS="$CC_ENVS -e CC_USER_PASSWORD=$CC_USER_PASSWORD"
+CC_ENVS="$CC_ENVS -e CC_USER_API_KEY=$CC_USER_API_KEY"
+CC_ENVS="$CC_ENVS -e CC_NON_AS_PASSWORD=$CC_NON_AS_PASSWORD"
+
+# Run Test containers
+CASSANDRA_CID=$(docker run -d -t -h cassandra -e MAX_HEAP_SIZE=512M -e HEAP_NEWSIZE=256M cassandra)
+CASSANDRA_IP=$(docker inspect $CASSANDRA_CID | grep IPAddress | cut -d '"' -f 4)
+OTTER_CID=$(docker run -d -t -e CASSANDRA_HOST=$CASSANDRA_IP -e CASSANDRA_IP=$CASSANDRA_IP otter /bin/bash)
+OTTER_IP=$(docker inspect $OTTER_CID | grep IPAddress | cut -d '"' -f 4)
+
+sed -i "s/%user_password%/$CC_USER_PASSWORD/g" $CC_CONFIG
+sed -i "s/%user_api_key%/$CC_USER_API_KEY/g" $CC_CONFIG
+sed -i "s/%non_autoscale_user_password%/$CC_NON_AS_PASSWORD/g" $CC_CONFIG
+sed -i "s/%otter_ip%/$OTTER_IP/g" $CC_CONFIG
+
