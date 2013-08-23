84bd543c59732af2c44051342f27e10c710b0764
Phil Kates <me@philkates.com>
2013-08-23 13:37:54 -0700

    Remove sed notes from runner

--- docker/runner.sh
+++ docker/runner.sh
@@ -26,9 +26,3 @@ CASSANDRA_CID=$(docker run -d -t -h cassandra -e MAX_HEAP_SIZE=512M -e HEAP_NEWS
 CASSANDRA_IP=$(docker inspect $CASSANDRA_CID | grep IPAddress | cut -d '"' -f 4)
 OTTER_CID=$(docker run -d -t -e CASSANDRA_HOST=$CASSANDRA_IP -e CASSANDRA_IP=$CASSANDRA_IP otter /bin/bash)
 OTTER_IP=$(docker inspect $OTTER_CID | grep IPAddress | cut -d '"' -f 4)
-
-sed -i "s/%user_password%/$CC_USER_PASSWORD/g" $CC_CONFIG
-sed -i "s/%user_api_key%/$CC_USER_API_KEY/g" $CC_CONFIG
-sed -i "s/%non_autoscale_user_password%/$CC_NON_AS_PASSWORD/g" $CC_CONFIG
-sed -i "s/%otter_ip%/$OTTER_IP/g" $CC_CONFIG
-
