7215b2063620de79923e095369e7f00c8fec916d
Trevor Bramwell <trevor.bramwell@rackspace.com>
2013-08-26 09:40:53 -0700

    Remove 'root' application and prefix routes.

--- otter/rest/application.py
+++ otter/rest/application.py
@@ -20,15 +20,13 @@ class Otter(BaseApp):
     """
     Otter holds the Klein app and routes for the REST service.
     """
-    root = Klein()
     app = Klein()
 
     def __init__(self, store=None, *args, **kwargs):
-        self.root.route = partial(self.root.route, strict_slashes=False)
         self.app.route = partial(self.app.route, strict_slashes=False)
         super(Otter, self).__init__(store, *args, **kwargs)
 
-    @root.route('/')
+    @app.route('/')
     def base(self, request):
         """
         base root route.
@@ -37,39 +35,30 @@ class Otter(BaseApp):
         """
         return ''
 
-    @root.route('/v1.0', branch=True)
-    def v1_0(self, request):
-        """
-        Version 1.0 root route
-
-        :returns: All requests handled by app, not root.
-        """
-        return self.app.resource()
-
-    @app.route('/<string:tenant_id>/groups', branch=True)
+    @app.route('/v1.0/<string:tenant_id>/groups', branch=True)
     def groups(self, request, tenant_id):
         """
         /v1.0/<tenantId>/groups and subroutes delegated to OtterGroups.
         """
         return OtterGroups(tenant_id, self.store).app.resource()
 
-    @app.route('/<string:tenant_id>/groups/<string:group_id>/config')
+    @app.route('/v1.0/<string:tenant_id>/groups/<string:group_id>/config')
     def config(self, request, tenant_id, group_id):
         return OtterConfig(tenant_id, group_id, self.store).app.resource()
 
-    @app.route('/<string:tenant_id>/groups/<string:group_id>/launch')
+    @app.route('/v1.0/<string:tenant_id>/groups/<string:group_id>/launch')
     def launch(self, request, tenant_id, group_id):
         return OtterLaunch(tenant_id, group_id, self.store).app.resource()
 
-    @app.route('/<string:tenant_id>/groups/<string:group_id>/policies', branch=True)
+    @app.route('/v1.0/<string:tenant_id>/groups/<string:group_id>/policies', branch=True)
     def policies(self, request, tenant_id, group_id):
         return OtterPolicies(tenant_id, group_id, self.store).app.resource()
 
-    @app.route('/<string:tenant_id>/groups/<string:group_id>/webhooks', branch=True)
+    @app.route('/v1.0/<string:tenant_id>/groups/<string:group_id>/webhooks', branch=True)
     def webhooks(self, request, tenant_id, group_id):
         return OtterWebhooks(tenant_id, group_id, self.store).app.resource()
 
-    @app.route('/execute/<string:capability_version>/<string:capability_hash>/')
+    @app.route('/v1.0/execute/<string:capability_version>/<string:capability_hash>/')
     def execute(self, request, capability_version, capability_hash):
         """
         """
