6a28d429b5344b897175dfe008127e54aaaafb16
Phil Kates <me@philkates.com>
2013-08-30 12:42:50 -0700

    Move cloudcafe into it's own run script
    This allows us to update the IP at runtime meaning we can use one
    container for both otter and cloudcafe, simplifying things and stuff

--- docker/runner.sh
+++ docker/runner.sh
@@ -63,14 +63,11 @@ UNIT_TESTS=$(docker run -d -t $OTTER_ENVS -e JENKINS_URL=subunit otter:$GIT_SHA 
 OTTER_CID=$(docker run -d -t $OTTER_ENVS otter:$GIT_SHA)
 export OTTER_IP=$(docker inspect $OTTER_CID | grep IPAddress | cut -d '"' -f 4)
 
-CC_ENV="OTTER_IP=$OTTER_IP"
-CC_ENV="$CC_ENV -e PYTHONPATH=/opt/otter/autoscale_cloudcafe:/opt/otter/autoscale_cloudroast"
-
-CC_RUN="cafe-runner autoscale /opt/otter/docker/cloudcafe/preprod.config"
+CC_RUN="./docker/cloudcafe/run_cloudcafe.sh autoscale /opt/otter/docker/cloudcafe/preprod.config"
 
 # Run CloudCafe tests
-CC_FUNCTIONAL_TESTS=$(docker run -d -t -e $CC_ENV otter:$GIT_SHA $CC_RUN -p functional)
-CC_QUICKSYS_TESTS=$(docker run -d -t -e OTTER_IP=$OTTER_IP otter:$GIT_SHA $CC_RUN -p system -t speed=quick)
+CC_FUNCTIONAL_TESTS=$(docker run -d -t -e PYTHONPATH=$PYTHONPATH -e OTTER_IP=$OTTER_IP otter:$GIT_SHA $CC_RUN -p functional)
+CC_QUICKSYS_TESTS=$(docker run -d -t -e PYTHONPATH=$PYTHONPATH -e OTTER_IP=$OTTER_IP otter:$GIT_SHA $CC_RUN -p system -t speed=quick)
 # CC_SLOWSYS_TESTS=$(docker run -d -t -e OTTER_IP=$OTTER_IP otter/cloudcafe:$GIT_SHA -p system -t speed=slow)
 
 UNIT_EXIT=$(docker wait $UNIT_TESTS)
