15a127636f42af094b1dc3bee266a9303204888d
cyli <cyli@twistedmatrix.com>
2013-09-10 13:50:30 -0700

    Make more general so that can be used for dev or testing

--- otterdock.py
+++ otterdock.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 from sys import exit
-from shipper import *
+from shipper import Shipper, command, run
 import StringIO
 
 # Notice the replacements that happen for pip install commands
 # This allows quick docker builds and makes sure reqs stay in sync
-DOCKERFILE_DEV = """FROM ubuntu:12.04
+DOCKERFILE = """FROM ubuntu:12.04
 
 # CloudCafe
 ENV SUDO_USER root
 ENV HOME /root
 
-# Use our PyPi mirror
-ENV PIP_INDEX_URL http://pypi0.prod.ord.as.rax.io:3145/pypi
+{pip_mirror}
 
 # The build process isn't a real tty
 ENV DEBIAN_FRONTEND noninteractive
@@ -23,12 +24,12 @@ RUN echo "deb http://mirror.rackspace.com/ubuntu/ precise main restricted univer
 RUN echo "deb http://mirror.rackspace.com/ubuntu/ precise-updates main restricted universe" >> /etc/apt/sources.list
 RUN apt-get update
 RUN apt-get install -y build-essential git python-dev libxml2-dev libxslt1-dev python-pip python-virtualenv
-# Dev requirements
-RUN pip install {0}
 # requirements
-RUN pip install {1}
+RUN pip install {reqs}
+# Dev requirements
+RUN pip install {devreqs}
 
-VOLUME ["/opt/otter"]
+{otter}
 WORKDIR /opt/otter
 
 # Expose the otter port and set a default command
@@ -36,78 +37,127 @@ EXPOSE 9000
 CMD ["make", "run_docker"]
 """
 
-PYTHONPATH = ":".join([
+PYTHONPATHS = [
     '/opt/otter',
     '/opt/otter/autoscale_cloudcafe',
     '/opt/otter/autoscale_cloudroast'
-])
+]
+
+
+s = Shipper()
+
+
+def get_dockerfile(dev=True, otter_logs=None,
+                   pip_mirror='http://pypi0.prod.ord.as.rax.io:3145/pypi'):
+    """
+    Formats base Dockerfile string with values, and returns a file handle
+    whose contents are the formatted Dockerfile.
+    """
+    kwargs = {}
+    with open('requirements.txt') as f:
+        kwargs['reqs'] = " ".join([line.strip() for line in f.readlines()])
+    with open('dev_requirements.txt') as f:
+        kwargs['devreqs'] = " ".join([line.strip() for line in f.readlines()])
+
+    if dev:
+        kwargs['otter'] = 'VOLUME ["/opt/otter"]'
+    else:
+        kwargs['otter'] = 'ADD . /opt/otter'
+
+    if pip_mirror is not None:
+        kwargs['pip_mirror'] = (
+            '# Use our PyPi mirror\n'
+            'ENV PIP_INDEX_URL {0}'.format(pip_mirror))
+    else:
+        pip_mirror = ''
+
+    return StringIO.StringIO(DOCKERFILE.format(**kwargs))
+
 
 # Not required in latest docker-py master
 def format_docker_env(env_dict):
     return ['{0}={1}'.format(k, v) for k, v in env_dict.items()]
 
+
 @command
-def start():
-    s = Shipper()
+def start_cassandra(run_tag="dev"):
+    """
+    Starts Cassandra if it hasn't been started with that tag already and
+    returns the running container's IP
+    """
     cass = s.run(
         image='cassandra',
-        command='/opt/cassandra/bin/cassandra -f',
+        command='RUN_REASON={0} /opt/cassandra/bin/cassandra -f'.format(
+            run_tag),
         hostname='cassandra',
-        once=True
-    )
-    cass_ip = s.inspect(cass[0])[0]['NetworkSettings']['IPAddress']
-    otter_env = format_docker_env({
-        'CASSANDRA_HOST': cass_ip,
-        'PYTHONPATH': PYTHONPATH
-    })
-    otter = s.run(
-        image='otter:dev',
-        command='make run_docker',
-        environment=otter_env,
-        volumes=['/mnt/shared/otter:/opt/otter'],
-        once=True
-    )
-    print(otter)
+        once=True)
+    if len(cass) == 0:
+        print("Cassandra failed to start")
+        exit(1)
+
+    return s.inspect(cass[0])[0]['NetworkSettings']['IPAddress']
+
 
 @command
 def build():
-    with open('requirements.txt') as f:
-        reqs = " ".join([line.strip() for line in f.readlines()])
-    with open('dev_requirements.txt') as f:
-        devreqs = " ".join([line.strip() for line in f.readlines()])
-
-    dockerfile = StringIO.StringIO(DOCKERFILE_DEV.format(devreqs, reqs))
-    s = Shipper()
     s.build(
         tag='otter:dev',
-        fobj=dockerfile
+        fobj=get_dockerfile()
     )
 
+
 @command
-def run_tests():
-    s = Shipper()
-    cass = s.run(
-        image='cassandra',
-        command='/opt/cassandra/bin/cassandra -f',
-        hostname='cassandra',
-        once=True
-    )
-    try:
-        cass = cass[0]
-    except IndexError:
-        print("Cassandra failed to start")
-        exit(1)
-    cass_ip = s.inspect(cass)[0]['NetworkSettings']['IPAddress']
+def run_otter(command, run_tag="dev", pythonpaths=None, volumes=None):
+    cass_ip = start_cassandra(run_tag)
+
+    if pythonpaths is None:
+        pythonpaths = PYTHONPATHS
+    else:
+        pythonpaths = PYTHONPATHS + pythonpaths
+
     otter_env = format_docker_env({
         'CASSANDRA_HOST': cass_ip,
-        'PYTHONPATH': PYTHONPATH
+        'PYTHONPATH': ":".join(pythonpaths)
     })
-    s.run(
+
+    if run_tag == "dev":
+        if volumes is None:
+            volumes = ['/mnt/shared/otter:/opt/otter']
+        else:
+            volumes.append('/mnt/shared/otter:/opt/otter')
+
+    container_id = s.run(
         image='otter:dev',
-        command='make test',
+        command=command,
         environment=otter_env,
-        volumes=['/mnt/shared/otter:/opt/otter'],
+        volumes=volumes,
         once=True
     )
+
+    if len(container_id) == 0:
+        print("Otter failed to start")
+        exit(1)
+
+    return container_id[0]
+
+
+@command
+def start(run_tag="dev"):
+    container_id = run_otter('make run_otter', run_tag)
+
+
+@command
+def unit_tests(run_tag="dev"):
+    container_id = run_otter('make unit', run_tag)
+
+
+@command
+def run_tests(run_tag="dev"):
+    container_id = run_otter('make test', run_tag)
+
+
+@command
+def ps(all=False, running=True):
+    print(s.containers(pretty=True, all=all, running=running))
 
 run()
