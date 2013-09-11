5ac325171307a5d866ba9efcdfde0858cb30ef35
Lekha Jeevan <lekha.jeevan@rackspace.com>
2013-09-11 10:51:26 -0700

    negative test for when imageid or flavor id is not provided

--- autoscale_cloudroast/test_repo/autoscale/functional/launch_config/test_launch_config_negative.py
+++ autoscale_cloudroast/test_repo/autoscale/functional/launch_config/test_launch_config_negative.py
@@ -16,22 +16,19 @@ class LaunchConfigNegtaiveTest(AutoscaleFixture):
         Create a scaling group.
         """
         super(LaunchConfigNegtaiveTest, self).setUp()
-        group_response = self.autoscale_behaviors.create_scaling_group_min()
-        self.group = group_response.entity
-        self.resources.add(self.group.id,
-                           self.autoscale_client.delete_scaling_group)
         self.lc_name = rand_name('negative_launch_config')
-        self.invalid_flavor_ids = ['INVALID-FLAVOR-ID', 8888, 1, -4]
-        self.invalid_image_ids = ['INVALID-IMAGE-ID', 1111, self.lc_image_ref + 'Z']
+        self.invalid_flavor_ids = ['INVALID-FLAVOR-ID', 8888, 1, -4, None]
+        self.invalid_image_ids = ['INVALID-IMAGE-ID', 1111, self.lc_image_ref + 'Z', None]
 
     def test_update_scaling_group_launch_config_to_invalid_imageid(self):
         """
         Verify update luanch config fails with a 400 when the new launch config
         has an invalid imageId.
         """
+        group = self._create_group()
         for each_invalid_id in self.invalid_image_ids:
             update_launch_config_response = self.autoscale_client.update_launch_config(
-                group_id=self.group.id,
+                group_id=group.id,
                 name=self.lc_name,
                 image_ref=each_invalid_id,
                 flavor_ref=self.lc_flavor_ref)
@@ -44,9 +41,10 @@ class LaunchConfigNegtaiveTest(AutoscaleFixture):
         Verify update luanch config fails with a 400 when the new launch config
         has an invalid flavorId.
         """
+        group = self._create_group()
         for each_invalid_id in self.invalid_flavor_ids:
             update_launch_config_response = self.autoscale_client.update_launch_config(
-                group_id=self.group.id,
+                group_id=group.id,
                 name=self.lc_name,
                 image_ref=self.lc_image_ref,
                 flavor_ref=each_invalid_id)
@@ -60,9 +58,13 @@ class LaunchConfigNegtaiveTest(AutoscaleFixture):
         has an invalid imageId.
         """
         for each_invalid_id in self.invalid_image_ids:
-            create_group_response = self.autoscale_behaviors.create_scaling_group_given(
+            create_group_response = self.autoscale_client.create_scaling_group(
+                gc_name='test',
+                gc_cooldown=self.gc_cooldown,
                 gc_min_entities=self.gc_min_entities,
-                lc_image_ref=each_invalid_id)
+                lc_name=self.lc_name,
+                lc_image_ref=each_invalid_id,
+                lc_flavor_ref=self.lc_flavor_ref)
             self.assertEquals(create_group_response.status_code, 400,
                               msg='Create group with invalid server image id was successsful with'
                               ' response {0}'.format(create_group_response.status_code))
@@ -73,9 +75,22 @@ class LaunchConfigNegtaiveTest(AutoscaleFixture):
         has an invalid flavorid.
         """
         for each_invalid_id in self.invalid_flavor_ids:
-            create_group_response = self.autoscale_behaviors.create_scaling_group_given(
+            create_group_response = self.autoscale_client.create_scaling_group(
+                gc_name='test',
+                gc_cooldown=self.gc_cooldown,
                 gc_min_entities=self.gc_min_entities,
+                lc_name=self.lc_name,
+                lc_image_ref=self.lc_image_ref,
                 lc_flavor_ref=each_invalid_id)
             self.assertEquals(create_group_response.status_code, 400,
                               msg='Create group with invalid server image id was successsful with'
                               ' response {0}'.format(create_group_response.status_code))
+
+    def _create_group(self):
+        """
+        Create a group
+        """
+        group_response = self.autoscale_behaviors.create_scaling_group_min()
+        group = group_response.entity
+        self.resources.add(group, self.empty_scaling_group)
+        return group
