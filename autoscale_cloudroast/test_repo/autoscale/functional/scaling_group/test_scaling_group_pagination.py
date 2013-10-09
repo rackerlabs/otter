4301469a6d8730ce5d280843de8e8ebfee9ad554
Lekha Jeevan <lekha.jeevan@rackspace.com>
2013-10-09 12:54:21 -0700

    changes assert_limits and tests for above or below set imits

--- autoscale_cloudroast/test_repo/autoscale/functional/scaling_group/test_scaling_group_pagination.py
+++ autoscale_cloudroast/test_repo/autoscale/functional/scaling_group/test_scaling_group_pagination.py
@@ -22,6 +22,13 @@ class GroupPaginationTest(AutoscaleFixture):
         self.total_groups = len(
             (self.autoscale_client.list_scaling_groups().entity).groups)
 
+    def tearDown(self):
+        """
+        Delete the scaling groups.
+        """
+        super(GroupPaginationTest, self).tearDown()
+        self.resources.release()
+
     def test_list_groups_when_list_groups_is_greater_than_the_limit(self):
         """
         List the scaling groups without limit when over 100 groups exist on the group
@@ -29,10 +36,10 @@ class GroupPaginationTest(AutoscaleFixture):
         """
         self._create_multiple_groups(self.pagination_limit)
         list_groups = self._list_group_with_given_limit(None)
-        self.assertEquals(len(list_groups.groups), self.pagination_limit)
+        self._assert_list_groups_with_limits_and_next_link(1, list_groups)
         rem_list_group = self.autoscale_client.list_scaling_groups(
             list_groups.groups_links.next).entity
-        self._assert_list_groups_with_limits(1, rem_list_group)
+        self._assert_list_groups_with_limits_and_next_link(1, rem_list_group, False)
 
     def test_list_groups_with_limit_less_than_number_of_groups(self):
         """
@@ -42,12 +49,10 @@ class GroupPaginationTest(AutoscaleFixture):
         """
         param = self.total_groups - 1
         list_group = self._list_group_with_given_limit(param)
-        self.assertEquals(len(list_group.groups), param,
-                          msg='The length of the list when limited to {0} was {1} '
-                          'instead'.format(param, len(list_group.groups)))
+        self._assert_list_groups_with_limits_and_next_link(param, list_group)
         rem_list_group = self.autoscale_client.list_scaling_groups(
             list_group.groups_links.next).entity
-        self._assert_list_groups_with_limits(1, rem_list_group)
+        self._assert_list_groups_with_limits_and_next_link(1, rem_list_group, False)
 
     @unittest.skip('AUTO-700')
     def test_list_groups_with_limit_equal_to_number_of_groups(self):
@@ -58,7 +63,7 @@ class GroupPaginationTest(AutoscaleFixture):
         """
         param = self.total_groups
         list_groups = self._list_group_with_given_limit(param)
-        self._assert_list_groups_with_limits(param, list_groups)
+        self._assert_list_groups_with_limits_and_next_link(param, list_groups)
 
     def test_list_groups_with_limit_greater_than_number_of_groups(self):
         """
@@ -68,16 +73,35 @@ class GroupPaginationTest(AutoscaleFixture):
         """
         param = self.total_groups + 2
         list_groups = self._list_group_with_given_limit(param)
-        self._assert_list_groups_with_limits(self.total_groups, list_groups)
+        self._assert_list_groups_with_limits_and_next_link(self.total_groups, list_groups, False)
 
-    @unittest.skip('PR-398')
     def test_list_groups_with_invalid_limits(self):
         """
         List scaling groups with limit set to invalid values and verify the message returned
         """
-        params = [0, -1, 'ab', 10000]
-        for each in params:
-            self._list_group_with_given_limit(params, 400)
+        params = ['ab', '&']
+        for each_param in params:
+            self._list_group_with_given_limit(each_param, 400)
+
+    def test_list_groups_with_limits_below_set_limit(self):
+        """
+        Verify that when the limit is below the set limit(1), one group is
+        listed.
+        """
+        params = [0, -1]
+        for each_param in params:
+            list_groups = self._list_group_with_given_limit(each_param, 200)
+            self._assert_list_groups_with_limits_and_next_link(1, list_groups)
+
+    def test_list_groups_with_limits_above_set_limit(self):
+        """
+        Verify when the limit is over the set limit(100), all groups upto a 100
+        are returned
+        """
+        params = [101, 1000]
+        for each_param in params:
+            list_groups = self._list_group_with_given_limit(each_param, 200)
+            self._assert_list_groups_with_limits_and_next_link(self.total_groups, list_groups, False)
 
     def test_list_groups_with_marker(self):
         """
@@ -104,18 +128,23 @@ class GroupPaginationTest(AutoscaleFixture):
         """
         Lists groups with given limit and verifies they are successful
         """
-        groups_response = self.autoscale_client.list_scaling_groups(limit=param)
+        groups_response = self.autoscale_client.list_scaling_groups(
+            limit=param)
         self.assertEquals(groups_response.status_code, response, msg='list group failed'
                           ' with {0}'.format(groups_response.status_code))
         return groups_response.entity
 
-    def _assert_list_groups_with_limits(self, group_len, list_group):
+    def _assert_list_groups_with_limits_and_next_link(self, group_len, list_group, next_link=True):
         """
         Asserts the length of the list group returned and its groups links.
+        If next_link is False, asserts that the group_links is empty and does not have a next link
         """
         self.assertGreaterEqual(len(list_group.groups), group_len)
-        self.assertDictEqual(list_group.groups_links.links, {}, msg='Links to next provided'
-                             ' even when there are no more groups to list')
+        if next_link:
+            self.assertTrue(list_group.groups_links.next)
+        else:
+            self.assertDictEqual(list_group.groups_links.links, {}, msg='Links to next provided'
+                                 ' even when there are no more groups to list')
 
     def _create_multiple_groups(self, num):
         """
@@ -124,5 +153,4 @@ class GroupPaginationTest(AutoscaleFixture):
         for _ in range(num):
             group_response = self.autoscale_behaviors.create_scaling_group_min()
             self.group = group_response.entity
-            self.resources.add(
-                self.group.id, self.autoscale_client.delete_scaling_group)
+            self.resources.add(self.group.id, self.autoscale_client.delete_scaling_group)
