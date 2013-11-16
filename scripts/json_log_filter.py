c54c75416837fe27ad0e88b58faf2bf504c2a9ab
cyli <cyli@twistedmatrix.com>
2013-11-15 16:19:11 -0800

    lint fix

--- scripts/json_log_filter.py
+++ scripts/json_log_filter.py
@@ -1,6 +1,15 @@
 """
 Script that tails an otter log and filters out only JSON events that contain
 a particular key
+
+Usage:
+python json_log_filter.py <file to tail> [--key=<some json field name>]
+
+Works like tail, but will only emit the json event logs that have the key
+<some json field name>.  By default, only emits logs that have the key
+"audit_log"
+
+Works with otter logs whether they're debug or not.
 """
 
 from __future__ import print_function
@@ -20,7 +29,7 @@ endpoints._ProcessEndpointTransport.disconnecting = False
 
 class JsonEventReceiver(LineReceiver):
     """
-    Listens to a file, reads json events, and filters out the audit log
+    Listens to a file, reads json events, and filters out the audit log.
     """
     delimiter = '\n'
 
@@ -29,21 +38,37 @@ class JsonEventReceiver(LineReceiver):
         self.key = key
 
     def connectionMade(self):
+        """
+        When the tail process is openned
+        """
         print("Filtering for events containing '{key}'".format(key=self.key))
 
     def connectionLost(self, reason):
+        """
+        Print a newline, otherwise ignore.
+        """
         print("\n")
         reason.trap(ConnectionDone, ConnectionLost)
 
     def emit(self, data):
+        """
+        Actually just print
+        """
         print(data)
 
     def json_filter(self, event_str):
+        """
+        Loads a string as JSON - if it works, only return it if it has the key
+        """
         event = json.loads(event_str)
         if self.key in event:
             return event
 
     def lineReceived(self, line):
+        """
+        Processes log lines - they can either be all on one line, or split
+        across multiple lines as is produced by observer_factory_debug
+        """
         results = line.split(" ", 1)
 
         if len(results) < 2:
@@ -78,7 +103,10 @@ class JsonEventReceiver(LineReceiver):
 
 
 def parse_args():
-    parser = argparse.ArgumentParser('audit log filter')
+    """
+    Parse command line arguments for filename and key to look for
+    """
+    parser = argparse.ArgumentParser("json_log_filter.py")
     parser.add_argument('filepath', help="path to the file to tail")
     parser.add_argument('--key', help="json key to filter on", required=False,
                         default='audit_log')
@@ -86,6 +114,9 @@ def parse_args():
 
 
 def connect_to_tail(reactor, filepath, key):
+    """
+    Spawns "tail" process and hooks up the protocol
+    """
     endpoint = endpoints.ProcessEndpoint(reactor, 'tail',
                                          args=('tail', '-f', filepath))
     protocol = JsonEventReceiver(key)
