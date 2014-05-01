504b98679749cd8235caf4b433920ab5f3c8c585
cyli <cyli@twistedmatrix.com>
2014-04-30 21:53:23 -0700

    Shorten the query, and make it return audit log in reverse chronological order (newest first)

--- otter/rest/history.py
+++ otter/rest/history.py
@@ -17,78 +17,41 @@ from otter.util.http import (
     append_segments, check_success, get_collection_links, next_marker_by_offset)
 
 
-_ELASTICSEARCH_QUERY_TEMPLATE = {
-    "from": None,
-    "size": None,
-    "query": {
-        "filtered": {
-            "filter": {
-                "bool": {
-                    "must": [{
-                        "range": {
-                            "@timestamp": {
-                                "from": "now-30d",
-                                "to": "now"
-                            }
-                        }
-                    }, {
-                        "fquery": {
-                            "query": {
-                                "field": {
-                                    "audit_log": {
-                                        "query": True
-                                    }
-                                }
-                            }
-                        }
-                    }]
-                }
-            }
-        }
-    }
-}
-_TENANT_ID_TEMPLATE = {
-    "fquery": {
-        "query": {
-            "field": {
-                "tenant_id": {
-                    "query": None
-                }
-            }
-        }
-    }
-}
-_REGION_TEMPLATE = {
-    "fquery": {
-        "query": {
-            "field": {
-                "tags": {
-                    "query": None
-                }
-            }
-        }
-    }
-}
-
-
 def make_auditlog_query(tenant_id, region, marker=0, limit=0):
     """Make an elastic search query to fetch audit logs."""
-    query = copy.deepcopy(_ELASTICSEARCH_QUERY_TEMPLATE)
 
-    # Add the tenant id query
-    tenant_query = copy.deepcopy(_TENANT_ID_TEMPLATE)
-    tenant_query['fquery']['query']['field']['tenant_id']['query'] = tenant_id
-    query['query']['filtered']['filter']['bool']['must'].append(tenant_query)
-
-    # Add the region query
-    region_query = copy.deepcopy(_REGION_TEMPLATE)
-    region_query['fquery']['query']['field']['tags']['query'] = region.lower()
-    query['query']['filtered']['filter']['bool']['must'].append(region_query)
-
-    query['from'] = marker
-    query['size'] = limit
-
-    return query
+    return {
+        "from": marker,
+        "size": limit,
+        "query": {
+            "filtered": {
+                "filter": {
+                    "bool": {
+                        "must": [
+                            {
+                                "range": {
+                                    "@timestamp": {
+                                        "from": "now-30d",
+                                        "to": "now"
+                                    }
+                                }
+                            },
+                            {
+                                "term": {"audit_log": True}
+                            },
+                            {
+                                "term": {"tenant_id": tenant_id.strip()}
+                            },
+                            {
+                                "term": {"tags": region.lower().strip()}
+                            }
+                        ]
+                    }
+                }
+            }
+        },
+        "sort": [{"@timestamp": {'order': 'desc'}}]
+    }
 
 
 class OtterHistory(object):
@@ -133,13 +96,14 @@ class OtterHistory(object):
                     pass
 
             for hit in body['hits']['hits']:
-                fields = hit['_source']
-                event = {'timestamp': fields['@timestamp']}
-                for name in AUDIT_LOG_FIELDS.keys():
-                    field = fields.get(name)
-                    if field is not None:
-                        event[name] = field
-                events.append(event)
+                events.append(hit['_source'])
+                # fields = hit['_source']
+                # event = {'timestamp': fields['@timestamp']}
+                # for name in AUDIT_LOG_FIELDS.keys():
+                #     field = fields.get(name)
+                #     if field is not None:
+                #         event[name] = field
+                # events.append(event)
             links = get_collection_links(
                 events, request.uri, 'self', limit=paginate.get('limit'),
                 marker=paginate.get('marker'), next_marker=next_marker_by_offset)
