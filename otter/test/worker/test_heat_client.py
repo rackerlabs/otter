e1a08ddedb465c1862a4ec972a9f517767006c32
Christopher Armstrong <chris.armstrong@rackspace.com>
2014-05-09 18:10:41 -0700

    a stab at a reauthenticating HTTP client
    very poorly tested so far - just made the existing Heat client tests
    pass. Need to explicitly test the reauthentication support.

--- otter/test/worker/test_heat_client.py
+++ otter/test/worker/test_heat_client.py
@@ -6,50 +6,50 @@ import mock
 from twisted.trial.unittest import TestCase
 
 from otter.util.http import APIError
-from otter.worker.heat_client import HeatClient
-from otter.test.utils import mock_treq, mock_log, patch
+from otter.worker.heat_client import HeatClient, OpenStackClient
+from otter.test.utils import stub_treq, StubLog
 
 
 class HeatClientTests(TestCase):
     """Tests for HeatClient."""
 
-    def _assert_bound(self, orig_log, log, **kwargs):
-        # Invoke a log message on the mocked log object so that we can check
-        # which keys were bound to the log...
-        log.msg()
-        orig_log.msg.assert_called_once_with(**kwargs)
+    def _assert_bound(self, log, **kwargs):
+        self.assertEqual(log.binds, kwargs)
 
-    def _treq(self, **kwargs):
-        treq = mock_treq(**kwargs)
-        patch(self, 'otter.util.http.treq', new=treq)
-        return treq
+    def _http_client(self, code, json_content):
+        treq = stub_treq(code, json.dumps(json_content))
+        return OpenStackClient(treq, lambda: None, auth_token='my-auth-token')
 
     def test_create_stack(self):
         """
         create_stack POSTs data to the stack creation endpoint and returns
         the parsed JSON result.
         """
-        log = mock_log()
-        treq = self._treq(code=201, method='post',
-                          json_content={'hello': 'world'})
-        client = HeatClient('my-auth-token', log, treq)
+        log = StubLog()
+        http = self._http_client(code=201,
+                                 json_content={'hello': 'world'})
+        treq = http.treq
+        client = HeatClient(log, http)
         result = client.create_stack(
             'http://heat-url/', 'my-stack-name', {'p1': 'v1'}, 60,
             'my template')
-        treq.post.assert_called_once_with(
-            'http://heat-url/stacks',
-            headers={'x-auth-token': ['my-auth-token'],
-                     'content-type': ['application/json'],
-                     'accept': ['application/json'],
-                     'User-Agent': ['OtterScale/0.0']},
-            data=json.dumps({'stack_name': 'my-stack-name',
-                             'parameters': {'p1': 'v1'},
-                             'timeout_mins': 60,
-                             'template': 'my template'}),
-            log=mock.ANY)
+
+        self.assertEqual(
+            http.treq.requests,
+            [{'method': 'post',
+              'url': 'http://heat-url/stacks',
+              'headers': {'x-auth-token': ['my-auth-token'],
+                          'content-type': ['application/json'],
+                          'accept': ['application/json'],
+                          'User-Agent': ['OtterScale/0.0']},
+              'data': json.dumps({'stack_name': 'my-stack-name',
+                                  'parameters': {'p1': 'v1'},
+                                  'timeout_mins': 60,
+                                  'template': 'my template'}),
+              'log': mock.ANY}])
 
         self.assertEqual(self.successResultOf(result), {'hello': 'world'})
-        self._assert_bound(log, treq.post.mock_calls[-1][2]['log'],
+        self._assert_bound(http.treq.requests[-1]['log'],
                            system='heatclient', event='create-stack',
                            stack_name='my-stack-name')
 
@@ -57,10 +57,10 @@ class HeatClientTests(TestCase):
         """
         On any result other than 201, create_stack raises an exception.
         """
-        log = mock_log()
-        treq = self._treq(code=404, method='post',
-                          json_content={'hello': 'world'})
-        client = HeatClient('my-auth-token', log, treq)
+        log = StubLog()
+        http = self._http_client(code=404,
+                                 json_content={'hello': 'world'})
+        client = HeatClient(log, http)
         result = client.create_stack(
             'http://heat-url/', 'my-stack-name', {'p1': 'v1'}, 60,
             'my template')
@@ -72,35 +72,35 @@ class HeatClientTests(TestCase):
         update_stack PUTs data to the stack endpoint and returns
         the parsed JSON result.
         """
-        log = mock_log()
-        treq = self._treq(code=202, method='put',
-                          json_content={'hello': 'world'})
-        client = HeatClient('my-auth-token', log, treq)
+        log = StubLog()
+        http = self._http_client(code=202, json_content={'hello': 'world'})
+        client = HeatClient(log, http)
         result = client.update_stack(
             'http://heat-url/my-stack', {'p1': 'v1'}, 60,
             'my template')
-        treq.put.assert_called_once_with(
-            'http://heat-url/my-stack',
-            headers={'x-auth-token': ['my-auth-token'],
-                     'content-type': ['application/json'],
-                     'accept': ['application/json'],
-                     'User-Agent': ['OtterScale/0.0']},
-            data=json.dumps({
-                'parameters': {'p1': 'v1'},
-                'timeout_mins': 60,
-                'template': 'my template'}),
-            log=mock.ANY)
+        self.assertEqual(
+            http.treq.requests,
+            [{'method': 'put',
+              'url': 'http://heat-url/my-stack',
+              'headers': {'x-auth-token': ['my-auth-token'],
+                          'content-type': ['application/json'],
+                          'accept': ['application/json'],
+                          'User-Agent': ['OtterScale/0.0']},
+              'data': json.dumps({
+                  'parameters': {'p1': 'v1'},
+                  'timeout_mins': 60,
+                  'template': 'my template'}),
+              'log': mock.ANY}])
 
         self.assertEqual(self.successResultOf(result), {'hello': 'world'})
-        self._assert_bound(log, treq.put.mock_calls[-1][2]['log'],
+        self._assert_bound(http.treq.requests[-1]['log'],
                            system='heatclient', event='update-stack')
 
     def test_update_stack_error(self):
         """Non-202 codes from updating a stack are considered an APIError."""
-        log = mock_log()
-        treq = self._treq(code=204, method='put',
-                          json_content={'hello': 'world'})
-        client = HeatClient('my-auth-token', log, treq)
+        log = StubLog()
+        http = self._http_client(code=204, json_content={'hello': 'world'})
+        client = HeatClient(log, http)
         result = client.update_stack(
             'http://heat-url/my-stack', {'p1': 'v1'}, 60,
             'my template')
@@ -109,19 +109,29 @@ class HeatClientTests(TestCase):
 
     def test_get_stack(self):
         """get_stack performs a GET on the given stack URL."""
-        log = mock_log()
-        treq = self._treq(code=200, method='get',
-                          json_content={'hello': 'world'})
-        client = HeatClient('my-auth-token', log, treq)
+        log = StubLog()
+        http = self._http_client(code=200, json_content={'hello': 'world'})
+        client = HeatClient(log, http)
         result = client.get_stack('http://heat-url/my-stack')
+        self.assertEqual(
+            http.treq.requests,
+            [{'method': 'get',
+              'url': 'http://heat-url/my-stack',
+              'headers': {'x-auth-token': ['my-auth-token'],
+                          'content-type': ['application/json'],
+                          'accept': ['application/json'],
+                          'User-Agent': ['OtterScale/0.0']},
+              'data': None,
+              'log': mock.ANY}])
         self.assertEqual(self.successResultOf(result), {'hello': 'world'})
+        self._assert_bound(http.treq.requests[-1]['log'],
+                           system='heatclient', event='get-stack')
 
     def test_get_stack_error(self):
         """Non-200 codes from getting a stack are considered an APIError."""
-        log = mock_log()
-        treq = self._treq(code=201, method='get',
-                          json_content={'hello': 'world'})
-        client = HeatClient('my-auth-token', log, treq)
+        log = StubLog()
+        http = self._http_client(code=201, json_content={'hello': 'world'})
+        client = HeatClient(log, http)
         result = client.get_stack('http://heat-url/my-stack')
         failure = self.failureResultOf(result)
         failure.trap(APIError)
--- otter/worker/heat_client.py
+++ otter/worker/heat_client.py
@@ -13,13 +13,104 @@ from otter.util.http import (append_segments, headers, check_success,
 __all__ = ['HeatClient']
 
 
+class Request(object):
+    """
+    An immutable representation of a request.
+    """
+    # I just want this to be a PersistentDict with perform() as a plain
+    # function, but OO is the thing to do.
+    def __init__(self, method, url, headers=None, data=None):
+        self.method = method
+        self.url = url
+        self.headers = headers if headers is not None else {}
+        self.data = data
+
+    def perform(self, log, treq, headers):
+        """
+        Perform the request with the given treq client.
+
+        :param log: A bound log to pass on to treq.
+        :param treq: The treq object.
+        :param headers: Additional late-bound headers. Typically used for
+            the X-Auth-Token.
+        """
+        func = getattr(treq, self.method)
+        req_headers = self.headers.copy()
+        req_headers.update(headers)
+        return func(self.url, headers=req_headers, data=self.data, log=log)
+
+
+class ReauthenticationFailed(Exception):
+    """
+    Raised when an HTTP request returned 401 even after successful
+    reauthentication was performed.
+    """
+
+
+class OpenStackClient(object):
+    def __init__(self, treq, reauth, auth_token=None):
+        self.treq = treq
+        self.reauth = reauth
+        self.auth_token = auth_token
+
+    def _request_with_reauth(self, log, request, repeats=1):
+        """
+        Perform a request and automatically handle reauthentication.
+
+        If an HTTP 401 is returned, the `reauth` function will be called, and
+        when the returned Deferred fires, the request will be repeated.
+
+        If repeating the request fails, :class:`ReauthenticationFailed` will
+        be raised.
+        """
+        def _got_reauth_result(response):
+            return self._request_with_reauth(log, request, repeats - 1)
+
+        def _got_result(response):
+            if response.code == 401:
+                if repeats == 0:
+                    raise ReauthenticationFailed()
+                reauth_result = self.reauth()
+                reauth_result.addCallback(_got_reauth_result)
+                reauth_result.addErrback(_got_reauth_error)
+                return reauth_result
+            else:
+                return response
+
+        standard_headers = headers(self.auth_token)
+        result = request.perform(log, self.treq, standard_headers)
+        result.addCallback(_got_result)
+        return result
+
+    def json_request(self, log, method, url, headers=None, data=None,
+                     success=(200,)):
+        """
+        Do a request, check the response code, and parse the JSON result.
+
+        :param log: A bound log to pass on to the treq client.
+        :param method: The HTTP method to invoke.
+        :param url: As treq accepts.
+        :param headers: As treq accepts.
+        :param data: As treq accepts.
+        :param list success: The list of HTTP codes to consider successful.
+        """
+        request = Request(
+            method,
+            url,
+            data=data,
+            headers=headers)
+        result = self._request_with_reauth(log, request)
+        result.addCallback(check_success, success, self.treq)
+        result.addCallback(self.treq.json_content)
+        return result
+
+
 class HeatClient(object):
     """
     An Asynchronous Heat client.
     """
-    def __init__(self, auth_token, log, treq):
-        self.treq = treq
-        self.auth_token = auth_token
+    def __init__(self, log, http):
+        self.http = http
         self.log = log.bind(system='heatclient')
 
     def create_stack(self, heat_url, stack_name, parameters, timeout,
@@ -32,12 +123,12 @@ class HeatClient(object):
             'template': template
         }
         log = self.log.bind(event='create-stack', stack_name=stack_name)
-        result = self.treq.post(
+        return self.http.json_request(
+            log,
+            'post',
             append_segments(heat_url, 'stacks'),
             data=json.dumps(payload),
-            headers=headers(self.auth_token), log=log)
-        result.addCallback(check_success, [201])
-        return result.addCallback(self.treq.json_content)
+            success=[201])
 
     def update_stack(self, stack_url, parameters, timeout, template):
         """Update a stack."""
@@ -47,22 +138,19 @@ class HeatClient(object):
             'template': template,
         }
         log = self.log.bind(event='update-stack')
-        result = self.treq.put(
+        return self.http.json_request(
+            log,
+            'put',
             stack_url,
             data=json.dumps(payload),
-            headers=headers(self.auth_token),
-            log=log)
-        result.addCallback(check_success, [202])
-        result.addCallback(self.treq.json_content)
-        return result
+            success=[202])
 
     def get_stack(self, stack_url):
         """Get the metadata about a stack."""
-        result = self.treq.get(stack_url, headers=headers(self.auth_token),
-                               log=self.log)
-        result.addCallback(check_success, [200])
-        result.addCallback(self.treq.json_content)
-        return result
+        log = self.log.bind(event='get-stack')
+        return self.http.json_request(
+            log,
+            'get', stack_url)
 
 
 def main(reactor, *args):
