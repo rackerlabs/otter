4bafc5fe4d3dd1c7cb4a9aa6a80bff3a788ee559
Laurens Van Houtven <_@lvh.cc>
2014-05-20 11:12:26 +0200

    Fix docstrings

--- otter/test/test_tls.py
+++ otter/test/test_tls.py
@@ -1,15 +1,19 @@
-"""Tests related to TLS configuration and functionality."""
+"""
+Tests related to TLS configuration and functionality.
+"""
 from twisted.trial.unittest import SynchronousTestCase
 
 
 class ServiceIdentityTestCase(SynchronousTestCase):
-    """Tests that ``service_identity``, which causes TLS connections that
+    """
+    Tests that ``service_identity``, which causes TLS connections that
     use it (all of the ones we make) to correctly authenticate their
     peers.
-
     """
     def test_service_identity_is_installed(self):
-        """The ``service_identity`` module can be imported."""
+        """
+        The ``service_identity`` module can be imported.
+        """
         try:
             import service_identity
             service_identity  # pyflakes workaround
