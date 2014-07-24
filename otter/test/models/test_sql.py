92506153dcd5c9041e14134131f5cf1b7d5d1e85
Laurens Van Houtven <_@lvh.cc>
2014-07-24 15:44:24 +0200

    test_update_config_for_nonexistent_group

--- otter/test/models/test_sql.py
+++ otter/test/models/test_sql.py
@@ -215,6 +215,16 @@ class SQLScalingGroupTests(SQLiteTestMixin, ConfigTestMixin, TestCase):
         launch_cfg = yield group.view_launch_config()
         self.assertEqual(launch_cfg, self._launch)
 
+    def test_update_config_for_nonexistent_group(self):
+        """
+        When attempting to update the configuration for a group that
+        doesn't exist, an exception is raised.
+        """
+        group = sql.SQLScalingGroup(self.engine, b"TENANT", b"BOGUS")
+        cfg = group_examples.config()[1]
+        d = group.view_launch_config(cfg)
+        return self.assertFailure(d, interface.NoSuchScalingGroupError)
+
     def test_update_launch_config_for_nonexistent_group(self):
         """
         When attempting to update the launch configuration for a group that
