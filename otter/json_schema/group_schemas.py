4ccbf15592b498af399b56818b14dbfbc0d1ef6e
cyli <cyli@twistedmatrix.com>
2014-08-15 16:54:34 -0700

    Enable null server metadata, because nova allows it

--- otter/json_schema/group_schemas.py
+++ otter/json_schema/group_schemas.py
@@ -51,6 +51,21 @@ metadata = {
     "additionalProperties": False
 }
 
+server_metadata = {
+    # TODO: Taken from Nova's validation code at
+    # https://github.com/openstack/nova/blob/master/nova/api/validation/parameter_types.py#L83.
+    # It will be ideal to import nova package and use their schema but their
+    # schema is not fully complete and there are lots of dependencies when trying to
+    # install nova package
+    "type": "object",
+    "patternProperties": {
+        "^[a-zA-Z0-9-_:. ]{1,255}$": {
+            "type": "string", "maxLength": 255
+        }
+    },
+    "additionalProperties": False,
+}
+
 # nova server payload
 server = {
     "type": "object",
@@ -95,18 +110,7 @@ server = {
             "required": False
         },
         "metadata": {
-            # TODO: Taken from Nova's validation code at
-            # https://github.com/openstack/nova/blob/master/nova/api/validation/parameter_types.py#L83.
-            # It will be ideal to import nova package and use their schema but their
-            # schema is not fully complete and there are lots of dependencies when trying to
-            # install nova package
-            "type": "object",
-            "patternProperties": {
-                "^[a-zA-Z0-9-_:. ]{1,255}$": {
-                    "type": "string", "maxLength": 255
-                }
-            },
-            "additionalProperties": False,
+            "type": [server_metadata, "null"],
             "required": False
         }
     },
