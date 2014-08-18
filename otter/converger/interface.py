1c51f6901344ee0b54bfd858616c892eeb63eaf1
Laurens Van Houtven <_@lvh.io>
2014-08-18 15:07:31 +0200

    Address review comments

--- otter/converger/interface.py
+++ otter/converger/interface.py
@@ -6,23 +6,20 @@ from zope.interface import Interface
 class IConverger(Interface):
     """
     A converger is a continuous process that, over time, causes the
-    number of machines attached to a load balancer to converge to a
-    fixed value.
+    number of machines in a scaling group to converge to a fixed
+    value.
     """
-    def schedule_convergence(load_balancer_id):
-        """
-        Schedules convergence for the machiens attached to the load
-        balancer with the given identifier.
+    def schedule_convergence(group_id):
+        """Schedules convergence for the machines in a scaling group.
 
-        :param bytes load_balancer_id: The CLB's unique identifier.
+        :param bytes group_id: The group's unique identifier.
         :return: :data:`None`
         """
 
-    def set_desired_capacity(load_balancer_id, desired_capacity):
-        """Sets the desired number of machines attached to the load balancer
-        with given identifier.
+    def set_desired_capacity(group_id, desired_capacity):
+        """Sets the desired number of machines in a scaling group.
 
-        :param bytes load_balancer_id: The CLB's unique identifier.
+        :param bytes group_id: The group's unique identifier.
         :param int desired_capacity: The desired number of machines for the
             load balancer.
         :raises ValueError: (synchronously) If the given desired
