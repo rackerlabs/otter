132f4de191a90b932fd2211ce7d0b913d3bfcd8a
cyli <cyli@twistedmatrix.com>
2014-10-07 14:12:40 -0700

    Integration test create scaling group with blank image ID, remove negative test with empty image ID

--- autoscale_cloudroast/test_repo/autoscale/functional/scaling_group/test_create_scaling_group.py
+++ autoscale_cloudroast/test_repo/autoscale/functional/scaling_group/test_create_scaling_group.py
@@ -12,52 +12,51 @@ class CreateScalingGroupTest(AutoscaleFixture):
     Verify create group.
     """
 
-    @classmethod
-    def setUpClass(cls):
+    def _create_scaling_group(self):
         """
         Create a scaling group with all the fields.
         """
-        super(CreateScalingGroupTest, cls).setUpClass()
-        cls.gc_name = rand_name('testscalinggroup')
-        cls.gc_max_entities = 10
-        cls.gc_metadata = {'gc_meta_key_1': 'gc_meta_value_1',
-                           'gc_meta_key_2': 'gc_meta_value_2'}
-        cls.file_contents = 'This is a test file.'
-        cls.lc_personality = [{'path': '/root/.csivh',
-                               'contents': base64.b64encode(cls.file_contents)}]
-        cls.lc_metadata = {'meta_key_1': 'meta_value_1',
-                           'meta_key_2': 'meta_value_2'}
-        cls.lc_disk_config = 'AUTO'
-        cls.lc_networks = [{'uuid': '11111111-1111-1111-1111-111111111111'},
-                           {'uuid': '00000000-0000-0000-0000-000000000000'}]
-        cls.lc_load_balancers = [{'loadBalancerId': 9099, 'port': 8080}]
-        cls.sp_list = [{
+        self.gc_name = rand_name('testscalinggroup')
+        self.gc_max_entities = 10
+        self.gc_metadata = {'gc_meta_key_1': 'gc_meta_value_1',
+                            'gc_meta_key_2': 'gc_meta_value_2'}
+        self.file_contents = 'This is a test file.'
+        self.lc_personality = [{'path': '/root/.csivh',
+                                'contents': base64.b64encode(self.file_contents)}]
+        self.lc_metadata = {'meta_key_1': 'meta_value_1',
+                            'meta_key_2': 'meta_value_2'}
+        self.lc_disk_config = 'AUTO'
+        self.lc_networks = [{'uuid': '11111111-1111-1111-1111-111111111111'},
+                            {'uuid': '00000000-0000-0000-0000-000000000000'}]
+        self.lc_load_balancers = [{'loadBalancerId': 9099, 'port': 8080}]
+        self.sp_list = [{
             'name': 'scale up by 1',
             'change': 1,
             'cooldown': 0,
             'type': 'webhook'
         }]
-        cls.create_resp = cls.autoscale_client.create_scaling_group(
-            gc_name=cls.gc_name,
-            gc_cooldown=cls.gc_cooldown,
-            gc_min_entities=cls.gc_min_entities,
-            gc_max_entities=cls.gc_max_entities,
-            gc_metadata=cls.gc_metadata,
-            lc_name=cls.lc_name,
-            lc_image_ref=cls.lc_image_ref,
-            lc_flavor_ref=cls.lc_flavor_ref,
-            lc_personality=cls.lc_personality,
-            lc_metadata=cls.lc_metadata,
-            lc_disk_config=cls.lc_disk_config,
-            lc_networks=cls.lc_networks,
-            lc_load_balancers=cls.lc_load_balancers,
-            sp_list=cls.sp_list,
-            network_type='public')
-        cls.scaling_group = cls.create_resp.entity
-        cls.resources.add(cls.scaling_group.id,
-                          cls.autoscale_client.delete_scaling_group)
 
-    def test_create_scaling_group_response(self):
+        self.create_resp = self.autoscale_client.create_scaling_group(
+            gc_name=self.gc_name,
+            gc_cooldown=self.gc_cooldown,
+            gc_min_entities=self.gc_min_entities,
+            gc_max_entities=self.gc_max_entities,
+            gc_metadata=self.gc_metadata,
+            lc_name=self.lc_name,
+            lc_image_ref=self.lc_image_ref,
+            lc_flavor_ref=self.lc_flavor_ref,
+            lc_personality=self.lc_personality,
+            lc_metadata=self.lc_metadata,
+            lc_disk_config=self.lc_disk_config,
+            lc_networks=self.lc_networks,
+            lc_load_balancers=self.lc_load_balancers,
+            sp_list=self.sp_list,
+            network_type='public')
+        self.scaling_group = self.create_resp.entity
+        self.resources.add(self.scaling_group.id,
+                           self.autoscale_client.delete_scaling_group)
+
+    def _test_create_scaling_group_response(self):
         """
         Verify the response code for the create scaling group is 201
         """
@@ -69,7 +68,7 @@ class CreateScalingGroupTest(AutoscaleFixture):
                           '{1}'.format(self.create_resp.status_code, self.scaling_group.id))
         self.validate_headers(self.create_resp.headers)
 
-    def test_create_scaling_group_fields(self):
+    def _test_create_scaling_group_fields(self):
         """
         Verify the scaling group id and links exist in the response
         """
@@ -80,7 +79,7 @@ class CreateScalingGroupTest(AutoscaleFixture):
                         msg='Scaling Group links were not set in the response'
                         ' for group {0}'.format(self.scaling_group.id))
 
-    def test_created_scaling_group_groupconfig_fields(self):
+    def _test_created_scaling_group_groupconfig_fields(self):
         """
         Verify the group configuration of the group is as expected.
         """
@@ -106,19 +105,19 @@ class CreateScalingGroupTest(AutoscaleFixture):
                          msg='Scaling group metadata did not match'
                          ' for group {0}'.format(self.scaling_group.id))
 
-    def test_created_scaling_group_launchconfig_scalingpolicy_fields(self):
+    def _test_created_scaling_group_launchconfig_scalingpolicy_fields(self):
         """
         Verify the launch configuration on the group is as expected.
         """
-        self.assertEqual(self.autoscale_config.lc_name,
+        self.assertEqual(self.lc_name,
                          self.scaling_group.launchConfiguration.server.name,
                          msg='Server name provided in the launch config did not match'
                          ' for group {0}'.format(self.scaling_group.id))
-        self.assertEqual(self.autoscale_config.lc_image_ref,
+        self.assertEqual(self.lc_image_ref,
                          self.scaling_group.launchConfiguration.server.imageRef,
                          msg='Image id did not match'
                          ' for group {0}'.format(self.scaling_group.id))
-        self.assertEqual(self.autoscale_config.lc_flavor_ref,
+        self.assertEqual(self.lc_flavor_ref,
                          self.scaling_group.launchConfiguration.server.flavorRef,
                          msg='Flavor id did not match'
                          ' for group {0}'.format(self.scaling_group.id))
@@ -147,8 +146,37 @@ class CreateScalingGroupTest(AutoscaleFixture):
             msg='Scaling policies of the scaling group did not match'
             ' for group {0}'.format(self.scaling_group.id))
 
-    def test_created_scaling_group_state_fields(self):
+    def _test_created_scaling_group_state_fields(self):
         """
         Verify the state on the group is as expected.
         """
         self.assert_group_state(self.scaling_group.state)
+
+    def test_create_scaling_group_success_response(self):
+        """
+        Create a scaling group, and test all aspects of the response as defined
+        in the _test_* cases above.
+        """
+        self._create_scaling_group()
+        self._test_create_scaling_group_response()
+        self._test_create_scaling_group_fields()
+        self._test_created_scaling_group_groupconfig_fields()
+        self._test_created_scaling_group_launchconfig_scalingpolicy_fields()
+
+    def test_create_scaling_group_with_boot_from_volume(self):
+        """
+        Create a scaling group with a blank image ID, and test that the
+        response is successful and that all the launch config fields match
+        what was created.
+
+        TODO: once block_device_mapping is validated (because image ID should
+        only be empty if ``block_device_mapping`` is specified), the create
+        scaling group function should take ``block_device_mapping`` (the
+        autoscale and nova fixtures should be updated), and
+        :func:`_test_created_scaling_group_launchconfig_scalingpolicy_fields`
+        should test whether ``block_device_mapping`` matches.
+        """
+        self.lc_image_ref = ""
+        self._create_scaling_group()
+        self._test_create_scaling_group_response()
+        self._test_created_scaling_group_launchconfig_scalingpolicy_fields()
