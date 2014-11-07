52b344bb43fdae6757303c60ef4aebb2c1f27958
Laurens Van Houtven <_@lvh.io>
2014-11-07 12:35:20 +0100

    Add RCv3 constant

--- otter/constants.py
+++ otter/constants.py
@@ -7,3 +7,4 @@ class ServiceType(Names):
     """Constants representing Rackspace cloud services."""
     CLOUD_SERVERS = NamedConstant()
     CLOUD_LOAD_BALANCERS = NamedConstant()
+    RACKCONNECT_V3 = NamedConstant()
