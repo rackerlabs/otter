11211ad8ee786bb71d9b3d508e337afb16b86398
Laurens Van Houtven <_@lvh.io>
2014-11-12 14:01:04 +0100

    Nitpick: request_fn -> request_func consistency

--- otter/convergence.py
+++ otter/convergence.py
@@ -805,18 +805,18 @@ class Request(object):
     """
 
 
-def _reqs_to_effect(request_fn, conv_requests):
+def _reqs_to_effect(request_func, conv_requests):
     """Turns a collection of :class:`Request` objects into an effect.
 
-    :param request_fn: A pure-http request function, as produced by
+    :param request_func: A pure-http request function, as produced by
         :func:`otter.http.get_request_func`.
     :param conv_requests: Convergence requests to turn into effects.
     """
-    effects = [request_fn(service_type=r.service,
-                          method=r.method,
-                          url=r.path,
-                          headers=r.headers,
-                          data=r.data,
-                          success_codes=r.success_codes)
+    effects = [request_func(service_type=r.service,
+                            method=r.method,
+                            url=r.path,
+                            headers=r.headers,
+                            data=r.data,
+                            success_codes=r.success_codes)
                for r in conv_requests]
     return ParallelEffects(effects)
