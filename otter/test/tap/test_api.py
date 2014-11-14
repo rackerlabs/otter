5d12926b5aff1aa443b8c0669ce2de8d11534207
Laurens Van Houtven <_@lvh.io>
2014-11-14 20:05:40 +0100

    Write a test for hardcoded service names

--- otter/test/tap/test_api.py
+++ otter/test/tap/test_api.py
@@ -67,6 +67,14 @@ class APIOptionsTests(SynchronousTestCase):
         config.parseOptions(['-p', 'tcp:9999'])
         self.assertEqual(config['port'], 'tcp:9999')
 
+    def test_hardcoded_service_names(self):
+        """
+        Several hardcoded service names are injected.
+        """
+        config = Options()
+        config.parseOptions([])
+        self.assertEqual(config['cloudLoadBalancers'], 'cloudLoadBalancers')
+        self.assertEqual(config['cloudServersOpenStack'], 'cloudServersOpenStack')
 
 class HealthCheckerTests(SynchronousTestCase):
     """
