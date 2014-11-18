de1ca3d93ea8a6c605c4203fa58409cb40f3cba5
Christopher Armstrong <chris.armstrong@rackspace.com>
2014-11-18 11:56:38 -0600

    pull in retry changes from pure-server-info

--- otter/util/retry.py
+++ otter/util/retry.py
@@ -4,7 +4,10 @@ Module that provides retrying-at-a-particular-interval functionality.
 
 import random
 
+from characteristic import attributes, Attribute
+
 from effect import Effect, Delay, FuncIntent
+from effect.retry import retry as effect_retry
 
 from twisted.internet import defer
 from twisted.python.failure import Failure
@@ -158,13 +161,7 @@ def retry_times(max_tries):
     :return: a function that accepts a :class:`Failure` and returns ``True``
         until it has been called ``max_tries`` times. Otherwise, returns ``False``
     """
-    tries = [0]
-
-    def can_retry(f):
-        tries[0] += 1
-        return tries[0] <= max_tries
-
-    return can_retry
+    return RetryTimes(max_retries=max_tries)
 
 
 def compose_retries(*can_retry_funcs):
@@ -214,16 +211,7 @@ def exponential_backoff_interval(start=2):
     :param start: number of seconds > 0 to start with
     :return: a function that accepts a :class:`Failure` and returns ``interval``
     """
-    last_interval = [0]
-
-    def next_interval(f):
-        if last_interval[0]:
-            last_interval[0] *= 2
-        else:
-            last_interval[0] = start
-        return last_interval[0]
-
-    return next_interval
+    return ExponentialBackoffInterval(start=start)
 
 
 def retry(do_work, can_retry=None, next_interval=None, clock=None):
@@ -276,29 +264,97 @@ class TransientRetryError(Exception):
     """
 
 
-def should_retry_effect(can_retry, next_interval, exc_info):
+# TODO: The following code should be moved to effect.retry if it proves out.
+
+@attributes(['start', Attribute('last_interval', default_value=0)])
+class ExponentialBackoffInterval(object):
     """
-    Determine whether an :class:`Effect` should be retried, with delay support.
+    A callable that returns the previous interval * 2 (starting at
+    ``start``) every time it's called.
 
-    This is basically glue between :obj:`otter.util.retry` and :obj:`effect.retry`
-    -- after partially applying all but the last argument, this function is
-    suitable for the ``should_retry`` parameter of :func:`effect.retry.retry`.
-
-    This function is pure -- the Effect returned represents all side-effecting
-    operations.
-
-    :param can_retry: a potentially impure function of Failure -> bool that
-        indicates whether a retry should be performed.
-    :param next_interval: a potentially impure function that returns an
-        interval to wait for the next retry attempt.
-    :param tuple exc_info: an exception tuple
+    :param start: number of seconds > 0 to start with
+    :return: a function that accepts a :class:`Failure` and returns ``interval``
     """
-    def got_can_retry(bool):
-        if bool:
-            return Effect(FuncIntent(next_interval)).on(
-                lambda interval: Effect(Delay(interval))).on(lambda r: True)
+
+    def __call__(self, failure):
+        """Return an increasingly larger number."""
+        if self.last_interval != 0:
+            self.last_interval *= 2
         else:
-            return False
-    exc_type, exc_value, exc_traceback = exc_info
-    eff = Effect(FuncIntent(lambda: can_retry(Failure(exc_value, exc_type, exc_traceback))))
-    return eff.on(got_can_retry)
+            self.last_interval = self.start
+        return self.last_interval
+
+
+@attributes(['max_retries', Attribute('tries', default_value=0)])
+class RetryTimes(object):
+    """
+    A callable that returns True until it's been called ``max_retries``.
+    :param max_retries: Number of times to return True.
+    """
+    def __call__(self, failure):
+        """Return True if this has been called <= ``max_retries``."""
+        self.tries += 1
+        return self.tries <= self.max_retries
+
+
+@attributes(['can_retry', 'next_interval'])
+class ShouldDelayAndRetry(object):
+    """
+    A callable which can be passed as the should_retry argument to
+    :func:`effect.retry.retry`. Determines whether to retry and also causes
+    a delay before retrying.
+
+    :param can_retry: A callable of Failure -> Bool, indicating whether retry
+        should occur
+    :param next_interval: A callable of Failure -> interval to wait
+    """
+
+    def __call__(self, exc_info):
+        """
+        Determine whether retry should occur, based on the exception info.
+        """
+        exc_type, exc_value, exc_traceback = exc_info
+        failure = Failure(exc_value, exc_type, exc_traceback)
+
+        def doit():
+            if self.can_retry(failure):
+                interval = self.next_interval(failure)
+                return Effect(Delay(interval)).on(lambda r: True)
+            else:
+                return False
+        return Effect(FuncIntent(doit))
+
+
+@attributes(['effect', 'should_retry'])
+class Retry(object):
+    """
+    An effect intent that, when performed, executes another effect and
+    potentially retries it.
+
+    The main reason this class exists is to transparently represent the
+    retryable effect and its retry policy as public attributes, making it
+    easy to test that some effect should be retried, without actually running
+    a bunch of imperative code to test it's retried correctly.
+
+    :param effect: The effect to perform.
+    :param should_retry: The function to call to determine whether retry
+    should occur (usually an instance of :obj:`ShouldDelayAndRetry`).
+    """
+    def perform_effect(self, dispatcher):
+        """
+        Invoke :func:`effect.retry.retry` with the effect and the
+        should_retry function.
+        """
+        return effect_retry(self.effect, self.should_retry)
+
+
+def retry_effect(effect, can_retry, next_interval):
+    """
+    Convenience function for wrapping an effect in a :obj:`Retry`.
+
+    :return: :obj:`Effect` of :obj:`Retry`.
+    """
+    return Effect(Retry(
+        effect=effect,
+        should_retry=ShouldDelayAndRetry(can_retry=can_retry,
+                                         next_interval=next_interval)))
