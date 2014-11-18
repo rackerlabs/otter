7b43e3cee1c05b390357b09f929a6e3a65c058d3
Christopher Armstrong <chris.armstrong@rackspace.com>
2014-11-18 17:03:50 -0600

    extract a "get_service_mapping" out of the _get_request_func method in supervisor.py

--- otter/constants.py
+++ otter/constants.py
@@ -8,3 +8,19 @@ class ServiceType(Names):
     CLOUD_SERVERS = NamedConstant()
     CLOUD_LOAD_BALANCERS = NamedConstant()
     RACKCONNECT_V3 = NamedConstant()
+
+
+def get_service_mapping(service_config):
+    """
+    Return a mapping of :class:`ServiceType` members to the actual configured
+    service names to look up in tenant catalogs.
+
+    :param service_config: function of idiomatic service name ->
+        deployment-specific service name
+    """
+    return {
+        ServiceType.CLOUD_SERVERS: service_config('cloudServersOpenStack'),
+        ServiceType.CLOUD_LOAD_BALANCERS: service_config("cloudLoadBalancers"),
+        ServiceType.RACKCONNECT_V3: service_config('rackconnect'),
+    }
+
