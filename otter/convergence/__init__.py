9b9ecf903ab7d8bff9f8d129894f5dc139c22afd
Christopher Armstrong <chris.armstrong@rackspace.com>
2014-12-11 11:12:07 -0600

    effecting.py

--- otter/convergence/__init__.py
+++ otter/convergence/__init__.py
@@ -12,29 +12,12 @@ from toolz.curried import map
 
 # radix in-development imports
 
+from otter.constants import ServiceType
 from otter.convergence.planning import converge, _remove_from_lb_with_draining, _converge_lb_state, optimize_steps
 from otter.convergence.steps import AddNodesToLoadBalancer, BulkAddToRCv3, BulkRemoveFromRCv3, CreateServer, DeleteServer, RemoveFromLoadBalancer, ChangeLoadBalancerNode, SetMetadataItemOnServer, Request, Convergence
 from otter.convergence.model import NodeCondition, NodeType, ServerState, LBNode, LBConfig, NovaServer, DesiredGroupState
 from otter.convergence.gathering import get_all_server_details, get_scaling_group_servers, get_load_balancer_contents, extract_drained_at, to_nova_server, json_to_LBConfigs
-
-
-def _reqs_to_effect(request_func, conv_requests):
-    """Turns a collection of :class:`Request` objects into an effect.
-
-    :param request_func: A pure-http request function, as produced by
-        :func:`otter.http.get_request_func`.
-    :param conv_requests: Convergence requests to turn into effects.
-    :return: An effect which will perform all the requests in parallel.
-    :rtype: :class:`Effect`
-    """
-    effects = [request_func(service_type=r.service,
-                            method=r.method,
-                            url=r.path,
-                            headers=r.headers,
-                            data=r.data,
-                            success_codes=r.success_codes)
-               for r in conv_requests]
-    return parallel(effects)
+from otter.convergence.effecting import _reqs_to_effect
 
 
 def execute_convergence(request_func, group_id, desired, launch_config,
