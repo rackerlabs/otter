f163da178e5b79c2ce09d67041a3c63aed45542c
Manish Tomar <manish.tomar@rackspace.com>
2014-12-17 17:00:34 -0800

    Removing service defaults

--- otter/constants.py
+++ otter/constants.py
@@ -16,16 +16,14 @@ class ServiceType(Names):
 def get_service_mapping(config):
     """
     Return a mapping of :class:`ServiceType` members to the actual configured
-    service names to look up in tenant catalogs. If the service is not found,
-    it's default name is returned
+    service names to look up in tenant catalogs.
 
     :param dict config: Config from file containing service names that will be
         there in service catalog
     """
     return {
-        ServiceType.CLOUD_SERVERS: config.get('cloudServersOpenStack', 'cloudServersOpenStack'),
-        ServiceType.CLOUD_LOAD_BALANCERS: config.get("cloudLoadBalancers", 'cloudLoadBalancers'),
-        ServiceType.RACKCONNECT_V3: config.get('rackconnect', 'rackconnect'),
-        ServiceType.CLOUD_METRICS_INGEST: get_in(['metrics', 'service'], config,
-                                                 default='cloudMetricsIngest')
+        ServiceType.CLOUD_SERVERS: config['cloudServersOpenStack'],
+        ServiceType.CLOUD_LOAD_BALANCERS: config["cloudLoadBalancers"],
+        ServiceType.RACKCONNECT_V3: config['rackconnect'],
+        ServiceType.CLOUD_METRICS_INGEST: config['metrics']['service']
     }
--- otter/test/test_constants.py
+++ otter/test/test_constants.py
@@ -27,26 +27,3 @@ class GetServiceMappingTests(SynchronousTestCase):
             get_service_mapping(self.config),
             {ServiceType.CLOUD_SERVERS: 'nova', ServiceType.CLOUD_LOAD_BALANCERS: 'clb',
              ServiceType.RACKCONNECT_V3: 'rc', ServiceType.CLOUD_METRICS_INGEST: 'm'})
-
-    def test_defaults(self):
-        """
-        Returns default values if config doesn't have service names
-        """
-        self.assertEqual(
-            get_service_mapping({}),
-            {ServiceType.CLOUD_SERVERS: 'cloudServersOpenStack',
-             ServiceType.CLOUD_LOAD_BALANCERS: "cloudLoadBalancers",
-             ServiceType.RACKCONNECT_V3: 'rackconnect',
-             ServiceType.CLOUD_METRICS_INGEST: 'cloudMetricsIngest'})
-
-    def test_metrics_defaults(self):
-        """
-        Returns default value for metrics when metrics key is there but service
-        is not there in it
-        """
-        self.assertEqual(
-            get_service_mapping({'metrics': {'something': 'else'}}),
-            {ServiceType.CLOUD_SERVERS: 'cloudServersOpenStack',
-             ServiceType.CLOUD_LOAD_BALANCERS: "cloudLoadBalancers",
-             ServiceType.RACKCONNECT_V3: 'rackconnect',
-             ServiceType.CLOUD_METRICS_INGEST: 'cloudMetricsIngest'})
