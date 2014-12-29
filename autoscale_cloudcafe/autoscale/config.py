92f6d270b151ca9517acee717b84ab4e81a47f59
Samuel A. Falvo II <sam.falvo@rackspace.com>
2014-12-29 15:28:54 -0800

    Updating config.py too

--- autoscale_cloudcafe/autoscale/config.py
+++ autoscale_cloudcafe/autoscale/config.py
@@ -369,3 +369,44 @@ class AutoscaleConfig(ConfigSectionInterface):
         no access for autoscale.
         """
         return self.get('autoscale_nno_lno_aa')
+
+    @property
+    def rcv3_endpoint_name(self):
+        """
+        Get the catalog name of the RCV3 endpoint
+        """
+        return self.get('rcv3_endpoint_name')
+
+    @property
+    def rc_load_balancer_pool_1(self):
+        """
+        Name and id of the first shared RCV3 LB
+        """
+        print "Get rc_load_balancer_pool_1 ..... "
+
+        temp = self.get('rc_load_balancer_pool_1')
+        print temp
+        return temp
+
+    @property
+    def rc_load_balancer_pool_2(self):
+        """
+        Name and id of the second shared RCV3 LB
+        """
+        temp = self.get('rc_load_balancer_pool_2')
+        print "Get lb2 {0}".format(temp)
+        return temp
+
+    @property
+    def rcv3_region_override(self):
+        """
+        Allow override of the rcv3 region from the config file
+        """
+        return self.get('rcv3_region_override')
+
+    @property
+    def rcv3_cloud_network(self):
+        """
+        Specify the cloud network to use with RackConnect
+        """
+        return self.get('rcv3_cloud_network')
