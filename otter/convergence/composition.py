fc2124c287c77f010cc4612d09c92aa2cd54e03a
Laurens Van Houtven <_@lvh.io>
2015-01-20 15:23:43 -0800

    Fix minor lint

--- otter/convergence/composition.py
+++ otter/convergence/composition.py
@@ -32,7 +32,8 @@ def execute_convergence(request_func, group_id, desired_group_state,
     """
     eff = get_all_convergence_data(request_func, group_id)
     conv_eff = eff.on(
-        lambda (servers, lb_nodes): plan(desired_group_state, servers, lb_nodes, time.time()))
+        lambda (servers, lb_nodes): plan(desired_group_state, servers, lb_nodes,
+                                         time.time()))
     return conv_eff.on(partial(steps_to_effect, request_func)).on(bool)
 
 
