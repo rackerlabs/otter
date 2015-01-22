fb33f74dd1b5ac0fc488c01bb6f230ad7259ffd9
Christopher Armstrong <chris.armstrong@rackspace.com>
2015-01-21 18:02:08 -0600

    get rid of steps.Request, and change IStep.as_request to IStep.as_effect
    wow, this was easy!

--- otter/test/convergence/test_effecting.py
+++ otter/test/convergence/test_effecting.py
@@ -1,148 +1,26 @@
 """Tests for convergence effecting."""
 
-from effect import parallel
+from effect import Constant, Effect, parallel
 
 from twisted.trial.unittest import SynchronousTestCase
 
 from zope.interface import implementer
 
-from otter.constants import ServiceType
-from otter.convergence.effecting import _reqs_to_effect, steps_to_effect
-from otter.convergence.steps import IStep, Request
-from otter.http import service_request
-from otter.util.pure_http import has_code
-
-
-class RequestsToEffectTests(SynchronousTestCase):
-    """
-    Tests for converting :class:`Request` into effects.
-    """
-
-    def assertCompileTo(self, conv_requests, expected_effects):
-        """
-        Assert that the given convergence requests compile down to a parallel
-        effect comprised of the given effects.
-        """
-        effect = _reqs_to_effect(conv_requests)
-        self.assertEqual(effect, parallel(expected_effects))
-
-    def test_single_request(self):
-        """
-        A single request is correctly compiled down to an effect.
-        """
-        conv_requests = [
-            Request(service=ServiceType.CLOUD_LOAD_BALANCERS,
-                    method="GET",
-                    path="/whatever",
-                    success_pred=has_code(999))]
-        expected_effects = [
-            service_request(
-                service_type=ServiceType.CLOUD_LOAD_BALANCERS,
-                method="GET",
-                url="/whatever",
-                headers=None,
-                data=None,
-                success_pred=has_code(999))]
-        self.assertCompileTo(conv_requests, expected_effects)
-
-    def test_multiple_requests(self):
-        """
-        Multiple requests of the same type are correctly compiled down to an
-        effect.
-        """
-        conv_requests = [
-            Request(service=ServiceType.CLOUD_LOAD_BALANCERS,
-                    method="GET",
-                    path="/whatever"),
-            Request(service=ServiceType.CLOUD_LOAD_BALANCERS,
-                    method="GET",
-                    path="/whatever/something/else",
-                    success_pred=has_code(231))]
-        expected_effects = [
-            service_request(
-                service_type=ServiceType.CLOUD_LOAD_BALANCERS,
-                method="GET",
-                url="/whatever",
-                headers=None,
-                data=None),
-            service_request(
-                service_type=ServiceType.CLOUD_LOAD_BALANCERS,
-                method="GET",
-                url="/whatever/something/else",
-                headers=None,
-                data=None,
-                success_pred=has_code(231))]
-        self.assertCompileTo(conv_requests, expected_effects)
-
-    def test_multiple_requests_of_different_type(self):
-        """
-        Multiple requests of different types are correctly compiled down to
-        an effect.
-        """
-        data_sentinel = object()
-        conv_requests = [
-            Request(service=ServiceType.CLOUD_LOAD_BALANCERS,
-                    method="GET",
-                    path="/whatever"),
-            Request(service=ServiceType.CLOUD_LOAD_BALANCERS,
-                    method="GET",
-                    path="/whatever/something/else",
-                    success_pred=has_code(231)),
-            Request(service=ServiceType.CLOUD_SERVERS,
-                    method="POST",
-                    path="/xyzzy",
-                    data=data_sentinel)]
-        expected_effects = [
-            service_request(
-                service_type=ServiceType.CLOUD_LOAD_BALANCERS,
-                method="GET",
-                url="/whatever",
-                headers=None,
-                data=None),
-            service_request(
-                service_type=ServiceType.CLOUD_LOAD_BALANCERS,
-                method="GET",
-                url="/whatever/something/else",
-                headers=None,
-                data=None,
-                success_pred=has_code(231)),
-            service_request(
-                service_type=ServiceType.CLOUD_SERVERS,
-                method="POST",
-                url="/xyzzy",
-                headers=None,
-                data=data_sentinel)]
-        self.assertCompileTo(conv_requests, expected_effects)
+from otter.convergence.effecting import steps_to_effect
+from otter.convergence.steps import IStep
 
 
 @implementer(IStep)
-class Steppy(object):
-    """A dummy step."""
-    def as_request(self):
-        """Return a simple GET .../whatever on CLOUD_LOAD_BALANCERS"""
-        return Request(service=ServiceType.CLOUD_LOAD_BALANCERS,
-                       method="GET",
-                       path="whatever")
+class _Steppy(object):
+    def as_effect(self):
+        return Effect(Constant(None))
 
 
 class StepsToEffectTests(SynchronousTestCase):
     """Tests for :func:`steps_to_effect`"""
     def test_uses_step_request(self):
         """Steps are converted to requests."""
-        steps = [Steppy(), Steppy()]
-        expected_effects = [
-            service_request(
-                service_type=ServiceType.CLOUD_LOAD_BALANCERS,
-                method="GET",
-                url="whatever",
-                headers=None,
-                data=None),
-            service_request(
-                service_type=ServiceType.CLOUD_LOAD_BALANCERS,
-                method="GET",
-                url="whatever",
-                headers=None,
-                data=None),
-        ]
+        steps = [_Steppy(), _Steppy()]
+        expected_effects = [Effect(Constant(None))] * 2
         effect = steps_to_effect(steps)
         self.assertEqual(effect, parallel(expected_effects))
--- otter/test/convergence/test_steps.py
+++ otter/test/convergence/test_steps.py
@@ -12,9 +12,8 @@ from otter.convergence.steps import (
     CreateServer,
     DeleteServer,
     RemoveFromCLB,
-    Request,
     SetMetadataItemOnServer)
-from otter.http import has_code
+from otter.http import has_code, service_request
 
 
 class StepAsRequestTests(SynchronousTestCase):
@@ -24,62 +23,64 @@ class StepAsRequestTests(SynchronousTestCase):
 
     def test_create_server(self):
         """
-        :obj:`CreateServer.as_request` produces a request for creating a server.
+        :obj:`CreateServer.as_request` produces a request for creating a
+        server.
         """
-        create = CreateServer(launch_config=pmap({'name': 'myserver', 'flavorRef': '1'}))
+        create = CreateServer(
+            launch_config=pmap({'name': 'myserver', 'flavorRef': '1'}))
         self.assertEqual(
-            create.as_request(),
-            Request(
-                service=ServiceType.CLOUD_SERVERS,
-                method='POST',
-                path='servers',
+            create.as_effect(),
+            service_request(
+                ServiceType.CLOUD_SERVERS,
+                'POST',
+                'servers',
                 data=pmap({'name': 'myserver', 'flavorRef': '1'})))
 
     def test_delete_server(self):
         """
-        :obj:`DeleteServer.as_request` produces a request for deleting a server.
+        :obj:`DeleteServer.as_effect` produces a request for deleting a server.
         """
         delete = DeleteServer(server_id='abc123')
         self.assertEqual(
-            delete.as_request(),
-            Request(
-                service=ServiceType.CLOUD_SERVERS,
-                method='DELETE',
-                path='servers/abc123'))
+            delete.as_effect(),
+            service_request(
+                ServiceType.CLOUD_SERVERS,
+                'DELETE',
+                'servers/abc123'))
 
     def test_set_metadata_item(self):
         """
-        :obj:`SetMetadataItemOnServer.as_request` produces a request for
+        :obj:`SetMetadataItemOnServer.as_effect` produces a request for
         setting a metadata item on a particular server.
         """
         meta = SetMetadataItemOnServer(server_id='abc123', key='metadata_key',
                                        value='teapot')
         self.assertEqual(
-            meta.as_request(),
-            Request(
-                service=ServiceType.CLOUD_SERVERS,
-                method='PUT',
-                path='servers/abc123/metadata/metadata_key',
+            meta.as_effect(),
+            service_request(
+                ServiceType.CLOUD_SERVERS,
+                'PUT',
+                'servers/abc123/metadata/metadata_key',
                 data={'meta': {'metadata_key': 'teapot'}}))
 
     def test_remove_from_load_balancer(self):
         """
-        :obj:`RemoveFromCLB.as_request` produces a request for
+        :obj:`RemoveFromCLB.as_effect` produces a request for
         removing a node from a load balancer.
         """
         lbremove = RemoveFromCLB(
             lb_id='abc123',
             node_id='node1')
         self.assertEqual(
-            lbremove.as_request(),
-            Request(
-                service=ServiceType.CLOUD_LOAD_BALANCERS,
-                method='DELETE',
-                path='loadbalancers/abc123/node1'))
+            lbremove.as_effect(),
+            service_request(
+                ServiceType.CLOUD_LOAD_BALANCERS,
+                'DELETE',
+                'loadbalancers/abc123/node1'))
 
     def test_change_load_balancer_node(self):
         """
-        :obj:`ChangeCLBNode.as_request` produces a request for
+        :obj:`ChangeCLBNode.as_effect` produces a request for
         modifying a load balancer node.
         """
         changenode = ChangeCLBNode(
@@ -89,11 +90,11 @@ class StepAsRequestTests(SynchronousTestCase):
             weight=50,
             type="PRIMARY")
         self.assertEqual(
-            changenode.as_request(),
-            Request(
-                service=ServiceType.CLOUD_LOAD_BALANCERS,
-                method='PUT',
-                path='loadbalancers/abc123/nodes/node1',
+            changenode.as_effect(),
+            service_request(
+                ServiceType.CLOUD_LOAD_BALANCERS,
+                'PUT',
+                'loadbalancers/abc123/nodes/node1',
                 data={'condition': 'DRAINING',
                       'weight': 50}))
 
@@ -114,13 +115,14 @@ class StepAsRequestTests(SynchronousTestCase):
             ("lb-3", "node-c"),
             ("lb-3", "node-d")
         ]))
-        request = step.as_request()
-        self.assertEqual(request.service, ServiceType.RACKCONNECT_V3)
-        self.assertEqual(request.method, expected_method)
-        expected_code = 201 if request.method == "POST" else 204
-        self.assertEqual(request.success_pred, has_code(expected_code))
-        self.assertEqual(request.path, "load_balancer_pools/nodes")
-        self.assertEqual(request.headers, None)
+        request = step.as_effect()
+        self.assertEqual(request.intent.service_type,
+                         ServiceType.RACKCONNECT_V3)
+        self.assertEqual(request.intent.method, expected_method)
+        expected_code = 201 if request.intent.method == "POST" else 204
+        self.assertEqual(request.intent.success_pred, has_code(expected_code))
+        self.assertEqual(request.intent.url, "load_balancer_pools/nodes")
+        self.assertEqual(request.intent.headers, None)
 
         expected_data = [
             {'load_balancer_pool': {'id': 'lb-1'},
@@ -140,13 +142,14 @@ class StepAsRequestTests(SynchronousTestCase):
             {'load_balancer_pool': {'id': 'lb-3'},
              'cloud_server': {'id': 'node-d'}}
         ]
-        key_fn = lambda e: (e["load_balancer_pool"]["id"], e["cloud_server"]["id"])
-        request_data = sorted(request.data, key=key_fn)
+        key_fn = lambda e: (e["load_balancer_pool"]["id"],
+                            e["cloud_server"]["id"])
+        request_data = sorted(request.intent.data, key=key_fn)
         self.assertEqual(request_data, expected_data)
 
     def test_add_nodes_to_rcv3_load_balancers(self):
         """
-        :obj:`BulkAddToRCv3.as_request` produces a request for
+        :obj:`BulkAddToRCv3.as_effect` produces a request for
         adding any combination of nodes to any combination of RCv3 load
         balancers.
         """
@@ -154,7 +157,7 @@ class StepAsRequestTests(SynchronousTestCase):
 
     def test_remove_nodes_from_rcv3_load_balancers(self):
         """
-        :obj:`BulkRemoveFromRCv3.as_request` produces a request
+        :obj:`BulkRemoveFromRCv3.as_effect` produces a request
         for removing any combination of nodes from any combination of RCv3
         load balancers.
         """
