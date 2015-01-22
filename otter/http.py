009cdd8782087593fd3df9dbf3b34978c1170777
Laurens Van Houtven <_@lvh.io>
2015-01-22 13:43:45 -0800

    All done

--- otter/http.py
+++ otter/http.py
@@ -150,13 +150,13 @@ def concretize_service_request(
             invalidate_eff, service_request.reauth_codes, request_)
         request_ = add_error_handling(
             service_request.success_pred, request_)
+        request_ = add_json_request_data(request_)
         request_ = add_bind_service(
             catalog,
             service_name,
             region,
             log,
-            add_json_request_data(
-                request_))
+            request_)
         if service_request.json_response:
             request_ = add_json_response(request_)
         request_ = add_content_only(request_)
