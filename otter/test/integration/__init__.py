d1a58f6b7174bfb42c36340e2918b2e05f2e2eab
Laurens Van Houtven <_@lvh.io>
2015-01-23 15:48:57 -0800

    Move to utils.py

--- otter/test/integration/__init__.py
+++ otter/test/integration/__init__.py
@@ -1,16 +1,3 @@
 """
 Black-box integration testing of convergence.
 """
-
-
-def measure_progress(previous_state, current_state, desired_state):
-    """
-    How many steps have been made towards the desired state between
-    the previous and current states?
-
-    :param previous_state: The previous state of a scaling group.
-    :param current_state: The current state of a scaling group.
-    :param desired_state: The desired state of a scaling group.
-    :return: The number of steps made towards the desired.
-    :rtype: int
-    """
---
+++ otter/test/integration/utils.py
@@ -0,0 +1,34 @@
+from characteristic import Attribute, attributes
+
+from pyrsistent import pset
+
+from otter.convergence.model import NovaServer
+
+@attributes([
+    Attribute("servers"),
+    Attribute("lb_connections")
+])
+class GroupState(object):
+    """
+    The externally visible state of a group at a point in time.
+
+    :attr pset servers: Set of the servers in the group.
+    :attr pset lb_nodes: Set of the load balancer nodes in the group.
+    """
+
+def measure_progress(previous_state, current_state, desired_state):
+    """
+    How many steps have been made towards the desired state between
+    the previous and current states?
+
+    XXX: The NovaServer instances we get from GroupState don't
+    describe their own flavor/image, making it impossible to see if
+    the new servers are of the correct type.
+
+    :param GroupState previous_state: The previous state of a scaling group.
+    :param GroupState current_state: The current state of a scaling group.
+    :param DesiredState desired_state: The desired state of a scaling group.
+    :return: The number of steps made towards the desired.
+    :rtype: int
+
+    """
