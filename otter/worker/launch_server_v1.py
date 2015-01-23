9cae31acaf59de1a217778bb5fee395dccaa16c0
Christopher Armstrong <chris.armstrong@rackspace.com>
2015-01-23 15:32:37 -0600

    get rid of the change_name parameter for now

--- otter/worker/launch_server_v1.py
+++ otter/worker/launch_server_v1.py
@@ -545,7 +545,7 @@ def add_to_load_balancers(log, request_func, lb_configs, server, undo):
     return d.addCallback(partial(zip, lb_configs))
 
 
-def prepare_launch_config(scaling_group_uuid, launch_config, change_name=True):
+def prepare_launch_config(scaling_group_uuid, launch_config):
     """
     Prepare a launch_config for the specified scaling_group.
 
@@ -556,9 +556,6 @@ def prepare_launch_config(scaling_group_uuid, launch_config, change_name=True):
         getting launched for.
     :param dict launch_config: The complete launch_config args we want to build
         servers from.
-    :param change_name: If True, the name will be randomly generated. If a name
-        already exists in the config, the random string will be appended. Note
-        that this function becomes impure if this argument is True.
 
     :return dict: The prepared launch config.
     """
@@ -571,10 +568,10 @@ def prepare_launch_config(scaling_group_uuid, launch_config, change_name=True):
     server_config['metadata'].update(generate_server_metadata(
         scaling_group_uuid, launch_config))
 
-    if change_name:
-        suffix = generate_server_name()
-        launch_config = thaw(prepare_server_name(freeze(launch_config),
-                                                 suffix))
+    suffix = generate_server_name()
+    launch_config = thaw(prepare_server_name(freeze(launch_config),
+                                             suffix))
+
     for lb_config in launch_config.get('loadBalancers', []):
         if 'metadata' not in lb_config:
             lb_config['metadata'] = {}
