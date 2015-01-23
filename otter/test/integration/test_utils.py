f985bcc20fca08bb043c56d3147c55ca1c056ccf
Laurens Van Houtven <_@lvh.io>
2015-01-23 14:51:14 -0800

    Stub MeasureProgress tests

---
+++ otter/test/integration/test_utils.py
@@ -0,0 +1,10 @@
+"""
+Tests for the utility functions for convergence black-box testing.
+"""
+from twisted.trial.unittest import SynchronousTestCase
+
+
+class MeasureProgressTests(SynchronousTestCase):
+    """
+    Tests for :func:`measure_progress`.
+    """
