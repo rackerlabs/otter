e87516cab5ae3456f9e2bcc60115d5efa1f33dde
Laurens Van Houtven <_@lvh.io>
2015-01-23 16:30:07 -0800

    Add undershooting test

--- otter/test/integration/test_utils.py
+++ otter/test/integration/test_utils.py
@@ -96,3 +96,24 @@ class MeasureProgressTests(SynchronousTestCase):
         self.assertRaises(
             AssertionError,
             measure_progress, previous_state, current_state, desired_state)
+
+    def test_undershoot(self):
+        """
+        When undershooting the desired capacity (group was above desired,
+        and is now below desired), no progress was made.
+        """
+        previous_state = GroupState(
+            servers=self._create_servers(6),
+            lb_connections=pset([]) # TODO: servers should already be attached to their load balancers
+        )
+        current_state = GroupState(
+            servers=self._create_servers(4),
+            lb_connections=pset([]) # TODO: old servers should still be attached
+        )
+        desired_state = DesiredGroupState(
+            launch_config=pmap(),
+            desired=5,
+        )
+        self.assertRaises(
+            AssertionError,
+            measure_progress, previous_state, current_state, desired_state)
