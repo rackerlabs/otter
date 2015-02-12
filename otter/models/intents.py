4efd839265665965ca056a88d014defd5691c580
Christopher Armstrong <chris.armstrong@rackspace.com>
2015-02-11 20:32:17 -0600

    add a ModifyGroupState intent and performer.

---
+++ otter/models/intents.py
@@ -0,0 +1,16 @@
+from characteristic import attributes
+
+from effect.twisted import deferred_performer
+
+
+@attributes(['scaling_group', 'modifier'])
+class ModifyGroupState(object):
+    """
+    An Effect intent which indicates that a group state should be updated.
+    """
+
+
+@deferred_performer
+def perform_modify_group_state(dispatcher, mgs):
+    """Perform an :obj:`UpdateGroupState`."""
+    return mgs.scaling_group.modify_state(mgs.modifier)
