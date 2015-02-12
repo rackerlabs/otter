4a4e0a1f55f4cb3b114e0ab63353093f405394a1
Laurens Van Houtven <_@lvh.io>
2015-02-11 16:11:32 -0800

    Refactor test_no_such_lb_message

--- otter/test/convergence/test_steps.py
+++ otter/test/convergence/test_steps.py
@@ -490,34 +490,7 @@ class RCv3CheckBulkDeleteTests(SynchronousTestCase):
         The regex for parsing messages saying the load balancer doesn't
         exist, parses those messages. It rejects other messages.
         """
-        match = _RCV3_LB_DOESNT_EXIST_PATTERN.match
-
-        test_data = [
-            ("Load Balancer Pool d95ae0c4-6ab8-4873-b82f-f8433840cff2 does "
-             "not exist",
-             ("d95ae0c4-6ab8-4873-b82f-f8433840cff2",)),
-            ("Load Balancer Pool D6D3AA7C-DFA5-4E61-96EE-1D54AC1075D2 does "
-             "not exist",
-             ("D6D3AA7C-DFA5-4E61-96EE-1D54AC1075D2",))
-        ]
-
-        for message, expected_groups in test_data:
-            res = match(message)
-            self.assertNotIdentical(res, None)
-            self.assertEqual(res.groups(), expected_groups)
-
-        for message in ["Load Balancer Pool {lb_id} is not in an ACTIVE state"
-                        .format(lb_id=lb_id) for lb_id in
-                        ['d95ae0c4-6ab8-4873-b82f-f8433840cff2',
-                         'D95AE0C4-6AB8-4873-B82F-F8433840CFF2']]:
-            self.assertIdentical(match(message), None)
-
-        for message in [
-                'Node d6d3aa7c-dfa5-4e61-96ee-1d54ac1075d2 is not a member '
-                'of Load Balancer Pool d95ae0c4-6ab8-4873-b82f-f8433840cff2',
-                'Node D6D3AA7C-DFA5-4E61-96EE-1D54AC1075D2 is not a member '
-                'of Load Balancer Pool D95AE0C4-6AB8-4873-B82F-F8433840CFF2']:
-            self.assertIdentical(match(message), None)
+        self._regex_test(_RCV3_LB_DOESNT_EXIST_PATTERN)
 
     def test_good_response(self):
         """
