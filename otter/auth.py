99ea973e31ae229078e4c6295661727961fdf725
Ken Wronkiewicz <ken.wronkiewicz@cloudkick.com>
2015-03-06 15:08:32 -0800

    fix for long line.

--- otter/auth.py
+++ otter/auth.py
@@ -239,7 +239,8 @@ class ImpersonatingAuthenticator(object):
             """
             Log this as a string we know we can find in the logging feed
             """
-            log.err(err, 'Failed to get a new identity admin token.', otter_msg_type='impersonation-failed')
+            log.err(err, 'Failed to get a new identity admin token.', 
+                    otter_msg_type='impersonation-failed')
             return err
 
         if log:
