5e45da04b422e72c2dd0626f282de49866c27ab5
Laurens Van Houtven <_@lvh.io>
2015-03-10 11:56:02 -0700

    Revert "lambda -> def"
    This reverts commit a9fcc9d9c26f56acbeac2425e8283691a24787e9.

--- otter/test/convergence/test_gathering.py
+++ otter/test/convergence/test_gathering.py
@@ -418,12 +418,10 @@ class GetAllConvergenceDataTests(SynchronousTestCase):
         If there are no servers in a group, get_all_convergence_data includes
         an empty list.
         """
+        get_lb = lambda: Effect(Stub(Constant([])))
 
         def get_servers():
             return Effect(Stub(Constant({})))
-
-        def get_lb():
-            return Effect(Stub(Constant([])))
 
         eff = get_all_convergence_data(
             'gid',
