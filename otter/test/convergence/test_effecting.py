a6cc1f3d2cf2610ee99e54ab405f48ca1ccdb807
Christopher Armstrong <chris.armstrong@rackspace.com>
2015-03-19 20:56:13 -0500

    RETRY on unexpected errors

--- otter/test/convergence/test_effecting.py
+++ otter/test/convergence/test_effecting.py
@@ -1,26 +1,41 @@
 """Tests for convergence effecting."""
 
-from effect import Constant, Effect, parallel
+from effect import Constant, Effect, ParallelEffects, ComposedDispatcher, TypeDispatcher, base_dispatcher, sync_perform, Error
+from effect.async import perform_parallel_async
 
 from twisted.trial.unittest import SynchronousTestCase
 
 from zope.interface import implementer
 
 from otter.convergence.effecting import steps_to_effect
+from otter.convergence.model import StepResult
 from otter.convergence.steps import IStep
+from otter.test.utils import transform_eq
 
 
 @implementer(IStep)
 class _Steppy(object):
+    def __init__(self, effect):
+        self.effect = effect
+
     def as_effect(self):
-        return Effect(Constant(None))
+        return self.effect
 
 
 class StepsToEffectTests(SynchronousTestCase):
     """Tests for :func:`steps_to_effect`"""
     def test_uses_step_request(self):
         """Steps are converted to requests."""
-        steps = [_Steppy(), _Steppy()]
-        expected_effects = [Effect(Constant(None))] * 2
+        steps = [_Steppy(Effect(Constant((StepResult.SUCCESS, 'foo')))),
+                 _Steppy(Effect(Error(RuntimeError('uh oh'))))]
         effect = steps_to_effect(steps)
-        self.assertEqual(effect, parallel(expected_effects))
+        self.assertIs(type(effect.intent), ParallelEffects)
+        dispatcher = ComposedDispatcher([
+            base_dispatcher,
+            TypeDispatcher({ParallelEffects: perform_parallel_async}),
+        ])
+        self.assertEqual(
+            sync_perform(dispatcher, effect),
+            [(StepResult.SUCCESS, 'foo'),
+             (StepResult.RETRY, [transform_eq(lambda e: (type(e), e.args),
+                                              (RuntimeError, ('uh oh',)))])])
