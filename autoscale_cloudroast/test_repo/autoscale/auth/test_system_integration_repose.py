05566e1a76b44c191dea097f22260313a69d1395
Laurens Van Houtven <_@lvh.io>
2015-03-25 14:03:37 -0700

    Reflow statement

--- autoscale_cloudroast/test_repo/autoscale/auth/test_system_integration_repose.py
+++ autoscale_cloudroast/test_repo/autoscale/auth/test_system_integration_repose.py
@@ -137,7 +137,8 @@ class AutoscaleReposeTests(AutoscaleFixture):
         Verify get scaling groups for an invalid group id through repose
         without a trailing slash, returns response code 404.
         """
-        list_response = self.autoscale_client.list_scaling_groups(self.url + '/groups/76765')
+        list_response = self.autoscale_client.list_scaling_groups(
+            self.url + '/groups/76765')
         self.assertEquals(list_response.status_code, 404,
                           msg='List scaling group returned response code '
                           '{0}'.format(list_response.status_code))
