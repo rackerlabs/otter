69b5bf4c8068957f5838465989f6b12428a99946
Laurens Van Houtven <_@lvh.io>
2015-03-26 10:44:39 -0700

    Reflow assertion

--- autoscale_cloudroast/test_repo/autoscale/auth/test_system_integration_repose_unauthorized.py
+++ autoscale_cloudroast/test_repo/autoscale/auth/test_system_integration_repose_unauthorized.py
@@ -93,11 +93,12 @@ class AutoscaleReposeUnauthTests(AutoscaleFixture):
         Verify GET non existing scaling groups through repose without a
         trailing slash, when unauthorized, returns response code 403.
         """
-        self.assertEquals(list_response.status_code, 403,
-                          msg='List scaling group returned response code {0}'.format(
-                          list_response.status_code))
         list_response = self.autoscale_temp_client.list_scaling_groups(
             self.url + '/groups/76765')
+        self.assertEquals(
+            list_response.status_code, 403,
+            msg='List scaling group returned response code {0}'
+            .format(list_response.status_code))
 
     @tags(type='repose')
     def test_system_repose_unauthorized_execute_webhook(self):
