fa51801c3273b3ca1545f435bde96ec90ade2d0d
Laurens Van Houtven <_@lvh.io>
2015-03-26 10:08:39 -0700

    Fix absurdly long test name

--- autoscale_cloudroast/test_repo/autoscale/auth/test_system_integration_repose_unauthenticated.py
+++ autoscale_cloudroast/test_repo/autoscale/auth/test_system_integration_repose_unauthenticated.py
@@ -34,7 +34,7 @@ class AutoscaleReposeUnauthTests(AutoscaleFixture):
                           .format(limits_response.status_code))
 
     @tags(type='repose')
-    def test_system_repose_unauthenticated_rate_limits_with_trailing_slash(self):
+    def test_rate_limits_with_trailing_slash(self):
         """
         Verify the relative rate limit api with a trailing slash, when
         unauthenticated, returns reponse code 401.
