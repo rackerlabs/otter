68f38a8fea2c733984af049f90ea3660de36ccb3
Laurens Van Houtven <_@lvh.io>
2015-03-30 16:13:15 -0700

    Reflow statement

--- autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_get_scheduler_policies.py
+++ autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_get_scheduler_policies.py
@@ -114,11 +114,12 @@ class GetSchedulerScalingPolicy(ScalingGroupFixture):
         self.assertEquals(
             del_resp.status_code, 204, msg='Delete at-style policy failed for group'
             ' {0}'.format(self.group.id))
-        get_at_style_policy_response = self.autoscale_client.get_policy_details(
-            self.group.id,
-            self.at_style_policy['id'])
         self.assertEquals(get_at_style_policy_response.status_code, 404,
                           msg='get deleted scheduler policy succeeded with: {0},'
                           ' policy/groupid: {1} / {2}'.format(
                               get_at_style_policy_response.status_code, self.group.id,
                               self.at_style_policy['id']))
+        get_at_style_policy_response = (
+            self.autoscale_client.get_policy_details(
+                self.group.id,
+                self.at_style_policy['id']))
