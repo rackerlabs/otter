cde633e7d9efd0d0492cc04d154a25d494214859
Laurens Van Houtven <_@lvh.io>
2015-03-30 13:37:41 -0700

    Reflow expression, get rid of pointless parens

--- autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_delete_schedule_policy.py
+++ autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_delete_schedule_policy.py
@@ -82,4 +82,5 @@ class DeleteSchedulerPolicy(ScalingGroupFixture):
         Lists the policies in a group and returns the policy id list
         """
         list_policies_resp = self.autoscale_client.list_policies(group_id)
-        return [each_policy.id for each_policy in (list_policies_resp.entity).policies]
+        return [each_policy.id for each_policy in
+                list_policies_resp.entity.policies]
