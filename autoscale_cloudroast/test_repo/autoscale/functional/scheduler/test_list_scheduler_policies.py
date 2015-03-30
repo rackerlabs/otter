73586ca0dca55cf707284451f7a1ed04611c3157
Laurens Van Houtven <_@lvh.io>
2015-03-30 13:53:31 -0700

    Reflow statement

--- autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_list_scheduler_policies.py
+++ autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_list_scheduler_policies.py
@@ -18,10 +18,11 @@ class ListSchedulerScalingPolicy(ScalingGroupPolicyFixture):
         super(ListSchedulerScalingPolicy, self).setUp()
         self.at_value = self.autoscale_behaviors.get_time_in_utc(600)
         self.cron_value = '0 */10 * * *'
-        self.at_style_policy = self.autoscale_behaviors.create_schedule_policy_given(
-            group_id=self.group.id,
-            sp_change=self.sp_change,
-            schedule_at=self.at_value)
+        self.at_style_policy = (self.autoscale_behaviors
+                                .create_schedule_policy_given(
+                                    group_id=self.group.id,
+                                    sp_change=self.sp_change,
+                                    schedule_at=self.at_value))
         self.assertEquals(
             self.at_style_policy['status_code'], 201,
             msg='Create schedule policy (at style) failed with {0} for '
