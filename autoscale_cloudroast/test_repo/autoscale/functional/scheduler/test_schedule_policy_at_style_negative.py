f7f24a56a6c78ff26634095fcd1995bca98c587d
Laurens Van Houtven <_@lvh.io>
2015-03-30 18:06:10 -0700

    Reflow assertion

--- autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_schedule_policy_at_style_negative.py
+++ autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_schedule_policy_at_style_negative.py
@@ -89,8 +89,7 @@ class ScheduleScalingPolicyNegative(AutoscaleFixture):
             sp_change=self.sp_change,
             schedule_at=schedule_value)
         self.assertEquals(
-            schedule_policy_at_style['status_code'],
-            400,
+            schedule_policy_at_style['status_code'], 400,
             msg='Create schedule policy via at style with no Z or T in date'
             'results in {0} for group {1}'.format(
                 schedule_policy_at_style['status_code'],
