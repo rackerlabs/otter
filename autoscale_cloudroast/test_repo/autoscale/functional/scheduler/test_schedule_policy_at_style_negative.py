90617126902a1fd3fe0e8456c0df522eb9c62fec
Laurens Van Houtven <_@lvh.io>
2015-03-30 18:10:00 -0700

    Reflow statement

--- autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_schedule_policy_at_style_negative.py
+++ autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_schedule_policy_at_style_negative.py
@@ -142,10 +142,11 @@ class ScheduleScalingPolicyNegative(AutoscaleFixture):
         results in a 400.
         """
         schedule_value = '031260'
-        schedule_policy_at_style = self.autoscale_behaviors.create_schedule_policy_given(
-            group_id=self.group.id,
-            sp_change=self.sp_change,
-            schedule_at=schedule_value)
+        schedule_policy_at_style = (self.autoscale_behaviors
+                                    .create_schedule_policy_given(
+                                        group_id=self.group.id,
+                                        sp_change=self.sp_change,
+                                        schedule_at=schedule_value))
         self.assertEquals(
             schedule_policy_at_style['status_code'],
             400,
