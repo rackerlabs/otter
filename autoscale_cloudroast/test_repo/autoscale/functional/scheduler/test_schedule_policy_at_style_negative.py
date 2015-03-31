a612fa2bd78673839cfd501d2dfb8249f32a846c
Laurens Van Houtven <_@lvh.io>
2015-03-30 18:11:06 -0700

    Reflow statement

--- autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_schedule_policy_at_style_negative.py
+++ autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_schedule_policy_at_style_negative.py
@@ -286,8 +286,8 @@ class ScheduleScalingPolicyNegative(AutoscaleFixture):
 
     def test_schedule_at_style_policy_with_invalid_day(self):
         """
-        Creating a scaling policy of type schedule with (at style) with invalid day in the date
-        results in a 400.
+        Creating a scaling policy of type schedule with (at style) with
+        invalid day in the date results in a 400.
         """
         schedule_value = '2013-12-33T03:12:00Z'
         schedule_policy_at_style = self.autoscale_behaviors.create_schedule_policy_given(
