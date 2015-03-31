0ed4254b495deda1dee088414fa57a0aff6af25b
Laurens Van Houtven <_@lvh.io>
2015-03-30 17:12:50 -0700

    Reflow assertion

--- autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_schedule_policy_at_style_negative.py
+++ autoscale_cloudroast/test_repo/autoscale/functional/scheduler/test_schedule_policy_at_style_negative.py
@@ -266,10 +266,12 @@ class ScheduleScalingPolicyNegative(AutoscaleFixture):
             group_id=self.group.id,
             sp_change=self.sp_change,
             schedule_at=schedule_value)
-        self.assertEquals(schedule_policy_at_style['status_code'], 400,
-                          msg='Create schedule scaling at style policy with invalid year results'
-                          ' in {0} for group {1}'.format(schedule_policy_at_style['status_code'],
-                                                         self.group.id))
+        self.assertEquals(
+            schedule_policy_at_style['status_code'], 400,
+            msg='Create schedule scaling at style policy with invalid year results'
+            ' in {0} for group {1}'.format(
+                schedule_policy_at_style['status_code'],
+                self.group.id))
 
     def test_schedule_at_style_policy_with_invalid_day(self):
         """
