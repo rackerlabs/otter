7b7750e45faa8affa24527f8023875c0d2612726
cyli <cyli@twistedmatrix.com>
2015-04-21 19:30:47 -0700

    Test that the full dispatcher passed to the converge can handle all intents.

--- otter/tap/api.py
+++ otter/tap/api.py
@@ -271,7 +271,7 @@ def makeService(config):
         def on_client_ready(_):
             dispatcher = get_full_dispatcher(reactor, authenticator, log,
                                              get_service_configs(config),
-                                             kz_client, store)
+                                             kz_client, store, supervisor)
             # Setup scheduler service after starting
             scheduler = setup_scheduler(s, store, kz_client)
             health_checker.checks['scheduler'] = scheduler.health_check
