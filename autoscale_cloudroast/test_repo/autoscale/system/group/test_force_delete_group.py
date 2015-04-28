00cc94a0c3df6c6db303e9fb90f4d395d99157de
Manish Tomar <manish.tomar@rackspace.com>
2015-04-27 18:25:03 -0700

    enabled delete group tests

--- autoscale_cloudroast/test_repo/autoscale/system/group/test_force_delete_group.py
+++ autoscale_cloudroast/test_repo/autoscale/system/group/test_force_delete_group.py
@@ -16,7 +16,7 @@ class ForceDeleteGroupTest(AutoscaleFixture):
     params = ['true', 'TRUE', 'TrUe', 'True', 'truE', True]
     invalid_params = [0, '', '$%^#', 'anything', 'truee']
 
-    @tags(speed='slow', convergence='error')
+    @tags(speed='slow', convergence='yes')
     def test_minentities_over_zero(self):
         """
         Force deleting a scaling group with active servers, updates the desired
