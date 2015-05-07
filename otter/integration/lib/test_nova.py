e60c285245107011e736320a56aab5d70705ccc9
cyli <cyli@twistedmatrix.com>
2015-05-07 14:41:52 -0700

    Refactor the nova test so that the fake treq can be used elsewhere.

--- otter/integration/lib/test_nova.py
+++ otter/integration/lib/test_nova.py
@@ -5,12 +5,43 @@ from twisted.internet.defer import succeed
 from twisted.trial.unittest import SynchronousTestCase
 
 from otter.integration.lib.nova import NovaServer
+from otter.util.http import headers
 
 
-class _Response(object):
+class Response(object):
     """Fake response object"""
     def __init__(self, code):
         self.code = code
+
+
+def get_fake_treq(test_case, method, url, expected_args_and_kwargs, response):
+    """
+    Return a fake treq object that would return the given response given
+    the correct request made.
+    """
+    expected_args, expected_kwargs = expected_args_and_kwargs
+    response_object, str_response_body = response
+
+    def requester(_url, *args, **kwargs):
+        test_case.assertEqual(args, expected_args)
+        test_case.assertEqual(kwargs, expected_kwargs)
+        test_case.assertEqual(_url, url)
+        return succeed(response_object)
+
+    class FakeTreq(object):
+        def content(cls, resp):
+            test_case.assertEqual(resp, response_object)
+            return succeed(str_response_body)
+
+        def json_content(cls, resp):
+            test_case.assertEqual(resp, response_object)
+            return succeed(json.loads(str_response_body))
+
+    _treq = FakeTreq()
+
+    setattr(_treq, method, requester)
+
+    return _treq
 
 
 class NovaServerTestCase(SynchronousTestCase):
@@ -29,43 +60,27 @@ class NovaServerTestCase(SynchronousTestCase):
 
         self.rcs = FakeRCS()
         self.server_id = 'server_id'
+        self.expected_kwargs = {
+            'headers': headers('token'),
+            'pool': self.pool
+        }
 
     def get_server(self, method, url, treq_args_kwargs, response, str_body):
         """
         Stub out treq, and return a nova server with
         """
-        def requester(_url, *args, **kwargs):
-            self.assertEqual(["token"],
-                             kwargs.get('headers', {}).get('x-auth-token'))
-            self.assertEqual(self.pool, kwargs.get('pool'))
-            kwargs.pop('headers')
-            kwargs.pop('pool')
-
-            if url == _url and (args, kwargs) == treq_args_kwargs:
-                return succeed(response)
-            self.fail(
-                "Expected a request to {} with args and kwargs: {}\n "
-                "Got a request to {} with args and kwargs: {}."
-                .format(url, treq_args_kwargs, _url, (args, kwargs)))
-
-        class FakeTreq(object):
-            def content(cls, resp):
-                return succeed(str_body)
-
-            def json_content(cls, resp):
-                return succeed(json.loads(str_body))
-
-        _treq = FakeTreq()
-        setattr(_treq, method, requester)
-
-        return NovaServer(id=self.server_id, pool=self.pool, treq=_treq)
+        return NovaServer(id=self.server_id, pool=self.pool,
+                          treq=get_fake_treq(self, method, url,
+                                             treq_args_kwargs,
+                                             (response, str_body)))
 
     def test_delete(self):
         """
         Delete calls the right endpoint and succeeds on 204.
         """
         server = self.get_server('delete', 'novaurl/servers/server_id',
-                                 ((), {}), _Response(204), "delete response")
+                                 ((), self.expected_kwargs),
+                                 Response(204), "delete response")
         d = server.delete(self.rcs)
         self.assertEqual('delete response', self.successResultOf(d))
 
@@ -74,7 +89,8 @@ class NovaServerTestCase(SynchronousTestCase):
         List metadata calls the right endpoint and succeeds on 200.
         """
         server = self.get_server('get', 'novaurl/servers/server_id/metadata',
-                                 ((), {}), _Response(200), '{"metadata": {}}')
+                                 ((), self.expected_kwargs),
+                                 Response(200), '{"metadata": {}}')
         d = server.list_metadata(self.rcs)
         self.assertEqual({'metadata': {}}, self.successResultOf(d))
 
@@ -83,8 +99,9 @@ class NovaServerTestCase(SynchronousTestCase):
         Update metadata calls the right endpoint and succeeds on 200.
         """
         server = self.get_server('put', 'novaurl/servers/server_id/metadata',
-                                 (('{"metadata": {}}',), {}), _Response(200),
-                                 '{"metadata": {}}')
+                                 (('{"metadata": {}}',),
+                                  self.expected_kwargs),
+                                 Response(200), '{"metadata": {}}')
         d = server.update_metadata({}, self.rcs)
         self.assertEqual({'metadata': {}}, self.successResultOf(d))
 
@@ -93,7 +110,7 @@ class NovaServerTestCase(SynchronousTestCase):
         Get addresses calls the right endpoint and succeeds on 200.
         """
         server = self.get_server('get', 'novaurl/servers/server_id/ips',
-                                 ((), {}), _Response(200),
-                                 '{"addresses": {}}')
+                                 ((), self.expected_kwargs),
+                                 Response(200), '{"addresses": {}}')
         d = server.get_addresses(self.rcs)
         self.assertEqual({'addresses': {}}, self.successResultOf(d))
