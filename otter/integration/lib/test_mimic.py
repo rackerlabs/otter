645841cd14e7f8cae0fe4e4219872aaad73eec5e
cyli <cyli@twistedmatrix.com>
2015-05-11 15:26:38 -0700

    Add a mimic api helper for specifying a sequence of behaviors.

--- otter/integration/lib/test_mimic.py
+++ otter/integration/lib/test_mimic.py
@@ -42,3 +42,24 @@ class MimicNovaTestCase(SynchronousTestCase):
         d = MimicNova(pool=self.pool, treq=_treq).change_server_statuses(
             self.rcs, {'id1': 'ERROR', 'id2': 'DELETED'})
         self.assertEqual('successful change response', self.successResultOf(d))
+
+    def test_sequenced_behaviors(self):
+        """
+        Cause a sequence of behaviors, and succeeds on 201.
+        """
+        criteria = [{"server_name": "name_criteria_.*"}]
+        behaviors = [{'name': "behavior name",
+                      'parameters': {"behavior": "params"}}]
+
+        _treq = get_fake_treq(
+            self, 'POST', "mimicnovaurl/behaviors/some_event",
+            ((json.dumps({'criteria': criteria,
+                          'name': "sequence",
+                          'parameters': {"behaviors": behaviors}}),),
+             self.expected_kwargs),
+            (Response(201), "successful behavior response"))
+
+        d = MimicNova(pool=self.pool, treq=_treq).sequenced_behaviors(
+            self.rcs, criteria, behaviors, event_description="some_event")
+        self.assertEqual('successful behavior response',
+                         self.successResultOf(d))
