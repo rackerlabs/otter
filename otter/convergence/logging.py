0a7b5ad2b493556d960f758ef3b14acce96a0c65
Christopher Armstrong <chris.armstrong@rackspace.com>
2015-06-10 11:55:53 -0500

    log ChangeCLBNode

--- otter/convergence/logging.py
+++ otter/convergence/logging.py
@@ -10,11 +10,19 @@ from toolz.curried import groupby
 from toolz.itertoolz import concat
 
 from otter.convergence.steps import (
-    AddNodesToCLB, CreateServer, DeleteServer, RemoveNodesFromCLB)
+    AddNodesToCLB, ChangeCLBNode, CreateServer, DeleteServer,
+    RemoveNodesFromCLB)
 from otter.log.cloudfeeds import cf_msg
 
 
+# Comments: - it kinda sucks that we're using separate effects for all of
+# these, maybe? CF has an API for sending a bunch of events at once and it'd be
+# good to use it. OTOH it could also be implemented at the logging observer
+# layer by using a "nagle".
+
+
 _loggers = {}
+
 
 def _logger(step_type):
     def _add_to_loggers(f):
@@ -24,9 +32,8 @@ def _logger(step_type):
 
 @_logger(CreateServer)
 def _(steps):
-    # Here we assume that all of the servers have the same server_config.
-    # Could also change this to support differing server_configs by producing
-    # one log message per config.
+    # XXX RADIX TODO: groupby the server_config so we don't assume they're all
+    # the same.
     return cf_msg(
         'convergence-create-servers',
         num_servers=len(steps),
@@ -34,17 +41,18 @@ def _(steps):
     )
 
 
+# Intentionally leaving out SetMetadataItemOnServer for now, since it seems
+# kind of low-level
+
 @_logger(DeleteServer)
-def _(steps):
+def _log_delete_servers(steps):
     return cf_msg(
         'convergence-delete-servers',
         server_ids=sorted([s.server_id for s in steps]))
 
-## Intentionally leaving out SetMetadataItemOnServer for now, since it seems
-## kind of low-level
 
 @_logger(AddNodesToCLB)
-def _(steps):
+def _log_add_nodes_clb(steps):
     lbs = defaultdict(list)
     for step in steps:
         for (address, config) in step.address_configs:
@@ -60,12 +68,27 @@ def _(steps):
 
 
 @_logger(RemoveNodesFromCLB)
-def _(steps):
+def _log_remove_from_clb(steps):
     lbs = groupby(lambda s: s.lb_id, steps)
     effs = [
         cf_msg('convergence-remove-nodes-from-clb',
                lb_id=lb, nodes=sorted(concat(s.node_ids for s in lbsteps)))
         for lb, lbsteps in sorted(lbs.iteritems())]
+    return parallel(effs)
+
+
+@_logger(ChangeCLBNode)
+def _log_change_clb_node(steps):
+    lbs = groupby(lambda s: (s.lb_id, s.condition, s.weight, s.type),
+                  steps)
+    effs = [
+        cf_msg('convergence-change-clb-nodes',
+               lb_id=lb,
+               nodes=', '.join(sorted([s.node_id for s in grouped_steps])),
+               condition=condition.name, weight=weight, type=node_type.name)
+        for (lb, condition, weight, node_type), grouped_steps
+        in sorted(lbs.iteritems())
+    ]
     return parallel(effs)
 
 
--- otter/test/convergence/test_logging.py
+++ otter/test/convergence/test_logging.py
@@ -6,28 +6,30 @@ from pyrsistent import pbag, pmap, pset
 from twisted.trial.unittest import SynchronousTestCase
 
 from otter.convergence.logging import log_steps
-from otter.convergence.model import CLBDescription
+from otter.convergence.model import (
+    CLBDescription, CLBNodeCondition, CLBNodeType)
 from otter.convergence.steps import (
-    AddNodesToCLB, CreateServer, DeleteServer, RemoveNodesFromCLB
+    AddNodesToCLB, ChangeCLBNode, CreateServer, DeleteServer,
+    RemoveNodesFromCLB
 )
-from otter.log.intents import Log, LogErr
+from otter.log.intents import Log
 from otter.test.utils import noop, test_dispatcher
 
 
 def _clbd(lbid, port):
     return CLBDescription(lb_id=lbid, port=port)
 
+
 class LogStepsTests(SynchronousTestCase):
-    def assert_logs(self, eff, intents):
+    def assert_logs(self, steps, intents):
         sequence = SequenceDispatcher([(intent, noop) for intent in intents])
         with sequence.consume():
-            sync_perform(test_dispatcher(sequence), eff)
+            sync_perform(test_dispatcher(sequence), log_steps(steps))
 
     def test_create_servers(self):
         cfg = {'configgy': 'configged'}
         creates = pbag([CreateServer(server_config=pmap(cfg))] * 3)
-        eff = log_steps(creates)
-        self.assert_logs(eff, [
+        self.assert_logs(creates, [
             Log('convergence-create-servers',
                 fields={'num_servers': 3, 'server_config': cfg,
                         'cloud_feed': True})])
@@ -36,11 +38,10 @@ class LogStepsTests(SynchronousTestCase):
         deletes = pbag([DeleteServer(server_id='1'),
                         DeleteServer(server_id='2'),
                         DeleteServer(server_id='3')])
-        eff = log_steps(deletes)
-        self.assert_logs(eff, [
+        self.assert_logs(deletes, [
             Log('convergence-delete-servers',
                 fields={'server_ids': ['1', '2', '3'],
-                'cloud_feed': True})
+                        'cloud_feed': True})
         ])
 
     def test_add_nodes_to_clbs(self):
@@ -54,7 +55,7 @@ class LogStepsTests(SynchronousTestCase):
             AddNodesToCLB(
                 lb_id='lbid2',
                 address_configs=pset([('10.0.0.1', _clbd('lbid2', 4321))]))])
-        self.assert_logs(log_steps(adds), [
+        self.assert_logs(adds, [
             Log('convergence-add-nodes-to-clb',
                 fields={'lb_id': 'lbid1',
                         'addresses': '10.0.0.1:1234, 10.0.0.2:1235',
@@ -71,7 +72,7 @@ class LogStepsTests(SynchronousTestCase):
             RemoveNodesFromCLB(lb_id='lbid2', node_ids=pset(['d', 'e', 'f']))
         ])
 
-        self.assert_logs(log_steps(removes), [
+        self.assert_logs(removes, [
             Log('convergence-remove-nodes-from-clb',
                 fields={'lb_id': 'lbid1',
                         'nodes': ['a', 'b', 'c'],
@@ -80,4 +81,44 @@ class LogStepsTests(SynchronousTestCase):
                 fields={'lb_id': 'lbid2',
                         'nodes': ['d', 'e', 'f'],
                         'cloud_feed': True}),
+        ])
+
+    def test_change_clb_node(self):
+        changes = pbag([
+            ChangeCLBNode(lb_id='lbid1', node_id='node1',
+                          condition=CLBNodeCondition.DRAINING,
+                          type=CLBNodeType.PRIMARY,
+                          weight=50),
+            ChangeCLBNode(lb_id='lbid1', node_id='node2',
+                          condition=CLBNodeCondition.DRAINING,
+                          type=CLBNodeType.PRIMARY,
+                          weight=50),
+            ChangeCLBNode(lb_id='lbid1', node_id='node3',
+                          condition=CLBNodeCondition.ENABLED,
+                          type=CLBNodeType.PRIMARY,
+                          weight=50),
+            ChangeCLBNode(lb_id='lbid2', node_id='node4',
+                          condition=CLBNodeCondition.ENABLED,
+                          type=CLBNodeType.PRIMARY,
+                          weight=50),
+        ])
+        self.assert_logs(changes, [
+            Log('convergence-change-clb-nodes',
+                fields={
+                    'lb_id': 'lbid1', 'nodes': 'node3',
+                    'type': 'PRIMARY', 'condition': 'ENABLED', 'weight': 50,
+                    'cloud_feed': True,
+                }),
+            Log('convergence-change-clb-nodes',
+                fields={
+                    'lb_id': 'lbid1', 'nodes': 'node1, node2',
+                    'type': 'PRIMARY', 'condition': 'DRAINING', 'weight': 50,
+                    'cloud_feed': True,
+                }),
+            Log('convergence-change-clb-nodes',
+                fields={
+                    'lb_id': 'lbid2', 'nodes': 'node4',
+                    'type': 'PRIMARY', 'condition': 'ENABLED', 'weight': 50,
+                    'cloud_feed': True,
+                }),
         ])
