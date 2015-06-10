0794f65ccd4f573ed41de493dc27692e8e268d52
Lindsay K <lakusmierek@gmail.com>
2015-06-10 16:19:19 -0700

    reference PR#

--- otter/integration/tests/test_convergence.py
+++ otter/integration/tests/test_convergence.py
@@ -1281,7 +1281,7 @@ class ConvergenceTestsWith1CLB(unittest.TestCase):
             )
         )
 
-    # @skip_me("Otter does not yet support this error transition")
+    @skip_me("Skipped until this error transition is fixed in Otter #1475")
     @skip_if(not_mimic, "This requires Mimic for error injection.")
     @tag("CATC-023")
     def test_scale_up_when_pending_delete(self):
