38fd01f397f46b46baa5fc22493996107ef18886
Manish Tomar <manish.tomar@rackspace.com>
2015-06-18 15:23:58 -0700

    started servers cache

--- otter/models/cass.py
+++ otter/models/cass.py
@@ -1702,6 +1702,65 @@ class CassScalingGroupCollection:
         return d
 
 
+@implementer(IScalingGroupServersCache)
+class CassScalingGroupServersCache(object):
+    """
+    Collection of cache of scaling group servers
+    """
+
+    def __init__(self, tenant_id, group_id):
+        self.tenant_id = tenant_id
+        self.group_id = group_id
+        self.table = "servers_cache"
+        self.params = {"tenant_id": tenant_id, "group_id": group_id}
+
+    @do
+    def get_servers(self):
+        """
+        See :method:`IScalingGroupServersCache.get_servers`
+        """
+        query = ("SELECT server_blob, last_update FROM {cf} "
+                 "WHERE tenant_id=:tenant_id AND group_id=:group_id "
+                 "ORDER BY last_update DESC;")
+        rows = yield cql_eff(query.format(cf=self.table), self.params)
+        if len(rows) == 0:
+            yield do_return(([], None))
+        last_update = rows[0]['last_update']
+        rows = take_while(lambda r: r['last_update'] == last_update, rows)
+        yield do_return(([json.loads(r['server_blob']) for r in rows],
+                         last_update))
+
+    def insert_servers(self, last_update, servers, clear_others):
+        """
+        See :method:`IScalingGroupServersCache.insert_servers`
+        """
+        if len(servers) == 0:
+            return Effect(Constant(None))
+        query = ("INSERT INTO {cf} (tenant_id, group_id, last_update, "
+                 "server_id, server_blob) "
+                 "VALUES(:tenant_id, :group_id, :last_update, :server_id{i}, "
+                 ":server_blob{i});")
+        params = merge(self.params, {"last_update": last_update})
+        queries = []
+        for i, server in enumerate(servers):
+            params['server_id{}'.format(i)] = server['id']
+            params['server_blob{}'.format(i)] = json.dumps(server)
+            queries.append(query.format(cf=self.table, i=i))
+        if clear_others:
+            return self.delete_servers().on(
+                lambda _: cql_eff(batch(queries), params))
+        else:
+            return cql_eff(batch(queries), params)
+
+    def delete_servers(self):
+        """
+        See :method:`IScalingGroupServersCache.delete_servers`
+        """
+        query = ("DELETE FROM {cf} WHERE tenant_id=:tenant_id AND "
+                 "group_id=:group_id;")
+        return cql_eff(query.format(cf=self.table), self.params)
+
+
 @implementer(IAdmin)
 class CassAdmin(object):
     """
