7efdcf3824a4bc3ea8482569f8c2a0365d833105
cyli <cyli@twistedmatrix.com>
2015-06-30 23:45:13 -0700

    Add test to ensure Autoscale does not "correct" non-autoscale nodes.

--- otter/integration/tests/test_lbsh.py
+++ otter/integration/tests/test_lbsh.py
@@ -5,7 +5,12 @@ Tests covering the Load Balancer self healing behaviors
 from __future__ import print_function
 
 from testtools.matchers import (
-    MatchesAll
+    AfterPreprocessing,
+    ContainsDict,
+    Equals,
+    MatchesAll,
+    MatchesRegex,
+    MatchesSetwise
 )
 
 from twisted.internet.defer import gatherResults, inlineCallbacks
@@ -33,6 +38,7 @@ class TestLoadBalancerSelfHealing(unittest.TestCase):
     This class contains test cases to test the load balancer healing
     function of the Otter Converger.
     """
+    timeout = 1800
 
     def setUp(self):
         """
@@ -53,7 +59,7 @@ class TestLoadBalancerSelfHealing(unittest.TestCase):
             for clb in self.helper.clbs])
         )
 
-    @tag("LBSH")
+    @tag("LBSH-001")
     @inlineCallbacks
     def test_oob_deleted_clb_node(self):
         """
@@ -94,6 +100,84 @@ class TestLoadBalancerSelfHealing(unittest.TestCase):
             MatchesAll(
                 HasLength(1),
                 ContainsAllIPs([the_node["address"]])
+            ),
+            timeout=timeout_default
+        )
+
+    @tag("LBSH-004")
+    @inlineCallbacks
+    def test_only_autoscale_nodes_are_modified(self):
+        """
+        Autoscale only self-heals the nodes that it added, without touching
+        any other nodes.  Assuming 1 CLB:
+
+        1. Create two non-autoscaled servers and add them to the CLB.
+        2. Wait for all servers to be on the CLB
+        3. Create a scaling group with said CLB and 1 server
+        4. Wait for AS server to be active and on the CLB.
+        4. Delete autoscaled server and 1 non-autoscaled server from the CLB
+        5. Converge
+        6. Assert that the autoscaled server is put back on the CLB, the
+           non-autoscaled server is left off the CLB, and the untouched
+           non-autoscaled server is left on the CLB.
+        """
+        clb = self.helper.clbs[0]
+
+        nodes = yield clb.list_nodes(self.rcs)
+        self.assertEqual(len(nodes['nodes']), 0,
+                         "There should be no nodes on the CLB yet.")
+
+        # create the other two non-autoscaled servers - just wait until they
+        # have servicenet addresses - don't bother waiting for them to be
+        # active, which will take too long
+        other_servers = yield self.helper.create_servers(
+            self.rcs, 2, wait_for=ContainsDict({
+                "addresses": ContainsDict({
+                    'private': MatchesSetwise(
+                        ContainsDict({
+                            "addr": MatchesRegex("(\d+\.){3}\d+")
+                        })
+                    )
+                })
+            }))
+        # add non-autoscaled servers to the CLB
+        clb_response = yield clb.add_nodes(
+            self.rcs,
+            [{'address': server['addresses']['private'][0]['addr'],
+              'port': 8080,
+              'condition': "ENABLED"} for server in other_servers])
+        remove_non_as_node, untouch_non_as_node = clb_response['nodes']
+
+        # set up the group and get the group's server's CLB node
+        group, _ = self.helper.create_group(min_entities=1)
+        yield self.helper.start_group_and_wait(group, self.rcs)
+
+        # Should be 3 nodes now that all servers are added
+        nodes = yield clb.wait_for_nodes(
+            self.rcs, AfterPreprocessing(len, Equals(3)), timeout_default)
+        as_node = [node for node in nodes
+                   if node not in (remove_non_as_node, untouch_non_as_node)][0]
+
+        # delete 1 autoscale node and 1 non-autoscale node
+        yield clb.delete_nodes(self.rcs,
+                               [as_node['id'], remove_non_as_node['id']])
+        # There should be 1 node left
+        yield clb.wait_for_nodes(
+            self.rcs, AfterPreprocessing(len, Equals(1)), timeout_default)
+
+        yield group.trigger_convergence(self.rcs)
+
+        yield clb.wait_for_nodes(
+            self.rcs,
+            MatchesSetwise(  # means there are only these two nodes and no more
+                # the untouched node should remain exactly the same
+                Equals(untouch_non_as_node),
+                # the AS node should have the same paramters, but not the same
+                # ID since it was re-added
+                ContainsDict({
+                    k: Equals(v) for k, v in as_node.items()
+                    if k in ('address', 'port', 'weight' 'type', 'condition')
+                })
             ),
             timeout=timeout_default
         )
