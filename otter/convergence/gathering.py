fb5ff215cb854b66d75791081b877b1018dc25d3
Christopher Armstrong <chris.armstrong@rackspace.com>
2015-07-29 14:08:13 -0500

    use CLBNode.from_node_json in gathering code

--- otter/convergence/gathering.py
+++ otter/convergence/gathering.py
@@ -203,15 +203,7 @@ def get_clb_contents():
 
     def fetch_drained_feeds((ids, all_lb_nodes)):
         nodes = [
-            CLBNode(
-                node_id=str(node['id']),
-                address=node['address'],
-                description=CLBDescription(
-                    lb_id=str(_id),
-                    port=node['port'],
-                    weight=node.get('weight', 1),
-                    condition=CLBNodeCondition.lookupByName(node['condition']),
-                    type=CLBNodeType.lookupByName(node['type'])))
+            CLBNode.from_node_json(_id, node)
             for _id, nodes in zip(ids, all_lb_nodes) for node in nodes]
         draining = [n for n in nodes
                     if n.description.condition == CLBNodeCondition.DRAINING]
