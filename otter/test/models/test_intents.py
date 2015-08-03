5f6ccda1ec667c9af6f31838afd5d5dba1c49090
Manish Tomar <manish.tomar@rackspace.com>
2015-08-03 14:03:49 -0700

    Test object is not mutated - thanks @cyli

--- otter/test/models/test_intents.py
+++ otter/test/models/test_intents.py
@@ -164,6 +164,8 @@ class ScalingGroupIntentsTests(SynchronousTestCase):
         self.assertIsNone(r)
         # Returned state has updated paused
         modified_state = self.group.modify_state_values[-1]
+        # Returned state object is different than original
+        self.assertIsNot(self.state, modified_state)
         # Nothing else is modified
         self.assertEqual(modified_state.paused, False)
         modified_state.paused = True
