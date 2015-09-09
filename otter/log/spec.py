45b338e2a970b612f423cb9518651161bf324d2d
Manish Tomar <manish.tomar@rackspace.com>
2015-09-09 13:53:40 -0700

    added new conv log specs

--- otter/log/spec.py
+++ otter/log/spec.py
@@ -102,6 +102,7 @@ msg_types = {
     # event -> [(event, format_str)]
     "add-server-clb": ("Adding {server_id} with IP address {ip_address} "
                        "to CLB {clb_id}"),
+    "beg-exec-convergence": "Beginning to execute convergence",
     "converge-all-groups": "Attempting to converge all dirty groups",
     "converge-all-groups-error": "Error while converging all groups",
     "converge-divergent-flag-disappeared":
@@ -115,6 +116,8 @@ msg_types = {
     "execute-convergence": split_execute_convergence,
     "execute-convergence-results": (
         "Got result of {worst_status} after executing convergence"),
+    "get-conv-exec-data": (
+        "Convergence gathering completed in {seconds_taken} seconds"),
     "launch-servers": "Launching {num_servers} servers",
     "mark-clean-failure": "Failed to mark group {scaling_group_id} clean",
     "mark-clean-not-found": (
