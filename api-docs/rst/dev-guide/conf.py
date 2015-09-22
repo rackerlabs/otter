d8f6d06bd257e0ce4664ba0d8b9cb77a5fde8306
Margaret Eker <meker@hotmail.com>
2015-09-21 21:08:40 -0500

    More conf.py syntax changes to resolve build errors.

--- api-docs/rst/dev-guide/conf.py
+++ api-docs/rst/dev-guide/conf.py
@@ -18,6 +18,7 @@ import sys
 # -- Custom options for PHP output ----------------------------------------
 
 from pygments.lexers.web import PhpLexer
+
 from sphinx.highlighting import lexers
 
 lexers['php'] = PhpLexer(startinline=True)
@@ -307,4 +308,3 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 # texinfo_no_detailmenu = False
-
