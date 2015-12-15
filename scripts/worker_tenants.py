50ecd98c2b495b6daef71aee6876931f128e5472
Manish Tomar <manish.tomar@rackspace.com>
2015-12-15 14:04:40 -0800

    script to print worker tenants
    generated list can be used in
    https://github.com/rackerlabs/autoscaling-chef/pull/806. Later can also
    be useful to get list of all tenants for whatever metrics-y reason

---
+++ scripts/worker_tenants.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+"""
+Script to print worker tenants. It gets all tenants from metrics and prints
+tenants which are not in "convergence-tenants" config. Takes chef otter.json
+as argument
+"""
+
+import json
+import os
+import sys
+
+import treq
+
+from twisted.internet import task
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+from otter.auth import generate_authenticator, public_endpoint_url
+from otter.util.http import append_segments, check_success, headers
+
+
+def get_tenant_ids(token, catalog):
+    endpoint = public_endpoint_url(catalog, "cloudMetrics", "IAD")
+    d = treq.get(
+        append_segments(endpoint, "metrics", "search"),
+        headers=headers(token), params={"query": "*.*.desired"})
+    d.addCallback(check_success, [200])
+    d.addCallback(treq.json_content)
+    d.addCallback(lambda body: [item["metric"].split(".")[1] for item in body])
+    return d
+
+
+@inlineCallbacks
+def main(reactor):
+    conf = json.load(open(sys.argv[1]))["default_attributes"]["otter"]["config"]
+    conf["identity"]["strategy"] = "single_tenant"
+    conf["identity"]["username"] = "stretchdev"
+    conf["identity"]["password"] = os.environ["STRETCHDEV_PWD"]
+    authenticator = generate_authenticator(reactor, conf["identity"])
+    token, catalog = yield authenticator.authenticate_tenant("764771")
+    all_tenants = set((yield get_tenant_ids(token, catalog)))
+    worker_tenants = all_tenants - set(conf["convergence-tenants"])
+    print "\n".join(worker_tenants)
+
+
+if __name__ == '__main__':
+    task.react(main, ())
