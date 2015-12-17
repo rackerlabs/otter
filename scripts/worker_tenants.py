62952bffdabc660fdd861742b31d1cde52c02cd4
Manish Tomar <manish.tomar@rackspace.com>
2015-12-17 11:15:35 -0800

    lint

--- scripts/worker_tenants.py
+++ scripts/worker_tenants.py
@@ -6,6 +6,8 @@ tenants which are not in "convergence-tenants" config. Takes chef otter.json
 as argument
 """
 
+from __future__ import print_function
+
 import json
 import os
 import sys
@@ -13,7 +15,7 @@ import sys
 import treq
 
 from twisted.internet import task
-from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.internet.defer import inlineCallbacks
 
 from otter.auth import generate_authenticator, public_endpoint_url
 from otter.util.http import append_segments, check_success, headers
@@ -32,15 +34,16 @@ def get_tenant_ids(token, catalog):
 
 @inlineCallbacks
 def main(reactor):
-    conf = json.load(open(sys.argv[1]))["default_attributes"]["otter"]["config"]
+    conf = json.load(open(sys.argv[1]))
+    conf = conf["default_attributes"]["otter"]["config"]
     conf["identity"]["strategy"] = "single_tenant"
-    conf["identity"]["username"] = "stretchdev"
-    conf["identity"]["password"] = os.environ["STRETCHDEV_PWD"]
+    conf["identity"]["username"] = os.environ["OTTERPROD_UNAME"]
+    conf["identity"]["password"] = os.environ["OTTERPROD_PWD"]
     authenticator = generate_authenticator(reactor, conf["identity"])
     token, catalog = yield authenticator.authenticate_tenant("764771")
     all_tenants = set((yield get_tenant_ids(token, catalog)))
     worker_tenants = all_tenants - set(conf["convergence-tenants"])
-    print "\n".join(worker_tenants)
+    print(worker_tenants, sep='\n')
 
 
 if __name__ == '__main__':
