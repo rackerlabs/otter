cc473666d4a1220211f9e13f28bad48904ccf84d
Drago Rosson <drago@rosson.me>
2016-01-21 16:22:14 +0000

    Convert endpoint to prevent unicode error

--- otter/integration/tests/test_launch_stack.py
+++ otter/integration/tests/test_launch_stack.py
@@ -80,7 +80,7 @@ class TestLaunchStack(unittest.TestCase):
 
     def get_stack_list(self):
         return (self.treq.get(
-                    '%s/stacks' % self.rcs.endpoints['heat'],
+                    '%s/stacks' % str(self.rcs.endpoints['heat']),
                     headers=headers(str(self.rcs.token)),
                     params={
                         'tags': get_stack_tag_for_group(self.group.group_id)},
