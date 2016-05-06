82739212f1f5cdac0ef4dac5db987b8e7ad8e95e
Manish Tomar <manishtomar.public@gmail.com>
2016-05-05 17:11:20 -0700

    started tests - some of them work

--- otter/convergence/selfheal.py
+++ otter/convergence/selfheal.py
@@ -2,13 +2,23 @@
 Self heal service
 """
 
+from effect import Effect
+from effect.do import do, do_return
+
+from kazoo.protocol.states import KazooState
+
 from toolz.curried import filter
 
+from twisted.application.internet import TimerService
 from twisted.application.service import MultiService
+from twisted.internet.defer import inlineCallbacks, returnValue
 
 from txeffect import perform
 
+from otter.convergence.composition import tenant_is_enabled
 from otter.convergence.service import trigger_convergence
+from otter.models.intents import GetAllValidGroups, GetScalingGroupInfo
+from otter.util.zk import GetChildren
 
 
 class SelfHeal(MultiService, object):
@@ -22,49 +32,69 @@ class SelfHeal(MultiService, object):
         super(SelfHeal, self).__init__()
         self.disp = dispatcher
         self.log = log
-        self.lock = self.kz_client.Lock("/selfheallock")
-        self.lock_acquired = False
+        self.lock = kz_client.Lock("/selfheallock")
+        self.kz_client = kz_client
         self.clock = clock
         self.time_range = interval - 5
         self.config_func = config_func
+        self.calls = []
         timer = TimerService(interval, self._converge_all)
         timer.clock = clock
         timer.setServiceParent(self)
 
     def stopService(self):
-        # We explicitly do not wait for convergence triggering to complete
-        # before shutting down the service. We however want to release the lock
-        # before shutting down
         super(SelfHeal, self).stopService()
-        if self.lock_acquired:
-            return self.lock.release()
+        self._cancel_scheduled_calls()
+        return self.lock.release()
+
+    def _cancel_scheduled_calls(self):
+        """
+        Cancel any remaining scheduled calls
+        """
+        active = 0
+        for call in self.calls:
+            if call.active():
+                active += 1
+                call.cancel()
+        self.calls = []
+        return active
 
     def health_check(self):
         """
         Return about whether this object has lock
         """
-        return True, {"has_lock": self.lock_acquired}
+        d = lock_is_acquired(self.disp, self.lock)
+        return d.addCallback(lambda b: (True, {"has_lock": b}))
 
     def _perform(self):
-        d = perform(self.disp, get_groups_to_converges(self.config_func))
+        d = perform(self.disp, get_groups_to_converge(self.config_func))
         d.addCallback(self._setup_converges)
         return d.addErrback(self.log.err, "self-heal-err")
 
     def _setup_converges(self, groups):
+        active = self._cancel_scheduled_calls()
+        if active:
+            self.log.err(RuntimeError("self-heal-calls-err"),
+                         "self-heal-calls-err", num_calls=active)
         wait_time = self.time_range / len(groups)
         for i, group in enumerate(groups):
-            self.clock.callLater(
-                i * wait_time, perform, self.disp,
-                check_and_trigger(group["tenantId"], group["groupId"]))
+            self.calls.append(
+                self.clock.callLater(
+                    i * wait_time, perform, self.disp,
+                    check_and_trigger(group["tenantId"], group["groupId"]))
+            )
 
     @inlineCallbacks
     def _converge_all(self):
-        if self.lock_acquired:
+        if self.kz_client.state != KazooState.CONNECTED:
+            self.log.err(RuntimeError("self-heal-kz-state"),
+                         "self-heal-kz-state", state=self.kz_client.state)
+            returnValue(None)
+        if (yield lock_is_acquired(self.lock)):
             yield self._perform()
         else:
             if (yield self.lock.acquire(False, None)):
                 self.log.msg("self-heal-lock-acquired")
-                self.lock_acquired = True
                 yield self._perform()
 
 
@@ -89,3 +119,17 @@ def check_and_trigger(tenant_id, group_id):
 
     if (state["status"] == "ACTIVE") and (not state["paused"]):
         yield trigger_convergence(tenant_id, group_id)
+
+
+def lock_is_acquired(dispatcher, lock):
+    return perform(dispatcher, lock_is_acquired_eff(lock))
+
+
+def lock_is_acquired_eff(lock):
+    children = yield Effect(GetChildren(lock.path))
+    if not children:
+        yield do_return(False)
+    # The last 10 characters are sequence number as per
+    # https://zookeeper.apache.org/doc/current/zookeeperProgrammers.html\
+    # #Sequence+Nodes+--+Unique+Naming
+    yield do_return(sorted(children, key=lambda c: c[-10:])[0] == lock.node)
--- otter/test/convergence/test_selfheal.py
+++ otter/test/convergence/test_selfheal.py
@@ -2,6 +2,17 @@
 Tests for :mod:`otter.convergence.selfheal`
 """
 
+from kazoo.protocol.states import KazooState
+
+import mock
+
+from twisted.internet.defer import fail, succeed
+from twisted.internet.task import Clock
+from twisted.trial.unittest import SynchronousTestCase
+
+from otter.convergence.selfheal import SelfHeal
+from otter.test.utils import intent_func, mock_log, patch
+
 
 class SelfHealTests(SynchronousTestCase):
     """
@@ -11,15 +22,90 @@ class SelfHealTests(SynchronousTestCase):
     def setUp(self):
         self.lock = mock.Mock(specs=["acquire", "release"])
         self.kzc = mock.Mock(specs=["Lock"])
+        self.kzc.state = KazooState.CONNECTED
+        self.kzc.Lock.return_value = self.lock
+        self.clock = Clock()
+        self.log = mock_log()
+        self.ggtc = patch(
+            self, "otter.convergence.selfheal.get_groups_to_converge",
+            side_effect=intent_func("ggtc"))
+        self.lia = patch(self, "otter.convergence.selfheal.lock_is_acquired")
+        self.s = SelfHeal("disp", self.kzc, 300, self.log, self.clock, "cf")
 
-    def test_calls_cat(self):
-        s = ConvergeAllGroups(disp,
+    def test_converge_all_lock_not_acquired(self):
+        """
+        When lock is not acquired, it is tried and if failed does not
+        call _perform
+        """
+        self.lia.return_value = succeed(False)
+        self.lock.acquire.return_value = succeed(False)
+        # tests that it is not called
+        self.s._perform = lambda: 1 / 0
+        self.s.startService()
+        self.lock.acquire.assert_called_once_with(False, None)
+
+    def test_converge_all_lock_acquired(self):
+        """
+        When lock is not acquired, it is tried and if successful self._perform
+        is called
+        """
+        self.lia.return_value = succeed(False)
+        self.lock.acquire.return_value = succeed(True)
+        self.s._perform = mock.Mock()
+        self.s.startService()
+        self.lock.acquire.assert_called_once_with(False, None)
+        self.s._perform.assert_called_once_with()
+        self.log.msg.assert_called_once_with("self-heal-lock-acquired")
+
+    def test_converge_all_lock_already_acquired(self):
+        """
+        If lock is already acquired, it will just call self._perform
+        """
+        self.lia.return_value = succeed(True)
+        self.s._perform = mock.Mock()
+        self.s.startService()
+        # Lock is not acquired again
+        self.assertFalse(self.lock.acquire.called)
+        self.s._perform.assert_called_once_with()
+
+    def test_converge_all_kz_not_connected(self):
+        """
+        If kazoo client is not connected then nothing is done
+        """
+        self.kzc.state = KazooState.LOST
+
+        def bad_func(*a):
+            return 1 / 0
+
+        self.s._perform = self.lia.side_effect = \
+            self.lock.acquire.side_effect = bad_func
+        self.s.startService()
+        self.log.err.assert_called_once_with(
+            mock.ANY, "self-heal-kz-state", state=KazooState.LOST)
+
+    def test_performs_again(self):
+        """
+        Calls _perform at every interval
+        """
+        self.lia.return_value = succeed(True)
+        self.s._perform = mock.Mock()
+        self.s.startService()
+        self.s._perform.assert_called_once_with()
+        self.clock.advance(300)
+        self.assertEqual(self.s._perform.call_count, 2)
+
+    def test_perform(self):
+        """
+        Gets groups and sets up convergence to be triggered at future time
+        """
+
+    def test_perform_errs(self):
+        """
+        If getting groups fails, perform just logs the error
+        """
 
     def test_health_check(self):
+        pass
 
     def test_stop_service(self):
-
-
-
-
-
+        pass
