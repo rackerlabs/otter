15dce4ecc3981124ff15912683090421ba1f1fe9
Manish Tomar <manishtomar.public@gmail.com>
2016-06-13 14:06:50 -0700

    selfheal integration test

---
+++ otter/integration/tests/test_selfheal.py
@@ -0,0 +1,74 @@
+"""
+Test self heal service
+"""
+
+import os
+
+from testtools.matchers import ContainsDict, Equals, MatchesListwise, NotEquals
+
+from twisted.trial.unittest import TestCase
+from twisted.internet.defer import inlineCallbacks
+
+from otter.integration.lib.nova import NovaServer, extract_active_ids
+from otter.integration.lib.resources import TestResources
+from otter.integration.lib.trial_tools import (
+    TestHelper,
+    get_identity,
+    get_resource_mapping,
+    region,
+    skip_if,
+)
+
+
+def only_server_id(rcs, group):
+    """
+    Extract only server id in the group
+    """
+    d = group.get_scaling_group_state(rcs, [200])
+    return d.addCallback(lambda (_, state): extract_active_ids(state))
+
+
+class SelfHealTests(TestCase):
+
+    def setUp(self):
+        self.helper = TestHelper(self)
+        self.rcs = TestResources()
+        self.identity = get_identity(self.helper.pool)
+        return self.identity.authenticate_user(
+            self.rcs,
+            resources=get_resource_mapping(),
+            region=region
+        )
+
+    @skip_if(lambda _: "AS_SELFHEAL_INTERVAL" in os.environ,
+             "AS_SELFHEAL_INTERVAL environment variable needed")
+    @inlineCallbacks
+    def test_selfheal(self):
+        """
+        SelfHeal service will replace deleted server
+        """
+        group, _ = self.helper.create_group(min_entities=1)
+        yield group.start(self.rcs, self)
+        yield group.wait_for_state(
+            self.rcs, ContainsDict({"activeCapacity": Equals(1)}))
+        # delete server OOB
+        server_id = yield only_server_id(self.rcs)
+        yield NovaServer(id=server_id, pool=self.pool).delete()
+        # Wait for new server to come back up by self heal service. It can
+        # take 2 * selfheal interval because the new group may get scheduled
+        # to be triggered after last scheduling is already setup
+        yield group.wait_for_state(
+            self.rcs,
+            ContainsDict(
+                {"active": MatchesListwise([
+                    ContainsDict({"id": NotEquals(server_id)})])}),
+            timeout=os.environ["AS_SELFHEAL_INTERVAL"] * 2)
+        # Delete new server again and see if it comes back. It should be
+        # back within selfheal interval
+        server_id = yield only_server_id(self.rcs)
+        yield group.wait_for_state(
+            self.rcs,
+            ContainsDict(
+                {"active": MatchesListwise([
+                    ContainsDict({"id": NotEquals(server_id)})])}),
+            timeout=os.environ["AS_SELFHEAL_INTERVAL"])
