ccf729bc1c778137342f9fbd4a713014ee7bc697
Manish Tomar <manishtomar.public@gmail.com>
2016-12-02 11:18:35 -0800

    cafe should run intedependently - NOT TESTED
    which is why this is separate branch

--- cafe_entrypoint.sh
+++ cafe_entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 set -e
-dockerize -wait http://otter:9000/health -wait http://mimic:8900 -timeout 30s
+
+OTTER_ROOT=${OTTER:-"http://otter:9000"}
+IDENTITY_ROOT=${IDENTITY_ROOT:-"http://mimic:8900"}
+
+if [ -n "$WAIT" ]; then
+    dockerize -wait $OTTER_ROOT/health -wait $IDENTITY_ROOT  -timeout 30s
+fi
+
 exec cafe-runner autoscale "$@"
