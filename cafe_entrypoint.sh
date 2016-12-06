45d063d19264008716d50e74f850de05a5d74750
Manish Tomar <manishtomar.public@gmail.com>
2016-12-05 21:37:40 -0800

    cafe container code is mounted from host
    in docker-compose.yml. Also, cafe container can be run independently based
    on env vars.

--- cafe_entrypoint.sh
+++ cafe_entrypoint.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-OTTER_ROOT=${OTTER:-"http://otter:9000"}
+OTTER_ROOT=${OTTER_ROOT:-"http://otter:9000"}
 IDENTITY_ROOT=${IDENTITY_ROOT:-"http://mimic:8900"}
+CAFE_DIR=/root/.cloudcafe/configs/autoscale
+
+sed -i -e "s,OTTER_ROOT,$OTTER_ROOT,g" $CAFE_DIR/dev-convergence.config $CAFE_DIR/dev-worker.config
+sed -i -e "s,IDENTITY_ROOT,$IDENTITY_ROOT,g" $CAFE_DIR/dev-convergence.config $CAFE_DIR/dev-worker.config
 
 if [ -n "$WAIT" ]; then
     dockerize -wait $OTTER_ROOT/health -wait $IDENTITY_ROOT  -timeout 30s
