rackerlabs/otter

commit 5dcd716adbf9865062a6f86f72e6ac01d5f4b799

drained_at as property - thanks @glyph
This way any access to it will raise DrainingUnavailable exception.

otter/convergence/model.py
@@ -586,7 +586,7 @@ class DrainingUnavailable(Exception):
 @attributes([Attribute("node_id", instance_of=basestring),
              Attribute("description", instance_of=CLBDescription),
              Attribute("address", instance_of=basestring),
-             Attribute("drained_at", default_value=None),
+             Attribute("_drained_at", default_value=None),
              Attribute("is_online", instance_of=bool,
                        default_value=True),
              Attribute("connections", default_value=None)])
@@ -603,14 +603,29 @@ class CLBNode(object):
     :ivar str address: The IP address of the node.  The IP and port form a
         unique mapping on the CLB, which is assigned a node ID.  Two
         nodes with the same IP and port cannot exist on a single CLB.
-    :ivar float drained_at: Seconds since EPOCH at which this node was put in
-        DRAINING.  Should be None if node is not DRAINING or when this info is
-        not available.
+    :ivar float _drained_at: Seconds since EPOCH at which this node was put in
+        DRAINING. This also represnts the time at which the node was created
+        for a node that was created in DRAINING. Should be None if node is not
+        DRAINING or when this info is not available.
     :ivar bool is_online: Is this node ONLINE and receiving traffic? This field
         corresponds to node's `status` field.
     :ivar int connections: The number of active connections on the node - this
         is None by default (the stat is not available yet).
     """
+
+    @property
+    def drained_at(self):
+        """
+        Return when this node was drained.
+
+        :return: Seconds since EPOCH
+        :rtype: float
+        :raises: :obj:`DrainingUnavailable` if this info is not available
+        """
+        if self._drained_at is None:
+            raise DrainingUnavailable(self.description.lb_id, self.node_id)
+        return self._drained_at
+
     def matches(self, server):
         """
         See :func:`ILBNode.matches`.
@@ -628,8 +643,6 @@ class CLBNode(object):
         """
         See :func:`IDrainable.is_done_draining`.
         """
-        if self.drained_at is None:
-            raise DrainingUnavailable(self.description.lb_id, self.node_id)
         return now - self.drained_at >= timeout or self.connections == 0
 
     def is_active(self):

otter/convergence/planning.py
@@ -426,18 +426,21 @@ def converge_launch_server(desired_state, servers_with_cheese,
     # converge all the servers that remain to their desired load balancer state
     still_active_servers = filter(lambda s: s not in servers_to_delete,
                                   servers_in_active)
-    lb_converge_steps = [
-        step
-        for server in still_active_servers
-        for step in _converge_lb_state(
-            server,
-            [node for node in load_balancer_nodes if node.matches(server)],
-            load_balancers,
-            now,
-            # Temporarily using build timeout as node offline timeout.
-            # See https://github.com/rackerlabs/otter/issues/1905
-            timeout)
-        ]
+    try:
+        lb_converge_steps = [
+            step
+            for server in still_active_servers
+            for step in _converge_lb_state(
+                server,
+                [node for node in load_balancer_nodes if node.matches(server)],
+                load_balancers,
+                now,
+                # Temporarily using build timeout as node offline timeout.
+                # See https://github.com/rackerlabs/otter/issues/1905
+                timeout)
+            ]
+    except DrainingUnavailable as de:
+        return pbag([fail_convergence(de)])
 
     # Converge again if we expect state transitions on any servers
     converge_later = []
@@ -657,9 +660,8 @@ def change_lb_node(node, description, lb, now, timeout):
                                      condition=CLBNodeCondition.ENABLED,
                                      weight=description.weight,
                                      type=description.type)
-            if node.drained_at is None:
-                return fail_convergence(
-                    DrainingUnavailable(description.lb_id, node.node_id))
+            # For a new node created in DRAINING, drained_at represents
+            # node's creation time.
             if now - node.drained_at > timeout:
                 rsfmt = ("Node {} has remained OFFLINE for more than "
                          "{} seconds")

commit 9b356d6c4d5deba206a4c213f7ea460c98cb1f6f

fixes error introduced in commit 5dcd716
assoc_obj was the real culprit which returned CLBNode with
_drained_at=None all the time. This made the unit tests pass as code
and test got objects with None drained_at. Fixed by switching to attr and using
its assoc method which ensures the attribute exists before accepting.

otter/convergence/gathering.py
@@ -35,7 +35,6 @@ from otter.convergence.model import (
     group_id_from_metadata)
 from otter.indexer import atom
 from otter.models.cass import CassScalingGroupServersCache
-from otter.util.fp import assoc_obj
 from otter.util.http import append_segments
 from otter.util.retry import (
     exponential_backoff_interval, retry_effect, retry_times)
@@ -211,9 +210,9 @@ def get_clb_contents():
         if node.description.lb_id in deleted_lbs:
             return None
         if feed is not None:
-            return assoc_obj(node, drained_at=extract_clb_drained_at(feed))
-        else:
-            return node
+            node.drained_at = extract_clb_drained_at(feed)
+        return node
+
     nodes = map(update_drained_at, concat(lb_nodes.values()))
     yield do_return((
         list(filter(bool, nodes)),

otter/convergence/model.py
@@ -6,7 +6,7 @@ import json
 import re
 
 import attr
-from attr.validators import instance_of
+from attr.validators import instance_of, optional
 
 from characteristic import Attribute, attributes
 
@@ -583,13 +583,7 @@ class DrainingUnavailable(Exception):
 
 
 @implementer(ILBNode, IDrainable)
-@attributes([Attribute("node_id", instance_of=basestring),
-             Attribute("description", instance_of=CLBDescription),
-             Attribute("address", instance_of=basestring),
-             Attribute("_drained_at", default_value=None),
-             Attribute("is_online", instance_of=bool,
-                       default_value=True),
-             Attribute("connections", default_value=None)])
+@attr.s
 class CLBNode(object):
     """
     A Rackspace Cloud Load Balancer node.
@@ -612,6 +606,12 @@ class CLBNode(object):
     :ivar int connections: The number of active connections on the node - this
         is None by default (the stat is not available yet).
     """
+    node_id = attr.ib(validator=instance_of(basestring))
+    description = attr.ib(validator=instance_of(CLBDescription))
+    address = attr.ib(validator=instance_of(basestring))
+    _drained_at = attr.ib(validator=optional(instance_of(float)), default=None)
+    is_online = attr.ib(validator=optional(instance_of(bool)), default=True)
+    connections = attr.ib(default=None)
 
     @property
     def drained_at(self):
@@ -625,6 +625,15 @@ class CLBNode(object):
         if self._drained_at is None:
             raise DrainingUnavailable(self.description.lb_id, self.node_id)
         return self._drained_at
+
+    @drained_at.setter
+    def drained_at(self, updated):
+        """
+        Update the internal _drained_at value
+
+        :param float updated: The updated value as seconds since EPOCH
+        """
+        self._drained_at = updated
 
     def matches(self, server):
         """

otter/test/convergence/test_gathering.py
@@ -4,6 +4,8 @@ from copy import deepcopy
 from datetime import datetime
 from functools import partial
 
+import attr
+
 from effect import (
     ComposedDispatcher,
     Constant,
@@ -64,7 +66,6 @@ from otter.test.utils import (
     server,
     stack
 )
-from otter.util.fp import assoc_obj
 from otter.util.retry import (
     Retry, ShouldDelayAndRetry, exponential_backoff_interval, retry_times)
 from otter.util.timestamp import timestamp_to_epoch
@@ -492,12 +493,10 @@ class GetCLBContentsTests(SynchronousTestCase):
         eff = get_clb_contents()
         self.assertEqual(
             perform_sequence(seq, eff),
-            ([assoc_obj(CLBNode.from_node_json(1, node11),
-                        drained_at=1.0),
+            ([attr.assoc(CLBNode.from_node_json(1, node11), _drained_at=1.0),
               CLBNode.from_node_json(1, node12),
               CLBNode.from_node_json(2, node21),
-              assoc_obj(CLBNode.from_node_json(2, node22),
-                        drained_at=2.0)],
+              attr.assoc(CLBNode.from_node_json(2, node22), _drained_at=2.0)],
              {'1': CLB(True), '2': CLB(False)}))
 
     def test_no_lb(self):
@@ -605,7 +604,7 @@ class GetCLBContentsTests(SynchronousTestCase):
         eff = get_clb_contents()
         self.assertEqual(
             perform_sequence(seq, eff),
-            ([assoc_obj(CLBNode.from_node_json(2, node21), drained_at=2.0)],
+            ([attr.assoc(CLBNode.from_node_json(2, node21), _drained_at=2.0)],
              {'2': CLB(True)}))
 
 