29b9ddd649910175092926f52af23385c070b0d7
rahu3180 <rahul.bardia@rackspace.com>
2019-03-06 06:17:46 -0800

    Adding token in self token

--- otter/auth.py
+++ otter/auth.py
@@ -276,15 +276,15 @@ class ImpersonatingAuthenticator(object):
         auth = partial(self._auth_me, log=log)
 #        d = auth()
         default_log.msg("RAHU3180 : Testing msg func")
-        if self._token is None:
-            e = authenticate_user(self._url,
-                                  self._identity_admin_user,
-                                  self._identity_admin_password,
-                                  log=log)
-            e.addCallback(extract_token)
+#        if self._token is None:
+        e = authenticate_user(self._url,
+                              self._identity_admin_user,
+                              self._identity_admin_password,
+                              log=log)
+        e.addCallback(extract_token)
 
 #            e.addErrback(_log_failed_auth)
-            e.addCallback(partial(setattr, self, "_token"))
+        e.addCallback(partial(setattr, self, "_token"))
         d = user_for_tenant(self._admin_url,
                             self._identity_admin_user,
                             self._token,
