5da343f9edab50a3c9044f83ae3dd95dea0db09a
Ravi <raviteja.valluri@rackspace.com>
2021-04-21 08:12:35 -0700

    Scripts and Conf Updates

--- api-docs/rst/dev-guide/conf.py
+++ api-docs/rst/dev-guide/conf.py
@@ -170,7 +170,8 @@ else:
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "collapse_navigation" : False
+    "collapse_navigation" : False,
+    "sticky_navigation": True, 
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
